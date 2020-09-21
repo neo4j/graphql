acb4e09fb35431a88157ec345bb6118253ca01af
Jared Kozak <jared@kozan.dev>
2020-09-21 14:11:37 +0200

    Don’t compile test files + allow jest/test globals

--- jest-global.config.js
+++ jest-global.config.js
@@ -1,9 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    globalSetup: path.join(__dirname, 'jest-global.setup.js'),
+    globalSetup: path.join(__dirname, "jest-global.setup.js"),
     verbose: true,
     // Windows builds are incredibly slow, and filesystem operations tend
     // to take that long.
     testTimeout: 120000,
+    testMatch: [`./**/*.test.ts`],
 };
