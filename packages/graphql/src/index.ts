e8bf1283c235f2e7d8f440d906bf22963f12380d
Jared Kozak <jared@kozan.dev>
2020-09-23 16:47:48 +0100

    feat: add support for skip & limit

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -5,15 +5,15 @@ export type Context = Record<string, unknown>;
 
 export function cypherQuery(args: any, _context: Context, resolveInfo: GraphQLResolveInfo): [string, any] {
     const { returnType } = resolveInfo;
-
     const typeName = returnType.toString();
 
     const selections = getSelections(resolveInfo);
     const queryArgs = getArguments(resolveInfo);
 
     const safeVariableName = safeVar(lowFirstLetter(typeName));
-
     const safeLabelName = safeLabel([typeName]);
+
+    let cypherParams: { [k: string]: any } = {};
 
     const projection = selections?.reduce((proj: string[], selection) => {
         if (selection.kind !== "Field") {
@@ -23,33 +23,63 @@ export function cypherQuery(args: any, _context: Context, resolveInfo: GraphQLRe
         return proj.concat(`.${selection.name.value}`);
     }, []) as string[];
 
-    const properties = queryArgs.reduce(
-        (allArgs, currentArg) => {
-            let incomingArg;
+    const properties = queryArgs
+        .filter((x) => !["skip", "limit"].includes(x.name.value))
+        .reduce(
+            (allArgs, currentArg) => {
+                let incomingArg;
 
-            if ("value" in currentArg.value) {
-                incomingArg = currentArg.value.value;
-            } else {
-                incomingArg = args[currentArg.name.value];
-            }
+                if ("value" in currentArg.value) {
+                    incomingArg = currentArg.value.value;
+                } else {
+                    incomingArg = args[currentArg.name.value];
+                }
 
-            const argName = currentArg.name.value;
-            const safeName = safeVar(argName);
+                const argName = currentArg.name.value;
+                const safeName = safeVar(argName);
 
-            return {
-                propertiesArgs: {
-                    ...allArgs.propertiesArgs,
-                    [currentArg.name.value]: incomingArg,
-                },
-                propertiesArr: allArgs.propertiesArr.concat(`${safeName}:$${argName}`),
-            };
-        },
-        { propertiesArr: [], propertiesArgs: {} } as { propertiesArr: string[]; propertiesArgs: any }
-    );
+                return {
+                    propertiesArgs: {
+                        ...allArgs.propertiesArgs,
+                        [currentArg.name.value]: incomingArg,
+                    },
+                    propertiesArr: allArgs.propertiesArr.concat(`${safeName}:$${argName}`),
+                };
+            },
+            { propertiesArr: [], propertiesArgs: {} } as { propertiesArr: string[]; propertiesArgs: any }
+        );
+
+    cypherParams = { ...cypherParams, ...properties.propertiesArgs };
+
+    let skipStr = "";
+    const skipArg = queryArgs.find((x) => x.name.value === "skip");
+    if (skipArg) {
+        skipStr = "SKIP $skip";
+
+        if ("value" in skipArg.value) {
+            cypherParams.skip = skipArg.value.value;
+        } else {
+            cypherParams.skip = args[skipArg.name.value];
+        }
+    }
+
+    let limitStr = "";
+    const limitArg = queryArgs.find((x) => x.name.value === "limit");
+    if (limitArg) {
+        limitStr = "LIMIT $limit";
+
+        if ("value" in limitArg.value) {
+            cypherParams.limit = limitArg.value.value;
+        } else {
+            cypherParams.limit = args[limitArg.name.value];
+        }
+    }
 
     const query = `
         MATCH (${safeVariableName}:${safeLabelName}${formatCypherProperties(properties.propertiesArr)})
         RETURN ${safeVariableName}${formatCypherProperties(projection)} AS ${safeVariableName}
+        ${skipStr || ""}
+        ${limitStr || ""}
     `;
 
     return [query, args];
