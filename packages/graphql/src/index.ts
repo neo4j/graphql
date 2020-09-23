1542c1966a2cd71eaaaa4c08463ad740e70b6339
Jared Kozak <jared@kozan.dev>
2020-09-23 13:26:20 +0100

    feat: add match properties/arguments support

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-useless-escape */
-import { GraphQLResolveInfo, SelectionNode } from "graphql";
+import { ArgumentNode, GraphQLResolveInfo, SelectionNode } from "graphql";
 
 export type Context = Record<string, unknown>;
 
@@ -9,29 +9,66 @@ export function cypherQuery(args: any, _context: Context, resolveInfo: GraphQLRe
     const typeName = returnType.toString();
 
     const selections = getSelections(resolveInfo);
+    const queryArgs = getArguments(resolveInfo);
 
     const safeVariableName = safeVar(lowFirstLetter(typeName));
 
     const safeLabelName = safeLabel([typeName]);
 
-    const projection = selections?.reduce((proj, selection, i, arr) => {
+    const projection = selections?.reduce((proj: string[], selection) => {
         if (selection.kind !== "Field") {
             return proj;
         }
 
-        const last = Boolean(i === arr.length - 1);
-        const lastBracket = last ? "}" : "";
-        const trailingComma = !last ? "," : "";
+        return proj.concat(`.${selection.name.value}`);
+    }, []) as string[];
 
-        return `${proj} .${selection.name.value}${trailingComma} ${lastBracket}`;
-    }, "{");
+    const properties = queryArgs.reduce(
+        (allArgs, currentArg) => {
+            let incomingArg;
+
+            if ("value" in currentArg.value) {
+                incomingArg = currentArg.value.value;
+            } else {
+                incomingArg = args[currentArg.name.value];
+            }
+
+            const argName = currentArg.name.value;
+            const safeName = safeVar(argName);
+
+            return {
+                propertiesArgs: {
+                    ...allArgs.propertiesArgs,
+                    [currentArg.name.value]: incomingArg,
+                },
+                propertiesArr: allArgs.propertiesArr.concat(`${safeName}:$${argName}`),
+            };
+        },
+        { propertiesArr: [], propertiesArgs: {} } as { propertiesArr: string[]; propertiesArgs: any }
+    );
 
     const query = `
-        MATCH (${safeVariableName}:${safeLabelName})
-        RETURN ${safeVariableName} ${projection} AS ${safeVariableName}
+        MATCH (${safeVariableName}:${safeLabelName}${formatCypherProperties(properties.propertiesArr)})
+        RETURN ${safeVariableName}${formatCypherProperties(projection)} AS ${safeVariableName}
     `;
 
     return [query, args];
+}
+
+function formatCypherProperties(properties: string[]): string {
+    if (!properties.length) {
+        return "";
+    }
+
+    return ` { ${properties.join(", ")} }`;
+}
+
+function getArguments(resolveInfo: GraphQLResolveInfo): ArgumentNode[] {
+    const node = resolveInfo.fieldNodes.find((n) => n.name.value === resolveInfo.fieldName);
+
+    const args = node?.arguments as ArgumentNode[];
+
+    return args;
 }
 
 function getSelections(resolveInfo: GraphQLResolveInfo): SelectionNode[] | undefined {
