4465b567ea1994e078b97b5f8746be0a02354e72
Jared Kozak <jared@kozan.dev>
2020-09-24 14:27:38 +0100

    test: add for remove type meta

---
+++ packages/graphql/tests/unit/graphql/remove-type-meta.unit.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "@jest/globals";
+import removeTypeMeta from "../../../src/graphql/remove-type-meta";
+
+describe("removeTypeMeta", () => {
+    test("should remove []", () => {
+        const initial = "[Movie]";
+
+        expect(removeTypeMeta(initial)).toEqual("Movie");
+    });
+
+    test("should remove !", () => {
+        const initial = "Movie!";
+
+        expect(removeTypeMeta(initial)).toEqual("Movie");
+    });
+
+    test("should remove [] & !", () => {
+        const initial = "[Movie!]!";
+
+        expect(removeTypeMeta(initial)).toEqual("Movie");
+    });
+});
