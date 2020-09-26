78252666d9dc338c2503b3df7a34211adf23100b
Daniel Starns <danielstarns@hotmail.com>
2020-09-26 22:55:08 +0100

    feat: add getFieldDirective

--- packages/graphql/src/graphql/index.ts
+++ packages/graphql/src/graphql/index.ts
@@ -1,3 +1,4 @@
 export { default as getSelections } from "./get-selections";
 export { default as getArguments } from "./get-arguments";
 export { default as removeTypeMeta } from "./remove-type-meta";
+export { default as getFieldDirective } from "./get-field-directive";
