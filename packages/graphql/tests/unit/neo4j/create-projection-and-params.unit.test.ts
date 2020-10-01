d73046eb6f377f76d1bf5b76f8784fcb297af493
Daniel Starns <danielstarns@hotmail.com>
2020-10-01 20:54:38 +0100

    test: add inital for createProjectionAndParams

--- packages/graphql/tests/unit/neo4j/create-projection-and-params.unit.test.ts
+++ packages/graphql/tests/unit/neo4j/create-projection-and-params.unit.test.ts
@@ -1,7 +1,52 @@
 import createProjectionAndParams from "../../../src/neo4j/create-projection-and-params";
+import { NeoSchema } from "../../../src/classes";
 
 describe("createProjectionAndParams", () => {
     test("should be a function", () => {
         expect(createProjectionAndParams).toBeInstanceOf(Function);
+    });
+
+    test("should return the correct projection with 1 selection", () => {
+        const fieldsByTypeName = {
+            Movie: {
+                title: {
+                    name: "title",
+                    alias: "title",
+                    args: {},
+                    fieldsByTypeName: {},
+                },
+            },
+        };
+
+        const node = {
+            name: "Movie",
+            relationFields: [],
+            cypherFields: [],
+            primitiveFields: [
+                {
+                    fieldName: "title",
+                    typeMeta: {
+                        name: "String",
+                        array: false,
+                        required: false,
+                        pretty: "String",
+                    },
+                    otherDirectives: [],
+                    arguments: [],
+                },
+            ],
+        };
+
+        // @ts-ignore
+        const neoSchema: NeoSchema = {
+            nodes: [node],
+        };
+
+        const typeName = "Movie";
+
+        const result = createProjectionAndParams({ fieldsByTypeName, node, neoSchema, typeName });
+
+        expect(result[0]).toEqual(`{ .title }`);
+        expect(result[1]).toMatchObject({});
     });
 });
