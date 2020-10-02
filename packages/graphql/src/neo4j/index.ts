c9856147a27468680ce94a1372084ae3ad56408c
Daniel Starns <danielstarns@hotmail.com>
2020-10-02 22:20:17 +0100

    refactor: remove unused function

--- packages/graphql/src/neo4j/index.ts
+++ packages/graphql/src/neo4j/index.ts
@@ -1,4 +1,3 @@
-export { default as formatCypherProperties } from "./format-cypher-properties";
 export { default as execute } from "./execute";
 export { default as createWhereAndParams } from "./create-where-and-params";
 export { default as createProjectionAndParams } from "./create-projection-and-params";
