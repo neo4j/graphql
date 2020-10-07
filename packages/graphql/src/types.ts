40b1ec0eda7ed55f225a34723981fc03e99e83e5
Daniel Starns <danielstarns@hotmail.com>
2020-10-07 17:30:19 +0100

    types: implement graphql querys types and add some comments to them

--- packages/graphql/src/types.ts
+++ packages/graphql/src/types.ts
@@ -1,5 +1,9 @@
 import { InputValueDefinitionNode, DirectiveNode } from "graphql";
 
+/**
+ * Metadata about a field.type on either
+ * FieldDefinitionNode or InputValueDefinitionNode.
+ */
 export interface TypeMeta {
     name: string;
     array: boolean;
@@ -7,6 +11,9 @@ export interface TypeMeta {
     pretty: string;
 }
 
+/**
+ * Representation a ObjectTypeDefinitionNode field.
+ */
 export interface BaseField {
     fieldName: string;
     typeMeta: TypeMeta;
@@ -14,13 +21,44 @@ export interface BaseField {
     arguments: InputValueDefinitionNode[];
 }
 
+/**
+ * Representation of the `@relationship` directive and its meta.
+ */
 export interface RelationField extends BaseField {
     direction: "OUT" | "IN";
     type: string;
 }
 
+/**
+ * Representation of the `@cypher` directive and its meta.
+ */
 export interface CypherField extends BaseField {
     statement: string;
 }
 
+/**
+ * Representation of any field thats not
+ * a cypher directive or relationship directive
+ * String, Int, Float, ID, Boolean... (custom scalars).
+ */
 export type PrimitiveField = BaseField;
+
+/**
+ * Representation of the options arg
+ * passed to resolvers.
+ */
+export interface GraphQLOptionsArg {
+    limit?: number;
+    skip?: number;
+    sort?: string[];
+}
+
+/**
+ * Representation of the query arg
+ * passed to resolvers.
+ */
+export interface GraphQLQueryArg {
+    [k: string]: any | GraphQLOptionsArg | GraphQLOptionsArg[];
+    _AND?: any | GraphQLOptionsArg | GraphQLOptionsArg[];
+    _OR?: any | GraphQLOptionsArg | GraphQLOptionsArg[];
+}
