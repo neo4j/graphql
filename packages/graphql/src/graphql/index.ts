50da917edc608c37001796fce8f11ed8a630cb38
Jared Kozak <jared@kozan.dev>
2020-10-07 13:13:06 +0100

    refactor: remove the retrevial of directive meta out of main reduce

--- packages/graphql/src/graphql/index.ts
+++ packages/graphql/src/graphql/index.ts
@@ -1,4 +1,5 @@
-export { default as getFieldDirective } from "./get-field-directive";
 export { default as getFieldTypeMeta } from "./get-field-type-meta";
 export { default as findOne } from "./find-one";
 export { default as findMany } from "./find-many";
+export { default as getRelationshipMeta } from "./get-relationship-meta";
+export { default as getCypherMeta } from "./get-cypher-meta";
---
+++ packages/graphql/tests/unit/graphql/get-cypher-meta.unit.test.ts
@@ -0,0 +1,145 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { FieldDefinitionNode } from "graphql";
+import getCypherMeta from "../../../src/graphql/get-cypher-meta";
+
+describe("getCypherMeta", () => {
+    test("should return undefined if no directive found", () => {
+        // @ts-ignore
+        const field: FieldDefinitionNode = {
+            directives: [
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 1" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 2" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 3" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 4" },
+                },
+            ],
+        };
+
+        const result = getCypherMeta(field);
+
+        expect(result).toEqual(undefined);
+    });
+
+    test("should throw statement required", () => {
+        const field: FieldDefinitionNode = {
+            directives: [
+                {
+                    // @ts-ignore
+                    name: { value: "cypher", arguments: [] },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 2" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 3" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 4" },
+                },
+            ],
+        };
+
+        try {
+            getCypherMeta(field);
+
+            throw new Error("I should not throw");
+        } catch (error) {
+            expect(error.message).toEqual("@cypher statement required");
+        }
+    });
+
+    test("should throw statement not a string", () => {
+        const field: FieldDefinitionNode = {
+            directives: [
+                {
+                    // @ts-ignore
+                    name: {
+                        value: "cypher",
+                        // @ts-ignore
+                    },
+                    arguments: [
+                        {
+                            // @ts-ignore
+                            name: { value: "statement" },
+                            // @ts-ignore
+                            value: { kind: "NOT A STRING!" },
+                        },
+                    ],
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 2" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 3" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 4" },
+                },
+            ],
+        };
+
+        try {
+            getCypherMeta(field);
+
+            throw new Error("I should not throw");
+        } catch (error) {
+            expect(error.message).toEqual("@cypher statement not a string");
+        }
+    });
+
+    test("should return the correct meta", () => {
+        const field: FieldDefinitionNode = {
+            directives: [
+                {
+                    // @ts-ignore
+                    name: {
+                        value: "cypher",
+                    },
+                    arguments: [
+                        {
+                            // @ts-ignore
+                            name: { value: "statement" },
+                            // @ts-ignore
+                            value: { kind: "StringValue", value: "MATCH (m:Movie) RETURN m" },
+                        },
+                    ],
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 2" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 3" },
+                },
+                {
+                    // @ts-ignore
+                    name: { value: "RANDOM 4" },
+                },
+            ],
+        };
+
+        const result = getCypherMeta(field);
+
+        expect(result).toMatchObject({
+            statement: "MATCH (m:Movie) RETURN m",
+        });
+    });
+});
