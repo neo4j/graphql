06507a6d52f2509337850382428a9b250b99ae2f
Daniel Starns <danielstarns@hotmail.com>
2020-10-17 12:50:03 +0100

    feat: add upper first letter

--- packages/graphql/src/utils/index.ts
+++ packages/graphql/src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as trimmer } from "./trimmer";
 export { default as execute } from "./execute";
 export { default as deserialize } from "./deserialize";
 export { default as serialize } from "./serialize";
+export { default as upperFirstLetter } from "./upper-first-letter";
---
+++ packages/graphql/tests/unit/utils/upper-first-letter.unit.test.ts
@@ -0,0 +1,11 @@
+import upperFirstLetter from "../../../src/utils/upper-first-letter";
+
+describe("upperFirstLetter", () => {
+    test("should uppercase the first letter and return the word", () => {
+        const initial = "movie";
+
+        const result = upperFirstLetter(initial);
+
+        expect(result).toEqual("Movie");
+    });
+});
