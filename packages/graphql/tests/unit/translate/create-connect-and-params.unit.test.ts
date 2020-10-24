0fee80e6e397d982e4e8bd8894ffd05b8694ccf8
Jared Kozak <jared@kozan.dev>
2020-10-24 20:24:13 +0100

    refactor: createConnectAndParams into own file

---
+++ packages/graphql/tests/unit/translate/create-connect-and-params.unit.test.ts
@@ -0,0 +1,63 @@
+import createConnectAndParams from "../../../src/translate/create-connect-and-params";
+import { NeoSchema, Node } from "../../../src/classes";
+import { trimmer } from "../../../src/utils";
+
+describe("createConnectAndParams", () => {
+    test("should return the correct connection", () => {
+        // @ts-ignore
+        const node: Node = {
+            name: "Movie",
+            relationFields: [
+                {
+                    direction: "OUT",
+                    type: "SIMILAR",
+                    fieldName: "similarMovies",
+                    typeMeta: {
+                        name: "Movie",
+                        array: true,
+                        required: false,
+                        pretty: "[Movies]",
+                    },
+                    otherDirectives: [],
+                    arguments: [],
+                },
+            ],
+            cypherFields: [],
+        };
+
+        // @ts-ignore
+        const neoSchema: NeoSchema = {
+            nodes: [node],
+        };
+
+        const result = createConnectAndParams({
+            withVars: ["this"],
+            value: [{ where: { title: "abc" }, connect: { similarMovies: [{ where: { title: "cba" } }] } }],
+            varName: "this",
+            relationField: node.relationFields[0],
+            parentVar: "this",
+            neoSchema,
+            parentNode: node,
+            refNode: node,
+        });
+
+        expect(trimmer(result[0])).toEqual(
+            trimmer(`
+                WITH this
+                OPTIONAL MATCH (this0:Movie)
+                WHERE this0.title = $this0_title 
+                FOREACH(_ IN CASE [] WHEN NULL THEN [] ELSE [1] END | MERGE (this)-[:SIMILAR]->(this0) )
+
+                WITH this, this0 
+                OPTIONAL MATCH (this0_similarMovies0:Movie) 
+                WHERE this0_similarMovies0.title = $this0_similarMovies0_title 
+                FOREACH(_ IN CASE [] WHEN NULL THEN [] ELSE [1] END | MERGE (this0)-[:SIMILAR]->(this0_similarMovies0) )
+            `)
+        );
+
+        expect(result[1]).toMatchObject({
+            this0_title: "abc",
+            this0_similarMovies0_title: "cba",
+        });
+    });
+});
