f53a6fbb4661c036157008ae435581fbfe3a32e1
Jared Kozak <jared@kozan.dev>
2020-11-06 14:30:00 +0000

    feat: first pass of and and or with relationship filtering

--- packages/graphql/src/schema/get-auth.ts
+++ packages/graphql/src/schema/get-auth.ts
@@ -1,5 +1,6 @@
-import { DirectiveNode } from "graphql";
-import { Auth, AuthRule, AuthOperations, AuthConstructor } from "../classes";
+/* eslint-disable no-inner-declarations */
+import { DirectiveNode, valueFromASTUntyped } from "graphql";
+import { Auth, AuthRule, AuthConstructor } from "../classes";
 
 function getAuth(directive: DirectiveNode): Auth {
     const authConstructor: AuthConstructor = { rules: [] };
@@ -14,103 +15,56 @@ function getAuth(directive: DirectiveNode): Auth {
         throw new Error("auth rules must be a ListValue");
     }
 
-    rules.value.values.forEach((value, i) => {
-        const rule: AuthRule = {};
+    const parsedRules = valueFromASTUntyped(rules.value) as AuthRule[];
 
-        if (value.kind !== "ObjectValue") {
-            throw new Error(`rules[${i}] must be a ObjectValue`);
+    parsedRules.forEach((rule, index) => {
+        const valueIsObject = Boolean(!Array.isArray(rule) && Object.keys(rule).length && typeof rule !== "string");
+        if (!valueIsObject) {
+            throw new Error(`rules[${index}] must be a ObjectValue`);
         }
 
-        const operations = value.fields.find((x) => x.name.value === "operations");
-
-        if (!operations) {
-            throw new Error(`rules[${i}].operations required`);
+        if (!rule.operations) {
+            throw new Error(`rules[${index}].operations required`);
         }
 
-        if (operations.value.kind !== "ListValue") {
-            throw new Error(`rules[${i}].operations must be a ListValue`);
+        if (!Array.isArray(rule.operations)) {
+            throw new Error(`rules[${index}].operations must be a ListValue`);
         }
 
-        if (!operations.value.values.length) {
-            throw new Error(`rules[${i}].operations cant be empty`);
+        if (!rule.operations.length) {
+            throw new Error(`rules[${index}].operations cant be empty`);
         }
 
-        operations.value.values.forEach((v, ii) => {
-            if (v.kind !== "StringValue") {
-                throw new Error(`rules[${i}].operations[${ii}] invalid`);
+        rule.operations.forEach((op, opIndex) => {
+            if (typeof op !== "string") {
+                throw new Error(`rules[${index}].operations[${opIndex}] invalid`);
             }
 
-            if (!["create", "read", "update", "delete"].includes(v.value)) {
-                throw new Error(`rules[${i}].operations[${ii}] invalid`);
+            if (!["create", "read", "update", "delete"].includes(op)) {
+                throw new Error(`rules[${index}].operations[${opIndex}] invalid`);
             }
-
-            if (!rule.operations) {
-                rule.operations = [];
-            }
-
-            const operation = (v.value as unknown) as AuthOperations;
-            rule.operations.push(operation);
         });
 
-        const isAuthenticated = value.fields.find((x) => x.name.value === "isAuthenticated");
-        if (isAuthenticated) {
-            if (isAuthenticated.value.kind !== "BooleanValue") {
-                throw new Error(`rules[${i}].isAuthenticated must be a boolean`);
-            }
-
-            rule.isAuthenticated = Boolean(isAuthenticated.value.value);
-        }
-
-        const allow = value.fields.find((x) => x.name.value === "allow");
-        if (allow) {
-            if (!["ObjectValue", "StringValue"].includes(allow.value.kind)) {
-                throw new Error(`rules[${i}].allow must be a ObjectValue or StringValue`);
-            }
-
-            if (allow.value.kind === "ObjectValue") {
-                allow.value.fields.forEach((field) => {
-                    if (field.value.kind !== "StringValue") {
-                        throw new Error(`rules[${i}].allow[${field.name.value}] must be a string`);
-                    }
-
-                    if (!rule.allow) {
-                        rule.allow = {};
-                    }
-
-                    rule.allow[field.name.value] = field.value.value;
-                });
-            }
-
-            if (allow.value.kind === "StringValue") {
-                if (allow.value.value !== "*") {
-                    throw new Error(`rules[${i}].allow invalid StringValue`);
-                }
-
-                rule.allow = "*";
+        if (rule.isAuthenticated) {
+            if (typeof rule.isAuthenticated !== "boolean") {
+                throw new Error(`rules[${index}].isAuthenticated must be a BooleanValue`);
             }
         }
 
-        const roles = value.fields.find((x) => x.name.value === "roles");
-        if (roles) {
-            if (roles.value.kind !== "ListValue") {
-                throw new Error(`rules[${i}].roles must be a ListValue`);
+        if (rule.roles) {
+            if (!Array.isArray(rule.roles)) {
+                throw new Error(`rules[${index}].roles must be a ListValue`);
             }
 
-            roles.value.values.forEach((v, ii) => {
-                if (v.kind !== "StringValue") {
-                    throw new Error(`rules[${i}].roles[${ii}] must be a StringValue`);
+            rule.roles.forEach((role, i) => {
+                if (typeof role !== "string") {
+                    throw new Error(`rules[${index}].roles[${i}] must be a StringValue`);
                 }
-
-                if (!rule.roles) {
-                    rule.roles = [];
-                }
-
-                rule.roles.push(v.value);
             });
         }
-
-        authConstructor.rules.push(rule);
     });
+
+    authConstructor.rules = parsedRules;
 
     return new Auth(authConstructor);
 }
