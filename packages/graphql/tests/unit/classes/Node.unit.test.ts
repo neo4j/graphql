d078ec4e507b425bf6c975a4169f7d2dfd8216b7
Jared Kozak <jared@kozan.dev>
2021-01-19 14:26:28 +0000

    test: fix missing property

--- packages/graphql/tests/unit/classes/Node.unit.test.ts
+++ packages/graphql/tests/unit/classes/Node.unit.test.ts
@@ -11,6 +11,7 @@ describe("Node", () => {
             enumFields: [],
             primitiveFields: [],
             scalarFields: [],
+            dateTimeFields: [],
         };
 
         // @ts-ignore
