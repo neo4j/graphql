7a787091772e3a04317d0c1531b799f0afddb0ee
Jared Kozak <jared@kozan.dev>
2021-03-02 16:52:23 +0000

    Ensure Prettier is properly configured in ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,8 +38,8 @@ module.exports = {
                 "airbnb-typescript/base",
                 "plugin:@typescript-eslint/recommended",
                 "plugin:@typescript-eslint/recommended-requiring-type-checking",
-                "prettier",
-                "prettier/@typescript-eslint",
+                "prettier/@typescript-eslint", // Disables ESLint rules from @typescript-eslint/eslint-plugin that conflict with Prettier
+                "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier - ensure this is always last in the extends array
             ],
             rules: {
                 "no-underscore-dangle": "off",
@@ -65,19 +65,15 @@ module.exports = {
                 ],
                 "import/prefer-default-export": [0],
                 "prefer-destructuring": "off",
-                // Below to silence @typescript-eslint/recommended-requiring-type-checking
-                // Approximately 1100 instances
-                "@typescript-eslint/no-unsafe-assignment": "off",
-                // Approximately 700 instances
-                "@typescript-eslint/no-unsafe-member-access": "off",
-                // Approximately 500 instances
-                "@typescript-eslint/no-unsafe-call": "off",
-                // Approximately 350 instances
-                "@typescript-eslint/restrict-template-expressions": "off",
-                // Approximately 70 instances
-                "@typescript-eslint/no-unsafe-return": "off",
-                // Apollo createTestClient unhappy with this switched on
-                "@typescript-eslint/unbound-method": "off",
+                // @typescript-eslint/recommended-requiring-type-checking rules which require large refactor
+                // START BLOCK
+                "@typescript-eslint/no-unsafe-assignment": "off", // Approximately 1100 instances
+                "@typescript-eslint/no-unsafe-member-access": "off", // Approximately 700 instances
+                "@typescript-eslint/no-unsafe-call": "off", // Approximately 500 instances
+                "@typescript-eslint/restrict-template-expressions": "off", // Approximately 350 instances
+                "@typescript-eslint/no-unsafe-return": "off", // Approximately 70 instances
+                "@typescript-eslint/unbound-method": "off", // Apollo createTestClient unhappy with this switched on
+                // END BLOCK
             },
             settings: {
                 "import/resolver": {
