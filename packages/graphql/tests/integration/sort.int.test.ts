5e780eb20cfd564ec790466654e5a926af092504
Daniel Starns <danielstarns@hotmail.com>
2021-03-04 18:31:08 +0000

    test: add coverage for both directions

--- packages/graphql/tests/integration/sort.int.test.ts
+++ packages/graphql/tests/integration/sort.int.test.ts
@@ -16,118 +16,141 @@ describe("sort", () => {
     });
 
     test("should sort a list of nodes", async () => {
-        const session = driver.session();
+        await Promise.all(
+            ["ASC", "DESC"].map(async (type) => {
+                const session = driver.session();
 
-        const typeDefs = `
-            type Movie {
-                id: ID
-                number: Int
-            }
-        `;
+                const typeDefs = `
+                    type Movie {
+                        id: ID
+                        number: Int
+                    }
+                `;
 
-        const neoSchema = new Neo4jGraphQL({ typeDefs });
+                const neoSchema = new Neo4jGraphQL({ typeDefs });
 
-        const movieIds = [
-            generate({
-                charset: "alphabetic",
-            }),
-            generate({
-                charset: "alphabetic",
-            }),
-            generate({
-                charset: "alphabetic",
-            }),
-        ].map((x) => `"${x}"`);
+                const movieIds = [
+                    generate({
+                        charset: "alphabetic",
+                    }),
+                    generate({
+                        charset: "alphabetic",
+                    }),
+                    generate({
+                        charset: "alphabetic",
+                    }),
+                ].map((x) => `"${x}"`);
 
-        const query = `
-            query {
-                movies(
-                    where: { id_IN: [${movieIds.join(",")}] },
-                    options: { sort: [number_ASC] }
-                ) {
-                   number
+                const query = `
+                    query {
+                        movies(
+                            where: { id_IN: [${movieIds.join(",")}] },
+                            options: { sort: [number_${type}] }
+                        ) {
+                           number
+                        }
+                    }
+                `;
+
+                try {
+                    await session.run(`
+                        CREATE (:Movie {id: ${movieIds[0]}, number: 1})
+                        CREATE (:Movie {id: ${movieIds[1]}, number: 2})
+                        CREATE (:Movie {id: ${movieIds[2]}, number: 3})
+                    `);
+
+                    const gqlResult = await graphql({
+                        schema: neoSchema.schema,
+                        source: query,
+                        contextValue: { driver },
+                    });
+
+                    expect(gqlResult.errors).toEqual(undefined);
+
+                    const movies = (gqlResult.data as any).movies;
+
+                    if (type === "ASC") {
+                        expect(movies[0].number).toEqual(1);
+                        expect(movies[1].number).toEqual(2);
+                        expect(movies[2].number).toEqual(3);
+                    }
+
+                    if (type === "DESC") {
+                        expect(movies[0].number).toEqual(3);
+                        expect(movies[1].number).toEqual(2);
+                        expect(movies[2].number).toEqual(1);
+                    }
+                } finally {
+                    await session.close();
                 }
-            }
-        `;
-
-        try {
-            await session.run(`
-                CREATE (:Movie {id: ${movieIds[0]}, number: 1})
-                CREATE (:Movie {id: ${movieIds[1]}, number: 2})
-                CREATE (:Movie {id: ${movieIds[2]}, number: 3})
-            `);
-
-            const gqlResult = await graphql({
-                schema: neoSchema.schema,
-                source: query,
-                contextValue: { driver },
-            });
-
-            expect(gqlResult.errors).toEqual(undefined);
-
-            const movies = (gqlResult.data as any).movies;
-
-            expect(movies[0].number).toEqual(1);
-            expect(movies[1].number).toEqual(2);
-            expect(movies[2].number).toEqual(3);
-        } finally {
-            await session.close();
-        }
+            })
+        );
     });
 
     test("should sort nested relationships", async () => {
-        const session = driver.session();
+        await Promise.all(
+            ["ASC", "DESC"].map(async (type) => {
+                const session = driver.session();
 
-        const typeDefs = `
-            type Movie {
-                id: ID
-                genres: [Genre] @relationship(type: "HAS_GENRE", direction: "OUT")
-            }
-            
-            type Genre {
-                id: ID
-            }
-        `;
-
-        const neoSchema = new Neo4jGraphQL({ typeDefs });
-
-        const movieId = generate({
-            charset: "alphabetic",
-        });
-
-        const query = `
-            query {
-                movies(where: { id: "${movieId}" }) {
-                    genres(options: { sort: [id_ASC] }) {
-                        id
+                const typeDefs = `
+                    type Movie {
+                        id: ID
+                        genres: [Genre] @relationship(type: "HAS_GENRE", direction: "OUT")
                     }
+
+                    type Genre {
+                        id: ID
+                    }
+                `;
+
+                const neoSchema = new Neo4jGraphQL({ typeDefs });
+
+                const movieId = generate({
+                    charset: "alphabetic",
+                });
+
+                const query = `
+                    query {
+                        movies(where: { id: "${movieId}" }) {
+                            genres(options: { sort: [id_${type}] }) {
+                                id
+                            }
+                        }
+                    }
+                `;
+
+                try {
+                    await session.run(`
+                        CREATE (m:Movie {id: "${movieId}"})
+                        CREATE (g1:Genre {id: "1"})
+                        CREATE (g2:Genre {id: "2"})
+                        MERGE (m)-[:HAS_GENRE]->(g1)
+                        MERGE (m)-[:HAS_GENRE]->(g2)
+                    `);
+
+                    const gqlResult = await graphql({
+                        schema: neoSchema.schema,
+                        source: query,
+                        contextValue: { driver },
+                    });
+
+                    expect(gqlResult.errors).toEqual(undefined);
+
+                    const genres = (gqlResult.data as any).movies[0].genres;
+
+                    if (type === "ASC") {
+                        expect(genres[0].id).toEqual("1");
+                        expect(genres[1].id).toEqual("2");
+                    }
+
+                    if (type === "DESC") {
+                        expect(genres[0].id).toEqual("2");
+                        expect(genres[1].id).toEqual("1");
+                    }
+                } finally {
+                    await session.close();
                 }
-            }
-        `;
-
-        try {
-            await session.run(`
-                CREATE (m:Movie {id: "${movieId}"})
-                CREATE (g1:Genre {id: "1"})
-                CREATE (g2:Genre {id: "2"})
-                MERGE (m)-[:HAS_GENRE]->(g1)
-                MERGE (m)-[:HAS_GENRE]->(g2)
-            `);
-
-            const gqlResult = await graphql({
-                schema: neoSchema.schema,
-                source: query,
-                contextValue: { driver },
-            });
-
-            expect(gqlResult.errors).toEqual(undefined);
-
-            const genres = (gqlResult.data as any).movies[0].genres;
-
-            expect(genres[0].id).toEqual("1");
-            expect(genres[1].id).toEqual("2");
-        } finally {
-            await session.close();
-        }
+            })
+        );
     });
 });
