6f88378c73ec4a3d3d500246f1741c6546319390
Jared Kozak <jared@kozan.dev>
2021-03-24 11:35:20 +0000

    fix: remove conflicting server port neo-push

--- examples/neo-push/server/src/config.ts
+++ examples/neo-push/server/src/config.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const HTTP_PORT = Number(process.env.HTTP_PORT || 4000);
+export const HTTP_PORT = Number(process.env.HTTP_PORT || 5000);
 export const NODE_ENV = process.env.NODE_ENV as string;
 export const NEO_USER: string = (process.env.NEO_USER || "admin") as string;
 export const NEO_PASSWORD: string = (process.env.NEO_PASSWORD || "password") as string;
