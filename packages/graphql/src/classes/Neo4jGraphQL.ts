960d92c08e7085743e4b758e3ed341c84427ebc2
Darrell Warde <darrell.warde@neotechnology.com>
2021-03-26 10:54:05 +0000

    Move addSchemaLevelResolver into Neo4jGraphQL

--- packages/graphql/src/classes/Neo4jGraphQL.ts
+++ packages/graphql/src/classes/Neo4jGraphQL.ts
@@ -1,7 +1,7 @@
 import { Driver } from "neo4j-driver";
 import { DocumentNode, GraphQLSchema, parse, printSchema } from "graphql";
 import { ITypeDefinitions, IResolvers } from "@graphql-tools/utils";
-import { IExecutableSchemaDefinition } from "@graphql-tools/schema";
+import { addSchemaLevelResolver, IExecutableSchemaDefinition } from "@graphql-tools/schema";
 import { DriverConfig } from "../types";
 import { makeAugmentedSchema } from "../schema";
 import Node from "./Node";
@@ -14,7 +14,6 @@ export interface Neo4jGraphQLConstructor {
     resolvers?: IResolvers;
     schemaDirectives?: SchemaDirectives;
     debug?: boolean | ((...values: any[]) => void);
-    context?: { [k: string]: any } & { driver?: Driver };
     driver?: Driver;
     driverConfig?: DriverConfig;
 }
@@ -32,9 +31,36 @@ class Neo4jGraphQL {
         this.input = input;
 
         const { nodes, schema } = makeAugmentedSchema(this);
+
         this.nodes = nodes;
-        this.schema = schema;
+        this.schema = this.createWrappedSchema(schema, input.driver, input.driverConfig);
         this.document = parse(printSchema(schema));
+    }
+
+    private createWrappedSchema(schema: GraphQLSchema, driver?: Driver, driverConfig?: DriverConfig): GraphQLSchema {
+        return addSchemaLevelResolver(schema, (_obj, _args, context: any, info: any) => {
+            /*
+                Deleting this property ensures that we call this function more than once,
+                See https://github.com/ardatan/graphql-tools/issues/353#issuecomment-499569711
+            */
+            // eslint-disable-next-line no-param-reassign,no-underscore-dangle
+            delete info.operation.__runAtMostOnce;
+
+            if (!context?.driver) {
+                if (!driver) {
+                    throw new Error(
+                        "A Neo4j driver instance must either be passed to Neo4jGraphQL on construction, or passed as context.driver in each request."
+                    );
+                }
+                context.driver = driver;
+            }
+
+            if (!context?.driverConfig) {
+                context.driverConfig = driverConfig;
+            }
+
+            context.neoSchema = this;
+        });
     }
 
     debug(message: string) {
--- packages/graphql/src/schema/make-augmented-schema.ts
+++ packages/graphql/src/schema/make-augmented-schema.ts
@@ -1,6 +1,5 @@
 import { mergeTypeDefs } from "@graphql-tools/merge";
-import { makeExecutableSchema, addSchemaLevelResolver } from "@graphql-tools/schema";
-import { IResolvers } from "@graphql-tools/utils";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import camelCase from "camelcase";
 import {
     DefinitionNode,
@@ -37,9 +36,7 @@ import * as point from "./point";
 import { graphqlDirectivesToCompose, objectFieldsToComposeFields } from "./to-compose";
 import validateTypeDefs from "./validation";
 
-function makeAugmentedSchema(
-    neoSchema: Neo4jGraphQL
-): { typeDefs: string; resolvers: IResolvers; schema: GraphQLSchema; nodes: Node[] } {
+function makeAugmentedSchema(neoSchema: Neo4jGraphQL): { schema: GraphQLSchema; nodes: Node[] } {
     const document = mergeTypeDefs(
         Array.isArray(neoSchema.input.typeDefs)
             ? (neoSchema.input.typeDefs as string[])
@@ -742,35 +739,9 @@ function makeAugmentedSchema(
         schemaDirectives: neoSchema.input.schemaDirectives,
     });
 
-    const newSchema = addSchemaLevelResolver(schema, (_obj, _args, context: any, info: any) => {
-        /* 
-            Deleting this property ensures that we call this function more than once,
-            See https://github.com/ardatan/graphql-tools/issues/353#issuecomment-499569711
-        */
-        // eslint-disable-next-line no-param-reassign,no-underscore-dangle
-        delete info.operation.__runAtMostOnce;
-
-        if (neoSchema.input.context) {
-            context = { ...context, ...neoSchema.input.context };
-        }
-
-        if (!context?.driver) {
-            if (!neoSchema.input.driver) {
-                throw new Error(
-                    "A Neo4j driver instance must either be passed to Neo4jGraphQL on construction, or passed as context.driver in each request."
-                );
-            }
-            context.driver = neoSchema.input.driver;
-        }
-
-        context.neoSchema = neoSchema;
-    });
-
     return {
         nodes,
-        typeDefs: generatedTypeDefs,
-        resolvers: generatedResolvers,
-        schema: newSchema,
+        schema,
     };
 }
 
