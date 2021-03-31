7b5d434c8d96e5fb7f37a544d54ceeecb420f9fc
Darrell Warde <darrell.warde@neotechnology.com>
2021-03-31 13:58:44 +0100

    Fix package tests

--- packages/package-tests/typescript/src/test.ts
+++ packages/package-tests/typescript/src/test.ts
@@ -19,15 +19,16 @@
 
 // @ts-ignore
 import { Neo4jGraphQL } from "@neo4j/graphql";
+import { printSchema } from "graphql";
 
 // Augment schema with simple typeDefs input
 const typeDefs = `type Movie{ id: ID!}`;
 const neoSchema = new Neo4jGraphQL({ typeDefs });
 
 // A "Movies" query should have been generated
-const generatedTypeDefsMatch = /Movies/;
+const generatedTypeDefsMatch = /movies/;
 
 // If not, throw to exit process with 1 and include stack trace
-if (!generatedTypeDefsMatch.test(neoSchema.typeDefs)) {
+if (!generatedTypeDefsMatch.test(printSchema(neoSchema.schema))) {
     throw new Error(`${generatedTypeDefsMatch} was not found in generated typeDefs`);
 }
