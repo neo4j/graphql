1adf41bded8eb26fa7184bd49e26de4696c9e9d1
Daniel Starns <danielstarns@hotmail.com>
2021-04-08 11:06:34 +0100

    refactor: prefix env vars with NEO4J_GRAPHQL (#131)
    * refactor: prefix env vars with NEO4J_GRAPHQL
    
    * refactor: uppercase J

--- examples/neo-push/server/src/utils/create-jwt.ts
+++ examples/neo-push/server/src/utils/create-jwt.ts
@@ -3,7 +3,7 @@ import * as config from "../config";
 
 function createJWT(data: { sub: string }): Promise<string> {
     return new Promise((resolve, reject) => {
-        jwt.sign(data, config.JWT_SECRET, (err, token) => {
+        jwt.sign(data, config.NEO4J_GRAPHQL_JWT_SECRET, (err, token) => {
             if (err) {
                 return reject(err);
             }
