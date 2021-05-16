85001a80ca3ea1e728f5c338e0f61923fa85e837
Zaid Daghestani <zaid.daghestani@gmail.com>
2021-05-16 02:38:43 -0700

    Use neo4j int for limit and skip

--- packages/graphql/src/translate/translate-read.ts
+++ packages/graphql/src/translate/translate-read.ts
@@ -23,6 +23,7 @@ import createProjectionAndParams from "./create-projection-and-params";
 import { GraphQLWhereArg, GraphQLOptionsArg, GraphQLSortArg, Context } from "../types";
 import createAuthAndParams from "./create-auth-and-params";
 import { AUTH_FORBIDDEN_ERROR } from "../constants";
+import neo4j from 'neo4j-driver'
 
 function translateRead({ node, context }: { context: Context; node: Node }): [string, any] {
     const { resolveTree } = context;
@@ -105,12 +106,12 @@ function translateRead({ node, context }: { context: Context; node: Node }): [st
 
         if (hasSkip) {
             skipStr = `SKIP $${varName}_skip`;
-            cypherParams[`${varName}_skip`] = optionsInput.skip;
+            cypherParams[`${varName}_skip`] = neo4j.int(optionsInput.skip);
         }
 
         if (hasLimit) {
             limitStr = `LIMIT $${varName}_limit`;
-            cypherParams[`${varName}_limit`] = optionsInput.limit;
+            cypherParams[`${varName}_limit`] = neo4j.int(optionsInput.limit);
         }
 
         if (optionsInput.sort && optionsInput.sort.length) {
