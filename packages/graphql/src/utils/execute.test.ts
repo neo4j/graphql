c9a466e34c61fe2dd23b037b03b2f38399cf9abe
Jared Kozak <jared@kozan.dev>
2021-07-21 11:18:03 +0100

    Allow for the optional setting of Cypher query options

--- packages/graphql/src/utils/execute.test.ts
+++ packages/graphql/src/utils/execute.test.ts
@@ -19,9 +19,20 @@
 
 import { Driver } from "neo4j-driver";
 import { Neo4jGraphQL } from "../classes";
-import { Context } from "../types";
+import {
+    Context,
+    CypherConnectComponentsPlanner,
+    CypherExpressionEngine,
+    CypherInterpretedPipesFallback,
+    CypherOperatorEngine,
+    CypherPlanner,
+    CypherReplanning,
+    CypherRuntime,
+    CypherUpdateStrategy,
+} from "../types";
 import execute from "./execute";
 import environment from "../environment";
+import { trimmer } from ".";
 
 describe("execute", () => {
     test("should execute return records.toObject", async () => {
@@ -93,5 +104,166 @@ describe("execute", () => {
                 );
             })
         );
+    });
+
+    describe("should set query options", () => {
+        test("no query options if object is empty", async () => {
+            const defaultAccessMode = "READ";
+
+            const cypher = trimmer(`
+                CREATE (u:User {title: $title})
+                RETURN u { .title } as u
+            `);
+
+            const title = "some title";
+            const params = { title };
+            const records = [{ toObject: () => ({ title }) }];
+            const database = "neo4j";
+            const bookmarks = ["test"];
+
+            // @ts-ignore
+            const driver: Driver = {
+                // @ts-ignore
+                session: (options) => {
+                    expect(options).toMatchObject({ defaultAccessMode, database, bookmarks });
+
+                    const tx = {
+                        run: (paramCypher, paramParams) => {
+                            expect(trimmer(paramCypher)).toEqual(cypher);
+                            expect(paramParams).toEqual(params);
+
+                            return { records };
+                        },
+                    };
+
+                    return {
+                        readTransaction: (fn) => {
+                            // @ts-ignore
+                            return fn(tx);
+                        },
+                        writeTransaction: (fn) => {
+                            // @ts-ignore
+                            return fn(tx);
+                        },
+                        close: () => true,
+                    };
+                },
+                // @ts-ignore
+                _config: {},
+            };
+
+            // @ts-ignore
+            const neoSchema: Neo4jGraphQL = {
+                // @ts-ignore
+                options: {},
+            };
+
+            const result = await execute({
+                cypher,
+                params,
+                defaultAccessMode,
+                context: {
+                    driverConfig: { database, bookmarks },
+                    neoSchema,
+                    driver,
+                    queryOptions: {},
+                } as Context,
+            });
+
+            expect(result).toEqual([{ title }]);
+            // @ts-ignore
+            expect(driver._userAgent).toEqual(`${environment.NPM_PACKAGE_NAME}/${environment.NPM_PACKAGE_VERSION}`);
+            // @ts-ignore
+            expect(driver._config.userAgent).toEqual(
+                `${environment.NPM_PACKAGE_NAME}/${environment.NPM_PACKAGE_VERSION}`
+            );
+        });
+
+        test("one of each query option", async () => {
+            const defaultAccessMode = "READ";
+
+            const inputCypher = trimmer(`
+            CREATE (u:User {title: $title})
+            RETURN u { .title } as u
+        `);
+
+            const expectedCypher = trimmer(`
+            CYPHER runtime=interpreted planner=cost connectComponentsPlanner=greedy updateStrategy=default expressionEngine=compiled operatorEngine=compiled interpretedPipesFallback=all replan=default
+            CREATE (u:User {title: $title})
+            RETURN u { .title } as u
+        `);
+
+            const title = "some title";
+            const params = { title };
+            const records = [{ toObject: () => ({ title }) }];
+            const database = "neo4j";
+            const bookmarks = ["test"];
+
+            // @ts-ignore
+            const driver: Driver = {
+                // @ts-ignore
+                session: (options) => {
+                    expect(options).toMatchObject({ defaultAccessMode, database, bookmarks });
+
+                    const tx = {
+                        run: (paramCypher, paramParams) => {
+                            expect(trimmer(paramCypher)).toEqual(expectedCypher);
+                            expect(paramParams).toEqual(params);
+
+                            return { records };
+                        },
+                    };
+
+                    return {
+                        readTransaction: (fn) => {
+                            // @ts-ignore
+                            return fn(tx);
+                        },
+                        writeTransaction: (fn) => {
+                            // @ts-ignore
+                            return fn(tx);
+                        },
+                        close: () => true,
+                    };
+                },
+                // @ts-ignore
+                _config: {},
+            };
+
+            // @ts-ignore
+            const neoSchema: Neo4jGraphQL = {
+                // @ts-ignore
+                options: {},
+            };
+
+            const result = await execute({
+                cypher: inputCypher,
+                params,
+                defaultAccessMode,
+                context: {
+                    driverConfig: { database, bookmarks },
+                    neoSchema,
+                    driver,
+                    queryOptions: {
+                        runtime: CypherRuntime.INTERPRETED,
+                        planner: CypherPlanner.COST,
+                        connectComponentsPlanner: CypherConnectComponentsPlanner.GREEDY,
+                        updateStrategy: CypherUpdateStrategy.DEFAULT,
+                        expressionEngine: CypherExpressionEngine.COMPILED,
+                        operatorEngine: CypherOperatorEngine.COMPILED,
+                        interpretedPipesFallback: CypherInterpretedPipesFallback.ALL,
+                        replan: CypherReplanning.DEFAULT,
+                    },
+                } as Context,
+            });
+
+            expect(result).toEqual([{ title }]);
+            // @ts-ignore
+            expect(driver._userAgent).toEqual(`${environment.NPM_PACKAGE_NAME}/${environment.NPM_PACKAGE_VERSION}`);
+            // @ts-ignore
+            expect(driver._config.userAgent).toEqual(
+                `${environment.NPM_PACKAGE_NAME}/${environment.NPM_PACKAGE_VERSION}`
+            );
+        });
     });
 });
