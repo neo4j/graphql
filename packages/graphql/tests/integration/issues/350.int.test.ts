bf0a65b73bebf0fa670dfcf8e02b8646e97cc032
dmoree <dmoree@users.noreply.github.com>
2021-07-26 02:03:07 -0700

    fix: alias tck test add: tck test related to issue#350 add: integration test related to issue#350

---
+++ packages/graphql/tests/integration/issues/350.int.test.ts
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Driver } from "neo4j-driver";
+import { graphql } from "graphql";
+import { gql } from "apollo-server";
+import { generate } from "randomstring";
+import neo4j from "../neo4j";
+import { Neo4jGraphQL } from "../../../src/classes";
+
+describe("https://github.com/neo4j/graphql/issues/350", () => {
+    let driver: Driver;
+
+    beforeAll(async () => {
+        driver = await neo4j();
+        const session = driver.session();
+        try {
+            await session.run(`
+                MATCH (n)
+                DETACH DELETE n
+            `);
+        } finally {
+            await session.close();
+        }
+    });
+
+    afterAll(async () => {
+        await driver.close();
+    });
+
+    test("Retain attributes when aliasing the same field multiple times in a single query", async () => {
+        const session = driver.session();
+        const typeDefs = gql`
+            type Post {
+                id: ID!
+                title: String!
+                content: String!
+                comments: [Comment!]! @relationship(type: "HAS_COMMENT", direction: OUT)
+            }
+            type Comment {
+                id: ID!
+                flagged: Boolean!
+                content: String!
+                post: Post! @relationship(type: "HAS_COMMENT", direction: IN)
+                canEdit: Boolean! @cypher(statement: "RETURN false")
+            }
+        `;
+
+        const neoSchema = new Neo4jGraphQL({ typeDefs });
+
+        const postId = generate({
+            charset: "alphabetic",
+        });
+
+        const postTitle = generate({
+            charset: "alphabetic",
+        });
+
+        const postContent = generate({
+            charset: "alphabetic",
+        });
+
+        const comment1Id = generate({
+            charset: "alphabetic",
+        });
+
+        const comment1Content = generate({
+            charset: "alphabetic",
+        });
+
+        const comment2Id = generate({
+            charset: "alphabetic",
+        });
+
+        const comment2Content = generate({
+            charset: "alphabetic",
+        });
+
+        const query = `
+            query {
+                posts {
+                    flaggedComments: comments(where: { flagged: true }) {
+                        content
+                        flagged
+                    }
+                    unflaggedComments: comments(where: {flagged: false}) {
+                        content
+                        flagged
+                    }
+                }
+            }
+        `;
+
+        try {
+            await session.run(
+                `  
+                    CREATE (post:Post {id: $postId, title: $postTitle, content: $postContent})
+                    CREATE (comment1:Comment {id: $comment1Id, content: $comment1Content, flagged: true})
+                    CREATE (comment2:Comment {id: $comment2Id, content: $comment2Content, flagged: false})
+                    MERGE (post)-[:HAS_COMMENT]->(comment1)
+                    MERGE (post)-[:HAS_COMMENT]->(comment2)
+
+                `,
+                {
+                    postId,
+                    postTitle,
+                    postContent,
+                    comment1Id,
+                    comment1Content,
+                    comment2Id,
+                    comment2Content,
+                }
+            );
+
+            const result = await graphql({
+                schema: neoSchema.schema,
+                source: query,
+                contextValue: { driver },
+            });
+            expect(result.errors).toBeFalsy();
+            expect(result?.data?.posts[0].flaggedComments).toContainEqual({
+                content: comment1Content,
+                flagged: true,
+            });
+            expect(result?.data?.posts[0].unflaggedComments).toContainEqual({
+                content: comment2Content,
+                flagged: false,
+            });
+        } finally {
+            await session.close();
+        }
+    });
+});
--- packages/graphql/tests/tck/tck-test-files/cypher/alias.md
+++ packages/graphql/tests/tck/tck-test-files/cypher/alias.md
@@ -6,13 +6,13 @@ Schema:
 
 ```schema
 type Actor {
-    name: String
+    name: String!
 }
 
 type Movie {
     id: ID
-    actors: [Actor] @relationship(type: "ACTED_IN", direction: IN)
-    custom: [Movie] @cypher(statement: """
+    actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
+    custom: [Movie!]! @cypher(statement: """
         MATCH (m:Movie)
         RETURN m
     """)
@@ -44,9 +44,9 @@ type Movie {
 ```cypher
 MATCH (this:Movie)
 RETURN this {
-    .id,
-    actors: [ (this)<-[:ACTED_IN]-(this_actors:Actor) | this_actors { .name } ],
-    custom: [this_custom IN apoc.cypher.runFirstColumn("MATCH (m:Movie) RETURN m", {this: this, auth: $auth}, true) | this_custom { .id }]
+    movieId: this.id,
+    actors: [ (this)<-[:ACTED_IN]-(this_actors:Actor) | this_actors { aliasActorsName: this_actors.name } ],
+    custom: [this_custom IN apoc.cypher.runFirstColumn("MATCH (m:Movie) RETURN m", {this: this, auth: $auth}, true) | this_custom { aliasCustomId: this_custom.id }]
 } as this
 ```
 
@@ -59,5 +59,48 @@ RETURN this {
        "roles": [],
        "jwt": {}
     }
+}
+```
+
+### Multiple aliases for single field with arguments
+
+**GraphQL input**
+
+```graphql
+{
+    movies {
+        id
+        keanu: actors(where: { name: "Keanu" }) {
+            name
+        }
+        carrie: actors(where: { name: "Carrie" }) {
+            name
+        }
+    }
+}
+```
+
+**Expected Cypher output**
+
+```cypher
+MATCH (this:Movie)
+RETURN this {
+    .id,
+    keanu: [ (this)<-[:ACTED_IN]-(this_keanu:Actor)  WHERE this_keanu.name = $this_keanu_name | this_keanu { .name } ],
+    carrie: [ (this)<-[:ACTED_IN]-(this_carrie:Actor)  WHERE this_carrie.name = $this_carrie_name | this_carrie { .name } ],
+} as this
+```
+
+**Expected Cypher params**
+
+```cypher-params
+{
+    "auth": {
+       "isAuthenticated": true,
+       "roles": [],
+       "jwt": {}
+    },
+    "this_keanu_name": "Keanu",
+    "this_carrie_name": "Carrie"
 }
 ```
