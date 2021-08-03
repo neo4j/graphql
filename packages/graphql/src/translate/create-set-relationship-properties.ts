a05468303097819bf11daeea2902a3952da5e430
Jared Kozak <jared@kozan.dev>
2021-08-03 14:43:37 +0100

    Refactor/unify relationship fields (#383)
    * feat: throw error when relationship directive is used on relationship props
    
    * test: when relationship directive is used on relationship props
    
    * refactor: unify relationship properties fields and reuse existing functions
    
    * test: add for cypher on interface

--- packages/graphql/src/translate/create-set-relationship-properties.ts
+++ packages/graphql/src/translate/create-set-relationship-properties.ts
@@ -18,7 +18,6 @@
  */
 
 import { Relationship } from "../classes";
-import { BaseField, DateTimeField, PrimitiveField } from "../types";
 
 /*
     TODO - lets reuse this function for setting either node or rel properties.
@@ -42,38 +41,37 @@ function createSetRelationshipProperties({
 
     Object.entries(properties).forEach(([key]) => {
         const paramName = `${parameterPrefix}.${key}`;
-        const field = (relationship.fields.find((x) => x.fieldName === key) as unknown) as BaseField;
 
-        if ("timestamps" in field) {
-            const f = field as DateTimeField;
-            (f.timestamps || []).forEach((ts) => {
+        const dateTimeField = relationship.dateTimeFields.find((x) => x.fieldName === key);
+        if (dateTimeField && dateTimeField.timestamps?.length) {
+            (dateTimeField.timestamps || []).forEach((ts) => {
                 if (ts.includes(operation)) {
-                    strs.push(`SET ${varName}.${f.fieldName} = datetime()`);
+                    strs.push(`SET ${varName}.${key} = datetime()`);
                 }
             });
 
             return;
         }
 
-        if ("autogenerate" in field) {
-            const f = field as PrimitiveField;
-
-            strs.push(`SET ${varName}.${f.fieldName} = randomUUID()`);
+        const primitiveField = relationship.primitiveFields.find((x) => x.fieldName === key);
+        if (primitiveField?.autogenerate) {
+            strs.push(`SET ${varName}.${key} = randomUUID()`);
 
             return;
         }
 
-        if (["Point", "CartesianPoint"].includes(field.typeMeta.name)) {
-            if (field.typeMeta.array) {
-                strs.push(`SET ${varName}.${field.fieldName} = [p in $${paramName} | point(p)]`);
+        const pointField = relationship.pointFields.find((x) => x.fieldName === key);
+        if (pointField) {
+            if (pointField.typeMeta.array) {
+                strs.push(`SET ${varName}.${key} = [p in $${paramName} | point(p)]`);
             } else {
-                strs.push(`SET ${varName}.${field.fieldName} = point($${paramName})`);
+                strs.push(`SET ${varName}.${key} = point($${paramName})`);
             }
 
             return;
         }
 
-        strs.push(`SET ${varName}.${field.fieldName} = $${paramName}`);
+        strs.push(`SET ${varName}.${key} = $${paramName}`);
     });
 
     return strs.join("\n");
