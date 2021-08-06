e7e41b924b6309915a7a921e9b283096e9eba2a3
Daniel Starns <danielstarns@hotmail.com>
2021-08-06 12:17:55 +0100

    feat: export auth errors from index (#394)

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -35,4 +35,9 @@ export {
     CypherRuntime,
     CypherUpdateStrategy,
 } from "./types";
-export { Neo4jGraphQL, Neo4jGraphQLConstructor } from "./classes";
+export {
+    Neo4jGraphQL,
+    Neo4jGraphQLConstructor,
+    Neo4jGraphQLAuthenticationError,
+    Neo4jGraphQLForbiddenError,
+} from "./classes";
