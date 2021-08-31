5e22a5bd2ae9b19c06637af0fe195c140c876b28
Jared Kozak <jared@kozan.dev>
2021-08-31 10:07:58 +0100

    test: add int for top level aggregations

---
+++ packages/graphql/tests/integration/aggregations/top_level/auth.int.test.ts
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Driver } from "neo4j-driver";
+import { graphql } from "graphql";
+import { generate } from "randomstring";
+import pluralize from "pluralize";
+import camelCase from "camelcase";
+import { IncomingMessage } from "http";
+import { Socket } from "net";
+import jsonwebtoken from "jsonwebtoken";
+import neo4j from "../../neo4j";
+import { Neo4jGraphQL } from "../../../../src/classes";
+
+describe("aggregations-top_level-auth", () => {
+    let driver: Driver;
+
+    beforeAll(async () => {
+        driver = await neo4j();
+    });
+
+    afterAll(async () => {
+        await driver.close();
+    });
+
+    test("should throw forbidden when incorrect allow on aggregate count", async () => {
+        const session = driver.session({ defaultAccessMode: "WRITE" });
+
+        const randomType = `${generate({
+            charset: "alphabetic",
+            readable: true,
+        })}Movie`;
+
+        const pluralRandomType = pluralize(camelCase(randomType));
+
+        const typeDefs = `
+            type ${randomType} {
+                id: ID
+            }
+
+            extend type ${randomType} @auth(rules: [{ allow: { id: "$jwt.sub" } }])
+        `;
+
+        const userId = generate({
+            charset: "alphabetic",
+        });
+
+        const query = `
+            {
+                ${pluralRandomType}Aggregate {
+                    count
+                }
+            }
+        `;
+
+        const secret = "secret";
+
+        const token = jsonwebtoken.sign(
+            {
+                roles: [],
+                sub: "invalid",
+            },
+            secret
+        );
+
+        const neoSchema = new Neo4jGraphQL({ typeDefs, config: { jwt: { secret } } });
+
+        try {
+            await session.run(`
+                CREATE (:${randomType} {id: "${userId}"})
+            `);
+
+            const socket = new Socket({ readable: true });
+            const req = new IncomingMessage(socket);
+            req.headers.authorization = `Bearer ${token}`;
+
+            const gqlResult = await graphql({
+                schema: neoSchema.schema,
+                source: query,
+                contextValue: { driver, req },
+            });
+
+            expect((gqlResult.errors as any[])[0].message).toEqual("Forbidden");
+        } finally {
+            await session.close();
+        }
+    });
+
+    test("should append auth where to predicate and return post count for this user", async () => {
+        const session = driver.session({ defaultAccessMode: "WRITE" });
+
+        const typeDefs = `
+            type User {
+                id: ID
+                posts: [Post] @relationship(type: "POSTED", direction: OUT)
+            }
+
+            type Post {
+                content: String
+                creator: User @relationship(type: "POSTED", direction: IN)
+            }
+
+            extend type Post @auth(rules: [{ where: { creator: { id: "$jwt.sub" } } }])
+        `;
+
+        const userId = generate({
+            charset: "alphabetic",
+        });
+
+        const query = `
+            {
+                postsAggregate {
+                    count
+                }
+            }
+        `;
+
+        const secret = "secret";
+
+        const token = jsonwebtoken.sign(
+            {
+                roles: [],
+                sub: userId,
+            },
+            secret
+        );
+
+        const neoSchema = new Neo4jGraphQL({ typeDefs, config: { jwt: { secret } } });
+
+        try {
+            await session.run(`
+                CREATE (:User {id: "${userId}"})-[:POSTED]->(:Post {content: randomUUID()})
+            `);
+
+            const socket = new Socket({ readable: true });
+            const req = new IncomingMessage(socket);
+            req.headers.authorization = `Bearer ${token}`;
+
+            const gqlResult = await graphql({
+                schema: neoSchema.schema,
+                source: query,
+                contextValue: { driver, req },
+            });
+
+            expect(gqlResult.errors).toBeUndefined();
+
+            expect(gqlResult.data).toEqual({
+                postsAggregate: {
+                    count: 1,
+                },
+            });
+        } finally {
+            await session.close();
+        }
+    });
+});
