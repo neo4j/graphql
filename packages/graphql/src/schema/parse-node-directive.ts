f4c70c9e425f3fab0d842e4d5af90a813b1f4853
Jared Kozak <jared@kozan.dev>
2021-09-03 11:53:55 +0200

    Define node directive and add it to the Node class
    Next step is to make use of the data we have in there

---
+++ packages/graphql/src/schema/parse-node-directive.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { DirectiveNode, valueFromASTUntyped, ArgumentNode } from "graphql";
+import NodeDirective from "../classes/NodeDirective";
+
+function parseNodeDirective(nodeDirective: DirectiveNode) {
+    if (!nodeDirective || nodeDirective.name.value !== "node") {
+        throw new Error("Undefined or incorrect directive passed into parseNodeDirective function");
+    }
+
+    const label = nodeDirective.arguments?.find((a) => a.name.value === "label") as ArgumentNode;
+    const additionalLabels = nodeDirective.arguments?.find((a) => a.name.value === "additionalLabels") as ArgumentNode;
+
+    const labelValue: string = valueFromASTUntyped(label.value);
+    const additionalLabelsValue: string[] = valueFromASTUntyped(additionalLabels.value);
+
+    return new NodeDirective({ label: labelValue, additionalLabels: additionalLabelsValue });
+}
+
+export default parseNodeDirective;
