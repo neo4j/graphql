b0f37742f9ba557a0d9c3baf9e84289b8b119537
Oskar Hane <oh@oskarhane.com>
2021-09-10 12:41:57 +0200

    Migrate schema tests from markdown into jest/ts and use inline snapshots
    Using inline snapshots makes in easy to see the output and easy to update snapshots when changes are made.

---
+++ packages/graphql/tests/schema/arrays.test.ts
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { printSchemaWithDirectives } from "@graphql-tools/utils";
+import { lexicographicSortSchema } from "graphql/utilities";
+import { Neo4jGraphQL } from "../../src";
+
+describe("Arrays", () => {
+    test("Arrays", () => {
+        const typeDefs = `
+type Movie {
+    id: ID!
+    ratings: [Float!]!
+    averageRating: Float!
+}`;
+        const neoSchema = new Neo4jGraphQL({ typeDefs });
+        const printedSchema = printSchemaWithDirectives(lexicographicSortSchema(neoSchema.schema));
+
+        expect(printedSchema).toMatchInlineSnapshot(`
+            "schema {
+              query: Query
+              mutation: Mutation
+            }
+
+            type CreateInfo {
+              bookmark: String
+              nodesCreated: Int!
+              relationshipsCreated: Int!
+            }
+
+            type CreateMoviesMutationResponse {
+              info: CreateInfo!
+              movies: [Movie!]!
+            }
+
+            type DeleteInfo {
+              bookmark: String
+              nodesDeleted: Int!
+              relationshipsDeleted: Int!
+            }
+
+            type Movie {
+              averageRating: Float!
+              id: ID!
+              ratings: [Float!]!
+            }
+
+            input MovieCreateInput {
+              averageRating: Float!
+              id: ID!
+              ratings: [Float!]!
+            }
+
+            input MovieOptions {
+              limit: Int
+              offset: Int
+              \\"\\"\\"Specify one or more MovieSort objects to sort Movies by. The sorts will be applied in the order in which they are arranged in the array.\\"\\"\\"
+              sort: [MovieSort]
+            }
+
+            \\"\\"\\"Fields to sort Movies by. The order in which sorts are applied is not guaranteed when specifying many fields in one MovieSort object.\\"\\"\\"
+            input MovieSort {
+              averageRating: SortDirection
+              id: SortDirection
+            }
+
+            input MovieUpdateInput {
+              averageRating: Float
+              id: ID
+              ratings: [Float!]
+            }
+
+            input MovieWhere {
+              AND: [MovieWhere!]
+              OR: [MovieWhere!]
+              averageRating: Float
+              averageRating_GT: Float
+              averageRating_GTE: Float
+              averageRating_IN: [Float]
+              averageRating_LT: Float
+              averageRating_LTE: Float
+              averageRating_NOT: Float
+              averageRating_NOT_IN: [Float]
+              id: ID
+              id_CONTAINS: ID
+              id_ENDS_WITH: ID
+              id_IN: [ID]
+              id_NOT: ID
+              id_NOT_CONTAINS: ID
+              id_NOT_ENDS_WITH: ID
+              id_NOT_IN: [ID]
+              id_NOT_STARTS_WITH: ID
+              id_STARTS_WITH: ID
+              ratings: [Float!]
+              ratings_INCLUDES: Float
+              ratings_NOT: [Float!]
+              ratings_NOT_INCLUDES: Float
+            }
+
+            type Mutation {
+              createMovies(input: [MovieCreateInput!]!): CreateMoviesMutationResponse!
+              deleteMovies(where: MovieWhere): DeleteInfo!
+              updateMovies(update: MovieUpdateInput, where: MovieWhere): UpdateMoviesMutationResponse!
+            }
+
+            type Query {
+              movies(options: MovieOptions, where: MovieWhere): [Movie!]!
+              moviesCount(where: MovieWhere): Int!
+            }
+
+            enum SortDirection {
+              \\"\\"\\"Sort by field values in ascending order.\\"\\"\\"
+              ASC
+              \\"\\"\\"Sort by field values in descending order.\\"\\"\\"
+              DESC
+            }
+
+            type UpdateInfo {
+              bookmark: String
+              nodesCreated: Int!
+              nodesDeleted: Int!
+              relationshipsCreated: Int!
+              relationshipsDeleted: Int!
+            }
+
+            type UpdateMoviesMutationResponse {
+              info: UpdateInfo!
+              movies: [Movie!]!
+            }
+            "
+        `);
+    });
+});
---
+++ packages/graphql/tests/schema/directives/default.test.ts
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { printSchemaWithDirectives } from "@graphql-tools/utils";
+import { lexicographicSortSchema } from "graphql/utilities";
+import { Neo4jGraphQL } from "../../../src";
+
+describe("Default", () => {
+    test("Simple", () => {
+        const typeDefs = `
+type User {
+    id: ID! @default(value: "00000000-00000000-00000000-00000000")
+    name: String! @default(value: "Jane Smith")
+    verified: Boolean! @default(value: false)
+    numberOfFriends: Int! @default(value: 0)
+    rating: Float! @default(value: 0.0)
+    verifiedDate: DateTime! @default(value: "1970-01-01T00:00:00.000Z")
+}`;
+        const neoSchema = new Neo4jGraphQL({ typeDefs });
+        const printedSchema = printSchemaWithDirectives(lexicographicSortSchema(neoSchema.schema));
+
+        expect(printedSchema).toMatchInlineSnapshot(`
+            "schema {
+              query: Query
+              mutation: Mutation
+            }
+
+            type CreateInfo {
+              bookmark: String
+              nodesCreated: Int!
+              relationshipsCreated: Int!
+            }
+
+            type CreateUsersMutationResponse {
+              info: CreateInfo!
+              users: [User!]!
+            }
+
+            \\"\\"\\"A date and time, represented as an ISO-8601 string\\"\\"\\"
+            scalar DateTime
+
+            type DeleteInfo {
+              bookmark: String
+              nodesDeleted: Int!
+              relationshipsDeleted: Int!
+            }
+
+            type Mutation {
+              createUsers(input: [UserCreateInput!]!): CreateUsersMutationResponse!
+              deleteUsers(where: UserWhere): DeleteInfo!
+              updateUsers(update: UserUpdateInput, where: UserWhere): UpdateUsersMutationResponse!
+            }
+
+            type Query {
+              users(options: UserOptions, where: UserWhere): [User!]!
+              usersCount(where: UserWhere): Int!
+            }
+
+            enum SortDirection {
+              \\"\\"\\"Sort by field values in ascending order.\\"\\"\\"
+              ASC
+              \\"\\"\\"Sort by field values in descending order.\\"\\"\\"
+              DESC
+            }
+
+            type UpdateInfo {
+              bookmark: String
+              nodesCreated: Int!
+              nodesDeleted: Int!
+              relationshipsCreated: Int!
+              relationshipsDeleted: Int!
+            }
+
+            type UpdateUsersMutationResponse {
+              info: UpdateInfo!
+              users: [User!]!
+            }
+
+            type User {
+              id: ID!
+              name: String!
+              numberOfFriends: Int!
+              rating: Float!
+              verified: Boolean!
+              verifiedDate: DateTime!
+            }
+
+            input UserCreateInput {
+              id: ID! = \\"00000000-00000000-00000000-00000000\\"
+              name: String! = \\"Jane Smith\\"
+              numberOfFriends: Int! = 0
+              rating: Float! = 0
+              verified: Boolean! = false
+              verifiedDate: DateTime! = \\"1970-01-01T00:00:00.000Z\\"
+            }
+
+            input UserOptions {
+              limit: Int
+              offset: Int
+              \\"\\"\\"Specify one or more UserSort objects to sort Users by. The sorts will be applied in the order in which they are arranged in the array.\\"\\"\\"
+              sort: [UserSort]
+            }
+
+            \\"\\"\\"Fields to sort Users by. The order in which sorts are applied is not guaranteed when specifying many fields in one UserSort object.\\"\\"\\"
+            input UserSort {
+              id: SortDirection
+              name: SortDirection
+              numberOfFriends: SortDirection
+              rating: SortDirection
+              verified: SortDirection
+              verifiedDate: SortDirection
+            }
+
+            input UserUpdateInput {
+              id: ID
+              name: String
+              numberOfFriends: Int
+              rating: Float
+              verified: Boolean
+              verifiedDate: DateTime
+            }
+
+            input UserWhere {
+              AND: [UserWhere!]
+              OR: [UserWhere!]
+              id: ID
+              id_CONTAINS: ID
+              id_ENDS_WITH: ID
+              id_IN: [ID]
+              id_NOT: ID
+              id_NOT_CONTAINS: ID
+              id_NOT_ENDS_WITH: ID
+              id_NOT_IN: [ID]
+              id_NOT_STARTS_WITH: ID
+              id_STARTS_WITH: ID
+              name: String
+              name_CONTAINS: String
+              name_ENDS_WITH: String
+              name_IN: [String]
+              name_NOT: String
+              name_NOT_CONTAINS: String
+              name_NOT_ENDS_WITH: String
+              name_NOT_IN: [String]
+              name_NOT_STARTS_WITH: String
+              name_STARTS_WITH: String
+              numberOfFriends: Int
+              numberOfFriends_GT: Int
+              numberOfFriends_GTE: Int
+              numberOfFriends_IN: [Int]
+              numberOfFriends_LT: Int
+              numberOfFriends_LTE: Int
+              numberOfFriends_NOT: Int
+              numberOfFriends_NOT_IN: [Int]
+              rating: Float
+              rating_GT: Float
+              rating_GTE: Float
+              rating_IN: [Float]
+              rating_LT: Float
+              rating_LTE: Float
+              rating_NOT: Float
+              rating_NOT_IN: [Float]
+              verified: Boolean
+              verifiedDate: DateTime
+              verifiedDate_GT: DateTime
+              verifiedDate_GTE: DateTime
+              verifiedDate_IN: [DateTime]
+              verifiedDate_LT: DateTime
+              verifiedDate_LTE: DateTime
+              verifiedDate_NOT: DateTime
+              verifiedDate_NOT_IN: [DateTime]
+              verified_NOT: Boolean
+            }
+            "
+        `);
+    });
+});
