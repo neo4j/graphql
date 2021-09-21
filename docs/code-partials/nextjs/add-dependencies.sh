097c5564380606727cc9d243ff8e323bd17b124a
Oskar Hane <oh@oskarhane.com>
2021-09-21 11:12:21 +0200

    Pull out code from docs and test it

---
+++ docs/code-partials/nextjs/add-dependencies.sh
@@ -0,0 +1,1 @@
+yarn add apollo-server-micro micro graphql @neo4j/graphql neo4j-driver
---
+++ docs/code-partials/nextjs/graphql.js
@@ -0,0 +1,46 @@
+import { gql, ApolloServer } from "apollo-server-micro";
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
+
+const typeDefs = gql`
+    type Movie @exclude(operations: [CREATE, UPDATE, DELETE]) {
+        title: String!
+        actors: [Actor] @relationship(type: "ACTED_IN", direction: IN)
+    }
+    type Actor @exclude(operations: [CREATE, UPDATE, DELETE]) {
+        name: String!
+        actedIn: [Movie] @relationship(type: "ACTED_IN", direction: OUT)
+    }
+`;
+
+const driver = neo4j.driver(
+    process.env.NEO4J_URI,
+    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+const apolloServer = new ApolloServer({ schema: neoSchema.schema });
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "https://studio.apollographql.com");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (req.method === "OPTIONS") {
+        res.end();
+        return false;
+    }
+
+    await startServer;
+    await apolloServer.createHandler({
+        path: "/api/graphql",
+    })(req, res);
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
