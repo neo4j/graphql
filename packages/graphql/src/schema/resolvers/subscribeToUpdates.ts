9df2feedaca2031e4173af617a7cf9fe90dd73d7
Jared Kozak <jared@kozan.dev>
2021-09-24 01:35:09 -0500

    Add subscribeToUpdates resolver

---
+++ packages/graphql/src/schema/resolvers/subscribeToUpdates.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { FieldNode, GraphQLResolveInfo } from "graphql";
+import { Node } from "../../classes";
+import { Context } from "../../types";
+
+export default function subscribeToUpdatesResolver({ node }: { node: Node }) {
+    async function resolve(_root: any, _args: any, _context: unknown, info: GraphQLResolveInfo) {
+        const context = _context as Context;
+
+        const responseField = info.fieldNodes[0].selectionSet?.selections.find(
+            (selection) => selection.kind === "Field" && selection.name.value === node.getPlural({ camelCase: true })
+        ) as FieldNode; // Field exist by construction and must be selected as it is the only field.
+
+        const responseKey = responseField.alias ? responseField.alias.value : responseField.name.value;
+
+        const updated = {};
+
+        return updated;
+
+        // return {
+            // info: {
+                // bookmark: executeResult.bookmark,
+                // ...executeResult.statistics,
+            // },
+            // [ responseKey ]: [],
+            // [responseKey]: executeResult.records.map((x) => x.this),
+        // };
+    }
+
+    return {
+        // type: `Update${node.getPlural({ camelCase: false })}MutationResponse!`,
+        type: `${ node.name }!`,
+        args: {
+            where: `${node.name}Where`,
+        },
+        resolve,
+        subscribe: () => context.pubsub.asyncIterator('updatePost'),
+    };
+}
