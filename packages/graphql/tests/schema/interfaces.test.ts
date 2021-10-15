a0749d0e9af5c73fa5c968017b4a09b9c4e92ef2
angrykoala <angrykoala@outlook.es>
2021-10-15 18:05:08 +0100

    Fix fieldAggregation types

--- packages/graphql/tests/schema/interfaces.test.ts
+++ packages/graphql/tests/schema/interfaces.test.ts
@@ -85,7 +85,7 @@ describe("Interfaces", () => {
               customQuery: [Movie]
               id: ID
               movies(options: MovieOptions, where: MovieWhere): [Movie]
-              moviesAggregate: MovieMoviemoviesAggregationSelection
+              moviesAggregate: MovieMovieMoviesAggregationSelection
               moviesConnection(after: String, first: Int, sort: [MovieMoviesConnectionSort!], where: MovieMoviesConnectionWhere): MovieMoviesConnection!
               nodes: [MovieNode]
             }
@@ -116,12 +116,12 @@ describe("Interfaces", () => {
               movies: [MovieMoviesDisconnectFieldInput!]
             }
 
-            type MovieMoviemoviesAggregationSelection {
+            type MovieMovieMoviesAggregationSelection {
               count: Int!
-              node: MovieMoviemoviesNodeAggregateSelection
+              node: MovieMovieMoviesNodeAggregateSelection
             }
 
-            type MovieMoviemoviesNodeAggregateSelection {
+            type MovieMovieMoviesNodeAggregateSelection {
               id: IDAggregateSelection!
             }
 
