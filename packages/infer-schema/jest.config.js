cbbe74cfdea0bb2e6eeb3e945fbecac709ecd574
Oskar Hane <oh@oskarhane.com>
2021-11-17 14:52:57 +0100

    Update packages/infer-schema/jest.config.js
    Co-authored-by: Darrell Warde <8117355+darrellwarde@users.noreply.github.com>

--- packages/infer-schema/jest.config.js
+++ packages/infer-schema/jest.config.js
@@ -2,7 +2,7 @@ const globalConf = require("../../jest.config.base");
 
 module.exports = {
     ...globalConf,
-    displayName: "@neo4j/graphql-infer-scehma",
+    displayName: "@neo4j/infer-schema",
     roots: ["<rootDir>/packages/infer-schema/src/", "<rootDir>/packages/infer-schema/tests/"],
     coverageDirectory: "<rootDir>/packages/infer-schema/coverage/",
     globals: {
