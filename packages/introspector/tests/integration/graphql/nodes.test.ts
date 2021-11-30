e0f53ed2703e9f6032e1f8a6866059c303f24bbc
Oskar Hane <oh@oskarhane.com>
2021-11-30 11:13:14 +0100

    Don't generate invalid typedefs for labels/reltypes starting with number
    NOTE: Neo4j GraphQL library doesn't support labels/additionalLabels starting with a number yet, so the test for the feature in this package is skipped for now.

--- packages/introspector/tests/integration/graphql/nodes.test.ts
+++ packages/introspector/tests/integration/graphql/nodes.test.ts
@@ -235,6 +235,34 @@ describe("GraphQL - Infer Schema nodes basic tests", () => {
 
         expect(() => new Neo4jGraphQL({ typeDefs, driver })).not.toThrow();
     });
+
+    // Enable when Neo4j GraphQL Library has support for this
+    test.skip("Can introspect and generate label that starts with a number", async () => {
+        // Skip if multi-db not supported
+        if (!MULTIDB_SUPPORT) {
+            // eslint-disable-next-line jest/no-disabled-tests, jest/no-jasmine-globals
+            pending();
+            return;
+        }
+
+        const nodeProperties = { first: "testString", second: neo4j.int(42) };
+        const wSession = driver.session({ defaultAccessMode: neo4j.session.WRITE, database: dbName });
+        await wSession.writeTransaction((tx) =>
+            tx.run("CREATE (:`2number` {prop: $props.second})", { props: nodeProperties })
+        );
+        const bm = wSession.lastBookmark();
+        await wSession.close();
+
+        const typeDefs = await toGraphQLTypeDefs(sessionFactory(bm));
+
+        expect(typeDefs).toMatchInlineSnapshot(`
+            "type _2number @node(label: \\"2number\\") {
+            	prop: BigInt!
+            }"
+        `);
+
+        expect(() => new Neo4jGraphQL({ typeDefs, driver })).not.toThrow();
+    });
     test("Should not include properties with ambiguous types", async () => {
         // Skip if multi-db not supported
         if (!MULTIDB_SUPPORT) {
