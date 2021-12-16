a35352c790bc9d91e8cacccd5644f2572e36392e
Andrés <angrykoala@outlook.es>
2021-12-16 18:18:56 +0000

    fix file dependencies

--- packages/graphql/tests/integration/connections/alias.int.test.ts
+++ packages/graphql/tests/integration/connections/alias.int.test.ts
@@ -23,7 +23,7 @@ import { generate } from "randomstring";
 import { gql } from "apollo-server";
 import neo4j from "../neo4j";
 import { Neo4jGraphQL } from "../../../src/classes";
-import { generateUniqueType } from "../../../src/utils/test/graphql-types";
+import { generateUniqueType } from "../../utils/graphql-types";
 
 describe("Connections Alias", () => {
     let driver: Driver;
