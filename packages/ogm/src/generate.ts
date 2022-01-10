7b5d291d98606cfae8bc91c3e8cc505cd9ce59c6
Andrés <angrykoala@outlook.es>
2022-01-10 14:12:26 +0000

    remove camelcase from ogm package

--- packages/ogm/src/generate.ts
+++ packages/ogm/src/generate.ts
@@ -21,8 +21,6 @@ import { codegen } from "@graphql-codegen/core";
 import * as typescriptPlugin from "@graphql-codegen/typescript";
 import { Types } from "@graphql-codegen/plugin-helpers";
 import { upperFirst, Neo4jGraphQL } from "@neo4j/graphql";
-import camelCase from "camelcase";
-import pluralize from "pluralize";
 import * as fs from "fs";
 import * as graphql from "graphql";
 import prettier from "prettier";
@@ -134,9 +132,6 @@ async function generate(options: IGenerateOptions): Promise<undefined | string> 
     const modeMap: Record<string, string> = {};
 
     options.ogm.neoSchema.nodes.forEach((node) => {
-        const pluralized = pluralize(node.name); // TODO: fix and test this!!
-        const camelName = camelCase(pluralized);
-        const upperCamel = upperFirst(camelName);
         const modelName = `${node.name}Model`;
         const hasFulltextArg = Boolean(node.fulltextDirective);
 
@@ -174,7 +169,7 @@ async function generate(options: IGenerateOptions): Promise<undefined | string> 
                     args?: any;
                     context?: any;
                     rootValue?: any;
-                }): Promise<Create${upperCamel}MutationResponse>
+                }): Promise<Create${upperFirst(node.plural)}MutationResponse>
                 public update(args: {
                     where?: ${node.name}Where;
                     update?: ${node.name}UpdateInput;
@@ -186,7 +181,7 @@ async function generate(options: IGenerateOptions): Promise<undefined | string> 
                     args?: any;
                     context?: any;
                     rootValue?: any;
-                }): Promise<Update${upperCamel}MutationResponse>
+                }): Promise<Update${upperFirst(node.plural)}MutationResponse>
                 public delete(args: {
                     where?: ${node.name}Where;
                     ${node.relationFields.length ? `delete?: ${node.name}DeleteInput` : ""}
