0e3d31c9a051764201ea44cbf4c4d449b13fbe72
Darrell Warde <8117355+darrellwarde@users.noreply.github.com>
2022-01-14 13:36:29 +0000

    Bump @graphql-tools monorepo to 8.x (#757)
    * Dependency upgrades and installations
    
    * Refactor usage of SchemaDirectiveVisitor and addSchemaLevelResolver
    
    * Fix neo-push graphql dependency problems
    
    * ESLint violation fixes
    
    * Attempting to fix ESLint violations only showing in CI
    
    * Fix ESLint warning violations
    
    * Attempt to fix Next.js tests by using tarball of library
    
    * Documentation updates for API and schema directives
    
    * Tidy up of code

--- packages/graphql/src/schema/make-augmented-schema.ts
+++ packages/graphql/src/schema/make-augmented-schema.ts
@@ -18,16 +18,15 @@
  */
 
 import { mergeTypeDefs } from "@graphql-tools/merge";
-import { IExecutableSchemaDefinition, makeExecutableSchema } from "@graphql-tools/schema";
-import { forEachField } from "@graphql-tools/utils";
+import { TypeSource } from "@graphql-tools/utils";
 import {
     DefinitionNode,
     DirectiveDefinitionNode,
     DirectiveNode,
+    DocumentNode,
     EnumTypeDefinitionNode,
     GraphQLInt,
     GraphQLNonNull,
-    GraphQLSchema,
     GraphQLString,
     InputObjectTypeDefinitionNode,
     InterfaceTypeDefinitionNode,
@@ -65,7 +64,6 @@ import {
     aggregateResolver,
     createResolver,
     cypherResolver,
-    defaultFieldResolver,
     deleteResolver,
     findResolver,
     updateResolver,
@@ -78,9 +76,9 @@ import getUniqueFields from "./get-unique-fields";
 import { AggregationTypesMapper } from "./aggregations/aggregation-types-mapper";
 
 function makeAugmentedSchema(
-    { typeDefs, ...schemaDefinition }: IExecutableSchemaDefinition,
+    typeDefs: TypeSource,
     { enableRegex, skipValidateTypeDefs }: { enableRegex?: boolean; skipValidateTypeDefs?: boolean } = {}
-): { schema: GraphQLSchema; nodes: Node[]; relationships: Relationship[] } {
+): { nodes: Node[]; relationships: Relationship[]; typeDefs: DocumentNode; resolvers } {
     const document = mergeTypeDefs(Array.isArray(typeDefs) ? (typeDefs as string[]) : [typeDefs as string]);
 
     if (!skipValidateTypeDefs) {
@@ -792,8 +790,8 @@ function makeAugmentedSchema(
         });
 
         if (node.fulltextDirective) {
-            const fields = node.fulltextDirective.indexes.reduce((res, index) => {
-                return {
+            const fields = node.fulltextDirective.indexes.reduce(
+                (res, index) => ({
                     ...res,
                     [index.name]: composer.createInputTC({
                         name: `${node.name}${upperFirst(index.name)}Fulltext`,
@@ -802,8 +800,9 @@ function makeAugmentedSchema(
                             score_EQUAL: "Int",
                         },
                     }),
-                };
-            }, {});
+                }),
+                {}
+            );
 
             composer.createInputTC({
                 name: `${node.name}Fulltext`,
@@ -1048,24 +1047,11 @@ function makeAugmentedSchema(
         }),
     };
 
-    const schema = makeExecutableSchema({
-        ...schemaDefinition,
-        typeDefs: parsedDoc,
-        resolvers: generatedResolvers,
-    });
-
-    // Assign a default field resolver to account for aliasing of fields
-    forEachField(schema, (field) => {
-        if (!field.resolve) {
-            // eslint-disable-next-line no-param-reassign
-            field.resolve = defaultFieldResolver;
-        }
-    });
-
     return {
         nodes,
         relationships,
-        schema,
+        typeDefs: parsedDoc,
+        resolvers: generatedResolvers,
     };
 }
 
