cbfcd583a4d0b47e32689a88f8681de75a4fb471
dmoree <dmoree@users.noreply.github.com>
2022-01-23 23:38:37 -0800

    add: `@cypher` field integration test

--- packages/graphql/tests/integration/directives/ignore.int.test.ts
+++ packages/graphql/tests/integration/directives/ignore.int.test.ts
@@ -28,121 +28,247 @@ const testLabel = generate({ charset: "alphabetic" });
 describe("@ignore directive", () => {
     let driver: Driver;
 
-    const typeDefs = `
-        type User {
-            id: ID!
-            firstName: String!
-            lastName: String!
-            fullName: String @ignore(require: ["firstName", "lastName"])
-        }
-    `;
-
-    const user = {
-        id: generate(),
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-    };
-
-    const fullName = ({ firstName, lastName }) => `${firstName} ${lastName}`;
-
-    const resolvers = {
-        User: { fullName },
-    };
-
-    const { schema } = new Neo4jGraphQL({ typeDefs, resolvers });
-
     beforeAll(async () => {
         driver = await neo4j();
-        const session = driver.session();
-        await session.run(
-            `
-            CREATE (user:User:${testLabel}) SET user = $user
-        `,
-            { user }
-        );
-        await session.close();
     });
 
     afterAll(async () => {
-        const session = driver.session();
-        await session.run(`MATCH (n:${testLabel}) DETACH DELETE n`);
-        await session.close();
         await driver.close();
     });
 
-    test("removes a field from all but its object type, and resolves with a custom resolver", async () => {
-        const source = `
-            query Users($userId: ID!) {
-                users(where: { id: $userId }) {
-                    id
-                    firstName
-                    lastName
-                    fullName
-                }
+    describe("Scalar fields", () => {
+        const typeDefs = `
+            type User {
+                id: ID!
+                firstName: String!
+                lastName: String!
+                fullName: String @ignore(require: ["firstName", "lastName"])
             }
         `;
 
-        const gqlResult = await graphql({
-            schema,
-            source,
-            contextValue: { driver },
-            variableValues: { userId: user.id },
+        const user = {
+            id: generate(),
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+        };
+
+        const fullName = ({ firstName, lastName }) => `${firstName} ${lastName}`;
+
+        const resolvers = {
+            User: { fullName },
+        };
+
+        const { schema } = new Neo4jGraphQL({ typeDefs, resolvers });
+
+        beforeAll(async () => {
+            const session = driver.session();
+            await session.run(
+                `
+                CREATE (user:User:${testLabel}) SET user = $user
+            `,
+                { user }
+            );
+            await session.close();
         });
 
-        expect(gqlResult.errors).toBeFalsy();
-        expect((gqlResult.data as any).users[0]).toEqual({
-            ...user,
-            fullName: fullName(user),
+        afterAll(async () => {
+            const session = driver.session();
+            await session.run(`MATCH (n:${testLabel}) DETACH DELETE n`);
+            await session.close();
+        });
+
+        test("removes a field from all but its object type, and resolves with a custom resolver", async () => {
+            const source = `
+                query Users($userId: ID!) {
+                    users(where: { id: $userId }) {
+                        id
+                        firstName
+                        lastName
+                        fullName
+                    }
+                }
+            `;
+
+            const gqlResult = await graphql({
+                schema,
+                source,
+                contextValue: { driver },
+                variableValues: { userId: user.id },
+            });
+
+            expect(gqlResult.errors).toBeFalsy();
+            expect((gqlResult.data as any).users[0]).toEqual({
+                ...user,
+                fullName: fullName(user),
+            });
+        });
+
+        test("resolves field with custom resolver without required fields in selection set", async () => {
+            const source = `
+                query Users($userId: ID!) {
+                    users(where: { id: $userId }) {
+                        id
+                        fullName
+                    }
+                }
+            `;
+
+            const gqlResult = await graphql({
+                schema,
+                source,
+                contextValue: { driver },
+                variableValues: { userId: user.id },
+            });
+
+            expect(gqlResult.errors).toBeFalsy();
+            expect((gqlResult.data as any).users[0]).toEqual({
+                id: user.id,
+                fullName: fullName(user),
+            });
+        });
+
+        test("resolves field with custom resolver with required field(s) aliased in selection set", async () => {
+            const source = `
+                query Users($userId: ID!) {
+                    users(where: { id: $userId }) {
+                        id
+                        f: firstName
+                        fullName
+                    }
+                }
+            `;
+
+            const gqlResult = await graphql({
+                schema,
+                source,
+                contextValue: { driver },
+                variableValues: { userId: user.id },
+            });
+
+            expect(gqlResult.errors).toBeFalsy();
+            expect((gqlResult.data as any).users[0]).toEqual({
+                id: user.id,
+                f: user.firstName,
+                fullName: fullName(user),
+            });
         });
     });
 
-    test("resolves field with custom resolver without required fields in selection set", async () => {
-        const source = `
-            query Users($userId: ID!) {
-                users(where: { id: $userId }) {
-                    id
-                    fullName
-                }
+    describe("Cypher fields", () => {
+        const user = {
+            id: generate(),
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+        };
+
+        const typeDefs = `
+            type User {
+                id: ID!
+                firstName: String! @cypher(statement: "RETURN '${user.firstName}'")
+                lastName: String! @cypher(statement: "RETURN '${user.lastName}'")
+                fullName: String @ignore(require: ["firstName", "lastName"])
             }
         `;
 
-        const gqlResult = await graphql({
-            schema,
-            source,
-            contextValue: { driver },
-            variableValues: { userId: user.id },
+        const fullName = ({ firstName, lastName }) => `${firstName} ${lastName}`;
+
+        const resolvers = {
+            User: { fullName },
+        };
+
+        const { schema } = new Neo4jGraphQL({ typeDefs, resolvers });
+
+        beforeAll(async () => {
+            const session = driver.session();
+            await session.run(
+                `
+                CREATE (user:User:${testLabel}) SET user.id = $userId
+            `,
+                { userId: user.id }
+            );
+            await session.close();
         });
 
-        expect(gqlResult.errors).toBeFalsy();
-        expect((gqlResult.data as any).users[0]).toEqual({
-            id: user.id,
-            fullName: fullName(user),
+        afterAll(async () => {
+            const session = driver.session();
+            await session.run(`MATCH (n:${testLabel}) DETACH DELETE n`);
+            await session.close();
         });
-    });
 
-    test("resolves field with custom resolver with required field(s) aliased in selection set", async () => {
-        const source = `
-            query Users($userId: ID!) {
-                users(where: { id: $userId }) {
-                    id
-                    f: firstName
-                    fullName
+        test("removes a field from all but its object type, and resolves with a custom resolver", async () => {
+            const source = `
+                query Users($userId: ID!) {
+                    users(where: { id: $userId }) {
+                        id
+                        firstName
+                        lastName
+                        fullName
+                    }
                 }
-            }
-        `;
+            `;
 
-        const gqlResult = await graphql({
-            schema,
-            source,
-            contextValue: { driver },
-            variableValues: { userId: user.id },
+            const gqlResult = await graphql({
+                schema,
+                source,
+                contextValue: { driver },
+                variableValues: { userId: user.id },
+            });
+
+            expect(gqlResult.errors).toBeFalsy();
+            expect((gqlResult.data as any).users[0]).toEqual({
+                ...user,
+                fullName: fullName(user),
+            });
         });
 
-        expect(gqlResult.errors).toBeFalsy();
-        expect((gqlResult.data as any).users[0]).toEqual({
-            id: user.id,
-            f: user.firstName,
-            fullName: fullName(user),
+        test("resolves field with custom resolver without required fields in selection set", async () => {
+            const source = `
+                query Users($userId: ID!) {
+                    users(where: { id: $userId }) {
+                        id
+                        fullName
+                    }
+                }
+            `;
+
+            const gqlResult = await graphql({
+                schema,
+                source,
+                contextValue: { driver },
+                variableValues: { userId: user.id },
+            });
+
+            expect(gqlResult.errors).toBeFalsy();
+            expect((gqlResult.data as any).users[0]).toEqual({
+                id: user.id,
+                fullName: fullName(user),
+            });
+        });
+
+        test("resolves field with custom resolver with required field(s) aliased in selection set", async () => {
+            const source = `
+                query Users($userId: ID!) {
+                    users(where: { id: $userId }) {
+                        id
+                        f: firstName
+                        fullName
+                    }
+                }
+            `;
+
+            const gqlResult = await graphql({
+                schema,
+                source,
+                contextValue: { driver },
+                variableValues: { userId: user.id },
+            });
+
+            expect(gqlResult.errors).toBeFalsy();
+            expect((gqlResult.data as any).users[0]).toEqual({
+                id: user.id,
+                f: user.firstName,
+                fullName: fullName(user),
+            });
         });
     });
 });
