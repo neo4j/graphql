00d72bca5d31ea4a4f2e824ac121ff1710fb9381
Darrell Warde <darrell.warde@neotechnology.com>
2022-02-03 09:41:43 +0000

    Update generate test

--- packages/ogm/tests/issues/635.test.ts
+++ packages/ogm/tests/issues/635.test.ts
@@ -29,11 +29,11 @@ describe("issues/635", () => {
             MANAGER
             EMPLOYEE
           }
-        
+
           enum FeatureName {
             NONE
           }
-        
+
           enum BodyStyle {
             MICRO
             SEDAN
@@ -51,25 +51,25 @@ describe("issues/635", () => {
             CAMPERVAN
             TRUCK
           }
-        
+
           enum TimeClockType {
             IN
             OUT
             BREAK
             LUNCH
           }
-        
+
           enum IntegrationNames {
             STRIPE
           }
-        
+
           enum PriceTier {
             FREE
             PREMIUM
             PARTNER
           }
           ## END ENUMS ##
-        
+
           ## START NODES ##
           type User {
             id: ID! @id
@@ -82,18 +82,18 @@ describe("issues/635", () => {
             createdAt: DateTime! @timestamp(operations: [CREATE])
             updatedAt: DateTime! @timestamp
             company: Company! @relationship(type: "WORKS_AT", direction: OUT)
-            workHistory: [WorkOrderItem]
+            workHistory: [WorkOrderItem!]!
               @relationship(type: "WORKED_ON", direction: OUT)
-            timeHistory: [TimeClock] @relationship(type: "CLOCK_FOR", direction: IN)
+            timeHistory: [TimeClock!]! @relationship(type: "CLOCK_FOR", direction: IN)
           }
-        
+
           type TimeClock {
             id: ID! @id
             type: TimeClockType
             createdAt: DateTime! @timestamp(operations: [CREATE])
             user: User! @relationship(type: "CLOCK_FOR", direction: OUT)
           }
-        
+
           type Company {
             id: ID! @id
             name: String! @unique
@@ -102,16 +102,16 @@ describe("issues/635", () => {
             createdAt: DateTime! @timestamp(operations: [CREATE])
             updatedAt: DateTime! @timestamp
             employees: [User!]! @relationship(type: "WORKS_AT", direction: IN)
-            customers: [Customer] @relationship(type: "CUSTOMER_AT", direction: IN)
-            workOrders: [WorkOrder] @relationship(type: "WORK_OWNED_BY", direction: IN)
-            features: [CompanyFeatures]
+            customers: [Customer!]! @relationship(type: "CUSTOMER_AT", direction: IN)
+            workOrders: [WorkOrder!]! @relationship(type: "WORK_OWNED_BY", direction: IN)
+            features: [CompanyFeatures!]!
               @relationship(type: "FEATURE_TOGGLED", direction: OUT)
             clockSettings: CompanyClockSettings!
               @relationship(type: "CLOCK_SETTINGS", direction: OUT)
-            integrations: [CompanyIntegrations]
+            integrations: [CompanyIntegrations!]!
               @relationship(type: "INTEGRATION_FOR", direction: IN)
           }
-        
+
           type CompanyIntegrations {
             id: ID! @id
             name: IntegrationNames!
@@ -119,21 +119,21 @@ describe("issues/635", () => {
             privateApiKey: String!
             company: Company! @relationship(type: "INTEGRATION_FOR", direction: OUT)
           }
-        
+
           type CompanyClockSettings {
             id: ID! @id
             breakTime: Int @default(value: 15)
             lunchTime: Int @default(value: 30)
             company: Company! @relationship(type: "CLOCK_SETTINGS", direction: IN)
           }
-        
+
           type CompanyFeatures {
             id: ID! @id
             featureName: FeatureName!
             createdAt: DateTime! @timestamp(operations: [CREATE])
             company: Company! @relationship(type: "FEATURE_TOGGLED", direction: IN)
           }
-        
+
           type WorkOrder {
             id: ID! @id
             budget: Float
@@ -148,7 +148,7 @@ describe("issues/635", () => {
               @relationship(type: "ITEM_FOR", direction: IN)
             vehicle: Vehicle! @relationship(type: "SERVICE_FOR", direction: OUT)
           }
-        
+
           type WorkOrderItem {
             id: ID! @id
             item: String!
@@ -160,7 +160,7 @@ describe("issues/635", () => {
             completedBy: User @relationship(type: "WORKED_ON", direction: IN)
             workOrder: WorkOrder @relationship(type: "ITEM_FOR", direction: OUT)
           }
-        
+
           type Customer {
             id: ID! @id
             email: String!
@@ -169,46 +169,46 @@ describe("issues/635", () => {
             fullName: String! @ignore
             phoneNumber: String!
             servicer: Company @relationship(type: "CUSTOMER_AT", direction: OUT)
-            vehicles: [Customer] @relationship(type: "VEHICLE_OWNED_BY", direction: IN)
+            vehicles: [Customer!]! @relationship(type: "VEHICLE_OWNED_BY", direction: IN)
           }
-        
+
           type Vehicle {
             id: ID! @id
             vin: String!
-            workOrders: [WorkOrder] @relationship(type: "SERVICE_FOR", direction: IN)
+            workOrders: [WorkOrder!]! @relationship(type: "SERVICE_FOR", direction: IN)
             owner: Customer! @relationship(type: "VEHICLE_OWNED_BY", direction: OUT)
             model: ModelTrim! @relationship(type: "VEHICLE_MODEL", direction: OUT)
           }
-        
+
           type Manufacturer {
             id: ID! @id
             name: String! @unique
-            models: [Model] @relationship(type: "MADE_BY", direction: IN)
+            models: [Model!]! @relationship(type: "MADE_BY", direction: IN)
           }
-        
+
           type Model {
             id: ID! @id
             year: Int!
             style: BodyStyle!
             name: String!
-            models: [Model] @relationship(type: "MADE_BY", direction: IN)
-            trims: [ModelTrim] @relationship(type: "TRIM_FOR", direction: IN)
+            models: [Model!]! @relationship(type: "MADE_BY", direction: IN)
+            trims: [ModelTrim!]! @relationship(type: "TRIM_FOR", direction: IN)
           }
-        
+
           type ModelTrim {
             id: ID! @id
             name: String!
             model: Model @relationship(type: "TRIM_FOR", direction: OUT)
-            customerVehicles: [Vehicle]
+            customerVehicles: [Vehicle!]!
               @relationship(type: "VEHICLE_MODEL", direction: IN)
-            paintCodes: [PaintCodes] @relationship(type: "HAS_PAINT", direction: OUT)
+            paintCodes: [PaintCodes!]! @relationship(type: "HAS_PAINT", direction: OUT)
           }
-        
+
           type PaintCodes {
             id: ID! @id
             code: String! @unique
-            trims: [ModelTrim] @relationship(type: "HAS_PAINT", direction: IN)
-          }        
+            trims: [ModelTrim!]! @relationship(type: "HAS_PAINT", direction: IN)
+          }
         `;
 
         const ogm = new OGM({
@@ -222,16 +222,16 @@ describe("issues/635", () => {
             noWrite: true,
         })) as string;
 
-        expect(generated.includes(`export type CompanyClockSettingsAggregateInput`)).toBe(true);
-        expect(generated.includes(`export interface CompanyClockSettingsAggregateInput`)).toBe(false);
-        expect(generated.includes(`export interface CompanyClockSettingsAggregateSelectionInput`)).toBe(true);
+        expect(generated).toContain(`export type CompanyClockSettingsAggregateInput`);
+        expect(generated).not.toContain(`export interface CompanyClockSettingsAggregateInput`);
+        expect(generated).toContain(`export interface CompanyClockSettingsAggregateSelectionInput`);
 
-        expect(generated.includes(`export type CompanyFeaturesAggregateInput`)).toBe(true);
-        expect(generated.includes(`export interface CompanyFeaturesAggregateInput`)).toBe(false);
-        expect(generated.includes(`export interface CompanyFeaturesAggregateSelectionInput`)).toBe(true);
+        expect(generated).toContain(`export type CompanyFeaturesAggregateInput`);
+        expect(generated).not.toContain(`export interface CompanyFeaturesAggregateInput`);
+        expect(generated).toContain(`export interface CompanyFeaturesAggregateSelectionInput`);
 
-        expect(generated.includes(`export type CompanyIntegrationsAggregateInput`)).toBe(true);
-        expect(generated.includes(`export interface CompanyIntegrationsAggregateInput`)).toBe(false);
-        expect(generated.includes(`export interface CompanyIntegrationsAggregateSelectionInput`)).toBe(true);
+        expect(generated).toContain(`export type CompanyIntegrationsAggregateInput`);
+        expect(generated).not.toContain(`export interface CompanyIntegrationsAggregateInput`);
+        expect(generated).toContain(`export interface CompanyIntegrationsAggregateSelectionInput`);
     });
 });
