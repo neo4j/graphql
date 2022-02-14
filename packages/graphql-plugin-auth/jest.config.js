2926baeb0dbd6d8246fc373f63f33db5fdc2105f
Daniel Starns <danielstarns@hotmail.com>
2022-02-14 13:38:56 +0000

    refactor: remove unused tsconfig roots

--- packages/graphql-plugin-auth/jest.config.js
+++ packages/graphql-plugin-auth/jest.config.js
@@ -3,7 +3,7 @@ const globalConf = require("../../jest.config.base");
 module.exports = {
     ...globalConf,
     displayName: "@neo4j/graphql-plugin-auth",
-    roots: ["<rootDir>/packages/graphql-plugin-auth/src", "<rootDir>/packages/graphql-plugin-auth/tests"],
+    roots: ["<rootDir>/packages/graphql-plugin-auth/src"],
     coverageDirectory: "<rootDir>/packages/graphql-plugin-auth/coverage/",
     globals: {
         "ts-jest": {
