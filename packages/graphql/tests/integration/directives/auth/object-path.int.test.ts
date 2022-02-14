6f0f43484ea909b70e4188449dbe781d218b07d7
Daniel Starns <danielstarns@hotmail.com>
2022-02-14 14:21:14 +0000

    lint: further changes to linting

--- packages/graphql/tests/integration/directives/auth/object-path.int.test.ts
+++ packages/graphql/tests/integration/directives/auth/object-path.int.test.ts
@@ -17,13 +17,13 @@
  * limitations under the License.
  */
 
+import { JWTPlugin, JWKSPlugin } from "@neo4j/graphql-plugin-auth";
 import { Driver } from "neo4j-driver";
 import { graphql } from "graphql";
 import { generate } from "randomstring";
 import neo4j from "../../neo4j";
 import { Neo4jGraphQL } from "../../../../src/classes";
 import { createJwtRequest } from "../../../utils/create-jwt-request";
-import { JWTPlugin, JWKSPlugin } from "@neo4j/graphql-plugin-auth";
 
 describe("auth/object-path", () => {
     let driver: Driver;
