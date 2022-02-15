8ecb815e508c0fa370ab541452005f0496459558
Daniel Starns <danielstarns@hotmail.com>
2022-02-15 11:16:44 +0000

    refactor: remove comment import

--- packages/graphql-plugin-auth/src/Neo4jGraphQLAuthJWKSPlugin.test.ts
+++ packages/graphql-plugin-auth/src/Neo4jGraphQLAuthJWKSPlugin.test.ts
@@ -17,7 +17,6 @@
  * limitations under the License.
  */
 
-// import * as jsonwebtoken from "jsonwebtoken";
 import Neo4jGraphQLAuthJWKSPlugin from "./Neo4jGraphQLAuthJWKSPlugin";
 
 describe("Neo4jGraphQLAuthJWKSPlugin", () => {
