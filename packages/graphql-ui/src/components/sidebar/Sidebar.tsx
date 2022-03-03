8b6840898c5211032776066490d19ff414d14a71
Thomas Wiss <thomas.wiss@neotechnology.com>
2022-03-03 08:37:07 +0100

    Add logout placeholder

--- packages/graphql-ui/src/components/sidebar/Sidebar.tsx
+++ packages/graphql-ui/src/components/sidebar/Sidebar.tsx
@@ -1,13 +1,20 @@
 const SideBar = () => {
     return (
-        <div className="flex flex-col w-24 h-screen p-4 overflow-y-auto bg-sidebargrey">
+        <div className="flex flex-col w-16 h-screen py-4 overflow-y-auto bg-sidebargrey">
             <div className="flex flex-col justify-between align-center text-white">
                 <ul>
-                    <li className="p-4 flex justify-center">
+                    <li className="pb-8 flex justify-center">
                         <span className="font-medium text-2xl cursor-pointer">S</span>
                     </li>
-                    <li className="p-4 flex justify-center">
+                    <li className="pb-8 flex justify-center">
                         <span className="font-medium text-2xl cursor-pointer">Q</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="flex flex-col-reverse flex-1 justify-between align-center text-white">
+                <ul>
+                    <li className="pb-4 flex justify-center">
+                        <span className="font-medium text-2xl cursor-pointer">L</span>
                     </li>
                 </ul>
             </div>
