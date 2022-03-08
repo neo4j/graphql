11aecd6d0db0a6369eb6867955363c842d8b336a
Daniel Starns <danielstarns@hotmail.com>
2022-03-08 12:49:18 +0000

    config/test: more groundwork for headless tests

--- packages/graphql-ui/src/components/editor/JSONEditor.tsx
+++ packages/graphql-ui/src/components/editor/JSONEditor.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { CodeMirror } from "../../util";
+import { CodeMirror } from "../../utils/utils";
 
 export interface Props {
     json?: string;
