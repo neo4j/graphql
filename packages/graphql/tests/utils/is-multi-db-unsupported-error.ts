5b81f60b237aed02d2e6a4055c339f1b495555e4
Darrell Warde <8117355+darrellwarde@users.noreply.github.com>
2022-03-14 08:07:57 +0000

    Update packages/graphql/tests/utils/is-multi-db-unsupported-error.ts
    Co-authored-by: Thomas Wiss <thomas.wiss@hotmail.com>

--- packages/graphql/tests/utils/is-multi-db-unsupported-error.ts
+++ packages/graphql/tests/utils/is-multi-db-unsupported-error.ts
@@ -20,7 +20,7 @@
 export function isMultiDbUnsupportedError(e: Error) {
     if (
         e.message.includes("This is an administration command and it should be executed against the system database") ||
-        e.message.includes(`Neo4jError: Unsupported administration command`) ||
+        e.message.includes("Neo4jError: Unsupported administration command") ||
         e.message.includes("Neo4jError: Unable to route write operation to leader for database 'system'")
     ) {
         return true;
