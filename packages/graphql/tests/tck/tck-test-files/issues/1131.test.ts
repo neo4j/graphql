ed6df2cc60a490f411eeef0e0a1fc8379ec78d77
Darrell Warde <8117355+darrellwarde@users.noreply.github.com>
2022-03-16 16:53:06 +0100

    Update packages/graphql/tests/tck/tck-test-files/issues/1131.test.ts
    Co-authored-by: Thomas Wiss <thomas.wiss@hotmail.com>

--- packages/graphql/tests/tck/tck-test-files/issues/1131.test.ts
+++ packages/graphql/tests/tck/tck-test-files/issues/1131.test.ts
@@ -23,7 +23,7 @@ import { Neo4jGraphQL } from "../../../../src";
 import { createJwtRequest } from "../../../utils/create-jwt-request";
 import { formatCypher, translateQuery, formatParams } from "../../utils/tck-test-utils";
 
-describe("https://github.com/neo4j/graphql/issues/988", () => {
+describe("https://github.com/neo4j/graphql/issues/1131", () => {
     let typeDefs: DocumentNode;
     let neoSchema: Neo4jGraphQL;
 
