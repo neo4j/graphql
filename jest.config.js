58a3226b849dd6a2454f0c4e6425951f8773ed7c
angrykoala <angrykoala@outlook.es>
2022-04-26 14:21:16 +0100

    move auth plugin to new folder

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const baseConfig = require("./jest.config.base");
 
 module.exports = {
     ...baseConfig,
-    projects: ["<rootDir>/examples/neo-push/server/jest.config.js", "<rootDir>/packages/*/jest.config.js"],
+    projects: ["<rootDir>/examples/neo-push/server/jest.config.js", "<rootDir>/packages/**/jest.config.js"],
     coverageDirectory: "<rootDir>/coverage/",
     collectCoverageFrom: ["<rootDir>/packages/*/src/**/*.{ts,tsx}"],
 };
