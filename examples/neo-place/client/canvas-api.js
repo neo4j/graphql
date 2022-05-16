2ee84c7f0eeec02dc6935f7debd1c1cd8de68843
Darrell Warde <darrell.warde@neotechnology.com>
2022-05-16 21:47:29 +0100

    BigInt to Int, and add local/prod WS

--- examples/neo-place/client/canvas-api.js
+++ examples/neo-place/client/canvas-api.js
@@ -1,36 +1,32 @@
 module.exports = class CanvasApi {
     constructor(id, pixelScale, onPixelClicked) {
         this.canvas = document.getElementById(id);
-        this.ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext("2d");
         this.ctx.imageSmoothingEnabled = false;
-        this.pixelScale = pixelScale
-        this.onPixelClicked = onPixelClicked
+        this.pixelScale = pixelScale;
+        this.onPixelClicked = onPixelClicked;
 
-        this.canvas.addEventListener('click', (ev) => this.onClick(ev), false);
+        this.canvas.addEventListener("click", (ev) => this.onClick(ev), false);
     }
-
 
     drawPixel(pixel, color) {
         const x0 = pixel[0] * this.pixelScale;
         const y0 = pixel[1] * this.pixelScale;
-        this.ctx.fillStyle = color
+        this.ctx.fillStyle = color;
         this.ctx.imageSmoothingEnabled = false;
         this.ctx.fillRect(x0, y0, this.pixelScale, this.pixelScale);
     }
 
     onClick(ev) {
-        const pixelClicked = this.getCanvasPixel([event.clientX, event.clientY])
-        this.onPixelClicked(pixelClicked)
+        const pixelClicked = this.getCanvasPixel([event.clientX, event.clientY]);
+        this.onPixelClicked(pixelClicked);
     }
 
-
-    getCanvasPixel(screenPixel){
+    getCanvasPixel(screenPixel) {
         let rect = this.canvas.getBoundingClientRect();
         let x = screenPixel[0] - parseInt(rect.left);
         let y = screenPixel[1] - parseInt(rect.top);
 
-        return [parseInt(x / this.pixelScale), parseInt(y / this.pixelScale)]
+        return [parseInt(x / this.pixelScale), parseInt(y / this.pixelScale)];
     }
-
-
-}
+};
--- examples/neo-place/client/index.js
+++ examples/neo-place/client/index.js
@@ -1,46 +1,43 @@
-import {
-    createClient,
-    defaultExchanges,
-    subscriptionExchange,
-    gql
-} from 'urql';
-import {
-    pipe,
-    subscribe
-} from 'wonka';
-import {
-    createClient as createWSClient
-} from 'graphql-ws';
+import { createClient, defaultExchanges, subscriptionExchange, gql } from "urql";
+import { pipe, subscribe } from "wonka";
+import { createClient as createWSClient } from "graphql-ws";
 
-import CanvasApi from './canvas-api';
+import CanvasApi from "./canvas-api";
 
-let color = "#FFFFFF"
+let color = "#FFFFFF";
 const buttons = [
     ["red-button", "#FF0000"],
     ["blue-button", "#0000FF"],
     ["white-button", "#FFFFFF"],
     ["black-button", "#000000"],
-    ["green-button", "#00FF00"]
-]
+    ["green-button", "#00FF00"],
+];
 
 for (const buttonAndColor of buttons) {
     const button = document.getElementById(buttonAndColor[0]);
     button.onclick = () => {
-        selectColor(buttonAndColor[1])
-    }
+        selectColor(buttonAndColor[1]);
+    };
 }
-
 
 function selectColor(newColor) {
-    color = newColor
+    color = newColor;
 }
 
+let wsUrl = "ws://localhost:4000/graphql";
+
+if (process.env.NODE_ENV === "production") {
+    wsUrl = "ws://team-graphql.uc.r.appspot.com/graphql";
+}
+
+console.log(wsUrl);
+
 const wsClient = createWSClient({
-    url: 'ws://localhost:4000/graphql',
+    url: wsUrl,
 });
 
 const client = createClient({
-    url: '/graphql',
+    url: "/graphql",
     exchanges: [
         ...defaultExchanges,
         subscriptionExchange({
@@ -54,75 +51,68 @@ const client = createClient({
 });
 
 const updatePixelQuery = gql`
-mutation UpdatePixels($update: PixelUpdateInput, $where: PixelWhere) {
-  updatePixels(update: $update, where: $where) {
-    pixels {
-      position
-      color
+    mutation UpdatePixels($update: PixelUpdateInput, $where: PixelWhere) {
+        updatePixels(update: $update, where: $where) {
+            pixels {
+                position
+                color
+            }
+        }
     }
-  }
-}
 `;
 
 const canvasApi = new CanvasApi("place", 10, (pixelClicked) => {
-    canvasApi.drawPixel(pixelClicked, color)
+    canvasApi.drawPixel(pixelClicked, color);
     const params = {
         update: {
-            color: color
+            color: color,
         },
         where: {
-            position: [`${pixelClicked[0]}`, `${pixelClicked[1]}`]
-        }
-    }
-    client
-        .mutation(updatePixelQuery, params)
-        .toPromise()
-})
-
+            position: [pixelClicked[0], pixelClicked[1]],
+        },
+    };
+    client.mutation(updatePixelQuery, params).toPromise();
+});
 
 const canvasQuery = gql`
-query Canvas {
-  canvas
-}
+    query Canvas {
+        canvas
+    }
 `;
 
 client
     .query(canvasQuery)
     .toPromise()
-    .then(result => {
-        console.log(result.data.canvas)
-        let i=0;
-        let j=0;
+    .then((result) => {
+        console.log(result.data.canvas);
+        let i = 0;
+        let j = 0;
         for (const pixel of result.data.canvas) {
-            canvasApi.drawPixel([i,j], pixel);
+            canvasApi.drawPixel([i, j], pixel);
             j++;
-            if(j===30){
+            if (j === 30) {
                 i++;
-                j=0;
+                j = 0;
             }
         }
-
     });
 
-
 const pixelsSubscription = gql`
-subscription Subscription {
-  pixelUpdated {
-    updatedPixel {
-      position
-      color
+    subscription Subscription {
+        pixelUpdated {
+            updatedPixel {
+                position
+                color
+            }
+        }
     }
-  }
-}
-    `;
+`;
 
-const {
-    unsubscribe
-} = pipe(
+const { unsubscribe } = pipe(
     client.subscription(pixelsSubscription),
-    subscribe(result => {
-        const updatedPixel = result.data.pixelUpdated.updatedPixel
+    subscribe((result) => {
+        const updatedPixel = result.data.pixelUpdated.updatedPixel;
 
-        canvasApi.drawPixel(updatedPixel.position, updatedPixel.color)
+        canvasApi.drawPixel(updatedPixel.position, updatedPixel.color);
     })
 );
