2c7eb45d324263b420d5318f28b8809ce73dca68
Darrell Warde <darrell.warde@neotechnology.com>
2022-05-16 21:53:12 +0100

    Server changes

--- examples/neo-place/server.js
+++ examples/neo-place/server.js
@@ -1,49 +1,39 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const { createServer } = require("http");
-const { EventEmitter } = require('events');
-const neo4j = require('neo4j-driver');
-const { Neo4jGraphQL } = require('@neo4j/graphql');
+const { Neo4jGraphQL } = require("@neo4j/graphql");
 const { WebSocketServer } = require("ws");
 const { useServer } = require("graphql-ws/lib/use/ws");
-const express = require('express');
-const  { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
-const setupMap=require('./map-setup')
-// Local Subscriptions
-class SubscriptionsPlugin {
-    constructor() {
-        this.events = new EventEmitter();
-    }
 
-    publish(eventMeta) {
-        this.events.emit(eventMeta.event, eventMeta);
-    }
-}
-
-const NEO4J_URL = "bolt://localhost:7687"
-const NEO4J_USER = "neo4j"
-const NEO4J_PASSWORD = "dontpanic42"
+const setupMap = require("./map-setup");
+const { getDriver } = require("./get-driver");
+const { getPlugin } = require("./get-plugin");
 
 // Load type definitions
-const typeDefs = fs.readFileSync(path.join(__dirname, "typedefs.graphql"), 'utf-8');
-
-
-const driver = neo4j.driver(NEO4J_URL, neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD));
-
-const neoSchema = new Neo4jGraphQL({
-    typeDefs: typeDefs,
-    driver,
-    plugins: {
-        subscriptions: new SubscriptionsPlugin(), // Add plugin
-    },
-});
+const typeDefs = fs.readFileSync(path.join(__dirname, "typedefs.graphql"), "utf-8");
 
 async function main() {
+    const driver = await getDriver();
+
+    const plugin = await getPlugin();
+
+    const neoSchema = new Neo4jGraphQL({
+        typeDefs: typeDefs,
+        driver,
+        plugins: {
+            subscriptions: plugin,
+        },
+    });
+
     await setupMap(30);
     // Apollo server setup
     const app = express();
-    app.use(express.static('dist'))
+
+    app.use(express.static("dist"));
+
     const httpServer = createServer(app);
 
     // Setup websocket server on top of express http server
@@ -54,15 +44,21 @@ async function main() {
 
     // Build Neo4j/graphql schema
     const schema = await neoSchema.getSchema();
-    const serverCleanup = useServer({
-        schema
-    }, wsServer);
+
+    await neoSchema.assertIndexesAndConstraints({ options: { create: true } });
+
+    const serverCleanup = useServer(
+        {
+            schema,
+        },
+        wsServer
+    );
 
     const server = new ApolloServer({
         schema,
         plugins: [
             ApolloServerPluginDrainHttpServer({
-                httpServer
+                httpServer,
             }),
             {
                 async serverWillStart() {
@@ -77,10 +73,10 @@ async function main() {
     });
     await server.start();
     server.applyMiddleware({
-        app
+        app,
     });
 
-    const PORT = 4000;
+    const PORT = process.env.PORT || 4000;
     httpServer.listen(PORT, () => {
         console.log(`Server is now running on http://localhost:${PORT}/graphql`);
     });
