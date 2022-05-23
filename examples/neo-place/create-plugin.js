3a10233305fcec230d1ee1694046daede6b35551
angrykoala <angrykoala@outlook.es>
2022-05-23 12:42:00 +0100

    Add listener events

--- examples/neo-place/create-plugin.js
+++ examples/neo-place/create-plugin.js
@@ -5,6 +5,10 @@ const { getSecret } = require("./get-secret");
 module.exports.createPlugin = async function () {
     if (process.env.NODE_ENV === "production") {
         const plugin = new Neo4jGraphQLSubscriptionsAMQP();
+        plugin.events.setMaxListeners(0);
+        setInterval(()=>{
+            console.log("Events Connections", plugin.events.listenerCount("update"))
+        }, 5000);
 
         const url = await getSecret("team-graphql", "NEO_PLACE_AMQP_URL");
 
@@ -16,6 +20,11 @@ module.exports.createPlugin = async function () {
     class SubscriptionsPlugin {
         constructor() {
             this.events = new EventEmitter();
+            this.events.setMaxListeners(0);
+            setInterval(()=>{
+                console.log("Events Connections", this.events.listenerCount("update"))
+            }, 5000);
+
         }
 
         publish(eventMeta) {
