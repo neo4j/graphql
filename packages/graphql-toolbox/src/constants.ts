20f1fd956777f3eea34dc23d615fa2f4020b0622
Thomas Wiss <thomas.wiss@hotmail.com>
2022-06-08 08:59:46 +0200

    BREAKING: changing the storage key for favorites

--- packages/graphql-toolbox/src/constants.ts
+++ packages/graphql-toolbox/src/constants.ts
@@ -28,7 +28,7 @@ export const LOCAL_STATE_CREATE_CONSTRAINT = "neo4j.graphql.createConstraint";
 export const LOCAL_STATE_CONSTRAINT = "neo4j.graphql.constraint";
 export const LOCAL_STATE_EDITOR_THEME = "neo4j.graphql.editorTheme";
 export const LOCAL_STATE_ENABLE_REGEX = "neo4j.graphql.enable.regex";
-export const LOCAL_STATE_FAVORITES = "neo4j.graphql.favourites";
+export const LOCAL_STATE_FAVORITES = "neo4j.graphql.favorites";
 export const LOCAL_STATE_SHOW_LINT_MARKERS = "neo4j.graphql.showLintMarkers";
 export const LOCAL_STATE_SELECTED_DATABASE_NAME = "neo4j.graphql.selectedDatabaseName";
 
