d4bb74d25aa839b9db50641032a9356a7ce2c384
Thomas Wiss <thomas.wiss@neotechnology.com>
2022-07-12 14:20:58 +0200

    feat: reduce E2E test flakiness

--- packages/graphql-toolbox/tests/urlqueryparameters.test.ts
+++ packages/graphql-toolbox/tests/urlqueryparameters.test.ts
@@ -23,16 +23,17 @@ import { test, describe, expect, beforeAll, afterAll } from "./utils/pagemodel";
 
 const { NEO_USER = "admin", NEO_PASSWORD = "password", NEO_URL = "neo4j://localhost:7687/neo4j" } = process.env;
 
-const DATABASE_NAME = "graphqltoolboxe2etestdatabase";
-
 describe("URL query parameters", () => {
+    const randomString = Math.random().toString(36).slice(2, 8);
+    const databaseName = `graphqltoolboxe2etestdb${randomString}`;
+
     let driver: neo4j.Driver;
 
     beforeAll(async () => {
         driver = neo4j.driver(NEO_URL, neo4j.auth.basic(NEO_USER, NEO_PASSWORD));
         const session = await driver.session();
         try {
-            await session.run(`CREATE OR REPLACE DATABASE ${DATABASE_NAME}`);
+            await session.run(`CREATE OR REPLACE DATABASE ${databaseName}`);
         } catch (err) {
             throw new Error(`Failed to create database for test run. Error: ${err}`);
         } finally {
@@ -43,7 +44,7 @@ describe("URL query parameters", () => {
     afterAll(async () => {
         const session = await driver.session();
         try {
-            await session.run(`DROP DATABASE ${DATABASE_NAME} IF EXISTS`);
+            await session.run(`DROP DATABASE ${databaseName} IF EXISTS`);
         } catch (err) {
             throw new Error(`Failed to drop database after test run. Error: ${err}`);
         } finally {
@@ -79,7 +80,7 @@ describe("URL query parameters", () => {
     });
 
     test("should select the database from provided url query parameter", async ({ page, topBarPage }) => {
-        await page.goto(`/?db=${DATABASE_NAME}`);
+        await page.goto(`/?db=${databaseName}`);
 
         const login = new Login(page);
         await login.login();
@@ -89,6 +90,6 @@ describe("URL query parameters", () => {
 
         const selectedDatabase = await topBarPage.getSelectedDatabase();
 
-        expect(selectedDatabase).toEqual(DATABASE_NAME);
+        expect(selectedDatabase).toEqual(databaseName);
     });
 });
