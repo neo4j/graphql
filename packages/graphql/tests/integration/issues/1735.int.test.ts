f20962c20e72bfec2accd7a4c29c2d5b7b253dab
Nick Sethi <nicholas.sethi@gmail.com>
2022-07-12 20:57:48 -0700

    apply tbwiss test suggestions

--- packages/graphql/tests/integration/issues/1735.int.test.ts
+++ packages/graphql/tests/integration/issues/1735.int.test.ts
@@ -17,79 +17,95 @@
  * limitations under the License.
  */
 
-import { graphql } from "graphql";
-import { Driver } from "neo4j-driver";
-import neo4j from "../neo4j";
+import { graphql, GraphQLSchema } from "graphql";
+import type { Driver } from "neo4j-driver";
 import { Neo4jGraphQL } from "../../../src";
 import { generateUniqueType } from "../../utils/graphql-types";
+import Neo4j from "../neo4j";
 
 describe("https://github.com/neo4j/graphql/issues/1735", () => {
+    const actorType = generateUniqueType("Actor");
+    const movieType = generateUniqueType("Movie");
+
+    let schema: GraphQLSchema;
     let driver: Driver;
-    let neoSchema: Neo4jGraphQL;
+    let neo4j: Neo4j;
 
-    const testMovie = generateUniqueType("Movie");
-    const testActor = generateUniqueType("Actor");
-
-    const typeDefs = `
-    interface MovieActorEdgeProperties @relationshipProperties {
-      isLead: Boolean
-    }
-
-    type ${testMovie.name} {
-        dbId: ID! @id(global: true) @alias(property: "id")
-        title: String!
-        actors: [${testActor.name}!]! @relationship(type: "ACTED_IN", direction: IN, properties: "MovieActorEdgeProperties")
-        leadActorsCount: Int! @cypher(statement:"""
-          MATCH (this)<-[rel:ACTED_IN]-(a:${testActor.name})
-          WHERE rel.isLead = true
-          RETURN count(a)
-        """)
-    }
-
-    type ${testActor.name} {
-        dbId: ID! @id(global: true) @alias(property: "id")
-        name: String!
-        movies: [${testMovie.name}!]! @relationship(type: "ACTED_IN", direction: OUT, properties: "MovieActorEdgeProperties")
-    }
-  `;
     beforeAll(async () => {
-        driver = await neo4j();
-        neoSchema = new Neo4jGraphQL({ typeDefs, driver });
-        await neoSchema.checkNeo4jCompat();
-    });
-    afterAll(async () => {
-        await driver.close();
-    });
-    test("root connection queries on nodes with Cypher fields should execute without a sort argument", async () => {
-        const session = driver.session();
+        neo4j = new Neo4j();
+        driver = await neo4j.getDriver();
+        const typeDefs = `
+          interface MovieActorEdgeProperties @relationshipProperties {
+            isLead: Boolean
+          }
 
-        const mutation = `
-        mutation($input: [${testMovie.name}CreateInput!]!) {
-          ${testMovie.operations.create}(input: $input) {
-            ${testMovie.plural} {
+          type ${movieType.name} {
+              dbId: ID! @id(global: true) @alias(property: "id")
+              title: String!
+              actors: [${actorType.name}!]! @relationship(type: "ACTED_IN", direction: IN, properties: "MovieActorEdgeProperties")
+              leadActorsCount: Int! @cypher(statement:"""
+                MATCH (this)<-[rel:ACTED_IN]-(a:${actorType.name})
+                WHERE rel.isLead = true
+                RETURN count(a)
+              """)
+          }
+
+          type ${actorType.name} {
+              dbId: ID! @id(global: true) @alias(property: "id")
+              name: String!
+              movies: [${movieType.name}!]! @relationship(type: "ACTED_IN", direction: OUT, properties: "MovieActorEdgeProperties")
+          }
+  `;
+        const neoGraphql = new Neo4jGraphQL({ typeDefs, driver });
+        schema = await neoGraphql.getSchema();
+    });
+
+    beforeEach(async () => {
+        const source = `
+        mutation($input: [${movieType.name}CreateInput!]!) {
+          ${movieType.operations.create}(input: $input) {
+            ${movieType.plural} {
               id
               leadActorsCount
             }
           }
         }
       `;
-        const mutationVars = {
-            input: [
-                {
-                    title: "A River Runs Through It",
-                    actors: {
-                        create: {
-                            edge: { isLead: false },
-                            node: { name: "River Phoenix" },
-                        },
+        const input = [
+            {
+                title: "A River Runs Through It",
+                actors: {
+                    create: {
+                        edge: { isLead: false },
+                        node: { name: "River Phoenix" },
                     },
                 },
-            ],
-        };
+            },
+        ];
+        await graphql({
+            source,
+            schema,
+            contextValue: { driver },
+            variableValues: { input },
+        });
+    });
 
+    afterEach(async () => {
+        const session = await neo4j.getSession();
+
+        await session.run(`MATCH (a: ${actorType.name}) DETACH DELETE a`);
+        await session.run(`MATCH (m: ${movieType.name}) DETACH DELETE m`);
+
+        await session.close();
+    });
+
+    afterAll(async () => {
+        await driver.close();
+    });
+    test("root connection queries on nodes with Cypher fields should execute without a sort argument", async () => {
         const query = `
         query {
-          ${testMovie.operations.connection} {
+          ${movieType.operations.connection} {
             totalCount
             edges {
               node {
@@ -101,36 +117,24 @@ describe("https://github.com/neo4j/graphql/issues/1735", () => {
         }
       `;
 
-        try {
-            const schema = await neoSchema.getSchema();
-            await graphql({
-                schema,
-                source: mutation,
-                variableValues: mutationVars,
-                contextValue: { driver },
-            });
+        const result = await graphql({
+            schema,
+            source: query,
+            variableValues: {},
+            contextValue: { driver },
+        });
 
-            const result = await graphql({
-                schema,
-                source: query,
-                variableValues: {},
-                contextValue: { driver },
-            });
-
-            expect(result.errors).toBeFalsy();
-            expect(result?.data?.[testMovie.operations.connection]).toEqual({
-                totalCount: 1,
-                edges: [
-                    {
-                        node: {
-                            id: expect.any(String),
-                            leadActorsCount: 0,
-                        },
+        expect(result.errors).toBeFalsy();
+        expect(result?.data?.[movieType.operations.connection]).toEqual({
+            totalCount: 1,
+            edges: [
+                {
+                    node: {
+                        id: expect.any(String),
+                        leadActorsCount: 0,
                     },
-                ],
-            });
-        } finally {
-            await session.close();
-        }
+                },
+            ],
+        });
     });
 });
