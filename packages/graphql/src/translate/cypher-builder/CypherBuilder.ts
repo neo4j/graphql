47e84fcb24b147e880a080e4b7e0b3ca7d55089c
angrykoala <angrykoala@outlook.es>
2022-07-22 14:15:59 +0100

    Withwith class with a with function

--- packages/graphql/src/translate/cypher-builder/CypherBuilder.ts
+++ packages/graphql/src/translate/cypher-builder/CypherBuilder.ts
@@ -22,9 +22,9 @@ export { Match } from "./clauses/Match";
 export { Create } from "./clauses/Create";
 export { Merge } from "./clauses/Merge";
 export { Call } from "./clauses/Call";
-export { Return } from "./clauses/Return";
+export { Return } from "./clauses/projection/Return";
 export { RawCypher } from "./clauses/RawCypher";
-export { With } from "./clauses/With";
+export { With } from "./clauses/projection/With";
 
 // Clauses-like things
 export { Exists } from "./Exists";
