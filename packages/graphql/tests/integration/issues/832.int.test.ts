237eaeb9e78695531db9172a52f63e256a918978
Darrell Warde <darrell.warde@neotechnology.com>
2022-09-06 13:27:44 +0100

    Add more test cases

--- packages/graphql/tests/integration/issues/832.int.test.ts
+++ packages/graphql/tests/integration/issues/832.int.test.ts
@@ -31,7 +31,6 @@ describe("https://github.com/neo4j/graphql/issues/832", () => {
     let session: Session;
 
     const Person = generateUniqueType("Person");
-    const Place = generateUniqueType("Place");
     const Interaction = generateUniqueType("Interaction");
 
     async function graphqlQuery(query: string) {
@@ -56,11 +55,6 @@ describe("https://github.com/neo4j/graphql/issues/832", () => {
                 name: String!
             }
 
-            type ${Place} implements Entity {
-                id: String! @unique
-                location: Point!
-            }
-
             type ${Interaction} {
                 id: ID! @id
                 kind: String!
@@ -75,12 +69,88 @@ describe("https://github.com/neo4j/graphql/issues/832", () => {
         schema = await neoGraphql.getSchema();
     });
 
+    beforeEach(async () => {
+        await session.run(`
+            CREATE (:${Person.name} { id: "adam", name: "Adam" })
+            CREATE (:${Person.name} { id: "eve", name: "Eve" })
+            CREATE (:${Person.name} { id: "cain", name: "Cain" })
+            CREATE (:${Person.name} { id: "abel", name: "Abel" })
+        `);
+    });
+
+    afterEach(async () => {
+        await session.run(`
+            CALL {
+                MATCH (p:${Person.name})
+                DETACH DELETE p
+            }
+            CALL {
+                MATCH (i:${Interaction.name})
+                DETACH DELETE i
+            }
+        `);
+    });
+
     afterAll(async () => {
         await session.close();
         await driver.close();
     });
 
-    test("should not throw error when querying nested relations under a root connection field", async () => {
+    test("should not create duplicate nodes when creating multiple interactions in separate mutations", async () => {
+        const mutation0 = `
+            mutation {
+                ${Interaction.operations.create}(
+                    input: [
+                        {
+                            subjects: { connect: { where: { node: { id_IN: ["adam", "eve"] } } } }
+                            kind: "PARENT_OF"
+                            objects: { connect: { where: { node: { id_IN: ["cain"] } } } }
+                        }
+                    ]
+                ) {
+                    info {
+                        nodesCreated
+                        relationshipsCreated
+                    }
+                    ${Interaction.plural} {
+                        id
+                    }
+                }
+            }
+        `;
+
+        const mutation0Result = await graphqlQuery(mutation0);
+        expect((mutation0Result.data?.[Interaction.operations.create] as any).info.nodesCreated).toBe(1);
+        expect((mutation0Result.data?.[Interaction.operations.create] as any).info.relationshipsCreated).toBe(3);
+
+        const mutation1 = `
+            mutation {
+                ${Interaction.operations.create}(
+                    input: [
+                        {
+                            subjects: { connect: { where: { node: { id_IN: ["adam", "eve"] } } } }
+                            kind: "PARENT_OF"
+                            objects: { connect: { where: { node: { id_IN: ["abel"] } } } }
+                        }
+                    ]
+                ) {
+                    info {
+                        nodesCreated
+                        relationshipsCreated
+                    }
+                    ${Interaction.plural} {
+                        id
+                    }
+                }
+            }
+        `;
+
+        const mutation1Result = await graphqlQuery(mutation1);
+        expect((mutation1Result.data?.[Interaction.operations.create] as any).info.nodesCreated).toBe(1);
+        expect((mutation1Result.data?.[Interaction.operations.create] as any).info.relationshipsCreated).toBe(3);
+    });
+
+    test("should not create duplicate nodes when creating multiple interactions in one", async () => {
         const createPeople = `
             mutation {
                 ${Person.operations.create}(
@@ -106,7 +176,40 @@ describe("https://github.com/neo4j/graphql/issues/832", () => {
             { id: "abel" },
         ]);
 
-        const createInteractions = `
+        const mutation = `
+            mutation {
+                ${Interaction.operations.create}(
+                    input: [
+                        {
+                            subjects: { connect: { where: { node: { id_IN: ["adam", "eve"] } } } }
+                            kind: "PARENT_OF"
+                            objects: { connect: { where: { node: { id_IN: ["cain"] } } } }
+                        }
+                        {
+                            subjects: { connect: { where: { node: { id_IN: ["adam", "eve"] } } } }
+                            kind: "PARENT_OF"
+                            objects: { connect: { where: { node: { id_IN: ["abel"] } } } }
+                        }
+                    ]
+                ) {
+                    info {
+                        nodesCreated
+                        relationshipsCreated
+                    }
+                    ${Interaction.plural} {
+                        id
+                    }
+                }
+            }
+        `;
+
+        const mutationResult = await graphqlQuery(mutation);
+        expect((mutationResult.data?.[Interaction.operations.create] as any).info.nodesCreated).toBe(2);
+        expect((mutationResult.data?.[Interaction.operations.create] as any).info.relationshipsCreated).toBe(6);
+    });
+
+    test("should not throw error when querying nested relations under a root connection field", async () => {
+        const mutation = `
             mutation {
                 ${Interaction.operations.create}(
                     input: [
@@ -138,7 +241,7 @@ describe("https://github.com/neo4j/graphql/issues/832", () => {
             }
         `;
 
-        const createInteractionsResult = await graphqlQuery(createInteractions);
-        expect((createInteractionsResult.data?.[Interaction.operations.create] as any).info.nodesCreated).toBe(2);
+        const mutationResult = await graphqlQuery(mutation);
+        expect(mutationResult.errors).toBeUndefined();
     });
 });
--- packages/graphql/tests/tck/issues/832.test.ts
+++ packages/graphql/tests/tck/issues/832.test.ts
@@ -55,7 +55,7 @@ describe("https://github.com/neo4j/graphql/issues/832", () => {
         });
     });
 
-    test("should produce Cypher which doesn't create duplicate nodes", async () => {
+    test("should produce Cypher which doesn't create duplicate nodes, only selecting created nodes", async () => {
         const query = gql`
             mutation {
                 createInteractions(
@@ -188,6 +188,226 @@ describe("https://github.com/neo4j/graphql/issues/832", () => {
             RETURN [
             this0 { .id },
             this1 { .id }] AS data"
+        `);
+
+        expect(formatParams(result.params)).toMatchInlineSnapshot(`
+            "{
+                \\"this0_kind\\": \\"PARENT_OF\\",
+                \\"this0_subjects_connect0_node_param0\\": [
+                    \\"adam\\",
+                    \\"eve\\"
+                ],
+                \\"this0_objects_connect0_node_param0\\": [
+                    \\"cain\\"
+                ],
+                \\"this1_kind\\": \\"PARENT_OF\\",
+                \\"this1_subjects_connect0_node_param0\\": [
+                    \\"adam\\",
+                    \\"eve\\"
+                ],
+                \\"this1_objects_connect0_node_param0\\": [
+                    \\"abel\\"
+                ],
+                \\"resolvedCallbacks\\": {}
+            }"
+        `);
+    });
+
+    test("should produce Cypher which doesn't create duplicate nodes, selecting related nodes", async () => {
+        const query = gql`
+            mutation {
+                createInteractions(
+                    input: [
+                        {
+                            subjects: { connect: { where: { node: { id_IN: ["adam", "eve"] } } } }
+                            kind: "PARENT_OF"
+                            objects: { connect: { where: { node: { id_IN: ["cain"] } } } }
+                        }
+                        {
+                            subjects: { connect: { where: { node: { id_IN: ["adam", "eve"] } } } }
+                            kind: "PARENT_OF"
+                            objects: { connect: { where: { node: { id_IN: ["abel"] } } } }
+                        }
+                    ]
+                ) {
+                    info {
+                        nodesCreated
+                    }
+                    interactions {
+                        id
+                        subjects {
+                            id
+                        }
+                        objects {
+                            id
+                        }
+                    }
+                }
+            }
+        `;
+
+        const result = await translateQuery(neoSchema, query);
+
+        expect(formatCypher(result.cypher)).toMatchInlineSnapshot(`
+            "CALL {
+            CREATE (this0:Interaction)
+            SET this0.id = randomUUID()
+            SET this0.kind = $this0_kind
+            WITH this0
+            CALL {
+            	WITH this0
+            	OPTIONAL MATCH (this0_subjects_connect0_node:Person)
+            	WHERE this0_subjects_connect0_node.id IN $this0_subjects_connect0_node_param0
+            	FOREACH(_ IN CASE WHEN this0 IS NULL THEN [] ELSE [1] END |
+            		FOREACH(_ IN CASE WHEN this0_subjects_connect0_node IS NULL THEN [] ELSE [1] END |
+            			MERGE (this0)<-[:ACTED_IN]-(this0_subjects_connect0_node)
+            		)
+            	)
+            	RETURN count(*) AS _
+            UNION
+            	WITH this0
+            	OPTIONAL MATCH (this0_subjects_connect0_node:Place)
+            	WHERE this0_subjects_connect0_node.id IN $this0_subjects_connect0_node_param0
+            	FOREACH(_ IN CASE WHEN this0 IS NULL THEN [] ELSE [1] END |
+            		FOREACH(_ IN CASE WHEN this0_subjects_connect0_node IS NULL THEN [] ELSE [1] END |
+            			MERGE (this0)<-[:ACTED_IN]-(this0_subjects_connect0_node)
+            		)
+            	)
+            	RETURN count(*) AS _
+            }
+            WITH this0
+            CALL {
+            	WITH this0
+            	OPTIONAL MATCH (this0_objects_connect0_node:Person)
+            	WHERE this0_objects_connect0_node.id IN $this0_objects_connect0_node_param0
+            	FOREACH(_ IN CASE WHEN this0 IS NULL THEN [] ELSE [1] END |
+            		FOREACH(_ IN CASE WHEN this0_objects_connect0_node IS NULL THEN [] ELSE [1] END |
+            			MERGE (this0)-[:ACTED_IN]->(this0_objects_connect0_node)
+            		)
+            	)
+            	RETURN count(*) AS _
+            UNION
+            	WITH this0
+            	OPTIONAL MATCH (this0_objects_connect0_node:Place)
+            	WHERE this0_objects_connect0_node.id IN $this0_objects_connect0_node_param0
+            	FOREACH(_ IN CASE WHEN this0 IS NULL THEN [] ELSE [1] END |
+            		FOREACH(_ IN CASE WHEN this0_objects_connect0_node IS NULL THEN [] ELSE [1] END |
+            			MERGE (this0)-[:ACTED_IN]->(this0_objects_connect0_node)
+            		)
+            	)
+            	RETURN count(*) AS _
+            }
+            RETURN this0
+            }
+            CALL {
+            CREATE (this1:Interaction)
+            SET this1.id = randomUUID()
+            SET this1.kind = $this1_kind
+            WITH this1
+            CALL {
+            	WITH this1
+            	OPTIONAL MATCH (this1_subjects_connect0_node:Person)
+            	WHERE this1_subjects_connect0_node.id IN $this1_subjects_connect0_node_param0
+            	FOREACH(_ IN CASE WHEN this1 IS NULL THEN [] ELSE [1] END |
+            		FOREACH(_ IN CASE WHEN this1_subjects_connect0_node IS NULL THEN [] ELSE [1] END |
+            			MERGE (this1)<-[:ACTED_IN]-(this1_subjects_connect0_node)
+            		)
+            	)
+            	RETURN count(*) AS _
+            UNION
+            	WITH this1
+            	OPTIONAL MATCH (this1_subjects_connect0_node:Place)
+            	WHERE this1_subjects_connect0_node.id IN $this1_subjects_connect0_node_param0
+            	FOREACH(_ IN CASE WHEN this1 IS NULL THEN [] ELSE [1] END |
+            		FOREACH(_ IN CASE WHEN this1_subjects_connect0_node IS NULL THEN [] ELSE [1] END |
+            			MERGE (this1)<-[:ACTED_IN]-(this1_subjects_connect0_node)
+            		)
+            	)
+            	RETURN count(*) AS _
+            }
+            WITH this1
+            CALL {
+            	WITH this1
+            	OPTIONAL MATCH (this1_objects_connect0_node:Person)
+            	WHERE this1_objects_connect0_node.id IN $this1_objects_connect0_node_param0
+            	FOREACH(_ IN CASE WHEN this1 IS NULL THEN [] ELSE [1] END |
+            		FOREACH(_ IN CASE WHEN this1_objects_connect0_node IS NULL THEN [] ELSE [1] END |
+            			MERGE (this1)-[:ACTED_IN]->(this1_objects_connect0_node)
+            		)
+            	)
+            	RETURN count(*) AS _
+            UNION
+            	WITH this1
+            	OPTIONAL MATCH (this1_objects_connect0_node:Place)
+            	WHERE this1_objects_connect0_node.id IN $this1_objects_connect0_node_param0
+            	FOREACH(_ IN CASE WHEN this1 IS NULL THEN [] ELSE [1] END |
+            		FOREACH(_ IN CASE WHEN this1_objects_connect0_node IS NULL THEN [] ELSE [1] END |
+            			MERGE (this1)-[:ACTED_IN]->(this1_objects_connect0_node)
+            		)
+            	)
+            	RETURN count(*) AS _
+            }
+            RETURN this1
+            }
+            WITH *
+            CALL {
+            WITH this0
+            CALL {
+                WITH this0
+                MATCH (this0)<-[:ACTED_IN]-(this0_Person:Person)
+                RETURN { __resolveType: \\"Person\\", id: this0_Person.id } AS subjects
+                UNION
+                WITH this0
+                MATCH (this0)<-[:ACTED_IN]-(this0_Place:Place)
+                RETURN { __resolveType: \\"Place\\", id: this0_Place.id } AS subjects
+            }
+            RETURN collect(subjects) AS subjects
+            }
+            WITH *
+            CALL {
+            WITH this0
+            CALL {
+                WITH this0
+                MATCH (this0)-[:ACTED_IN]->(this0_Person:Person)
+                RETURN { __resolveType: \\"Person\\", id: this0_Person.id } AS objects
+                UNION
+                WITH this0
+                MATCH (this0)-[:ACTED_IN]->(this0_Place:Place)
+                RETURN { __resolveType: \\"Place\\", id: this0_Place.id } AS objects
+            }
+            RETURN collect(objects) AS objects
+            }
+            WITH *
+            CALL {
+            WITH this1
+            CALL {
+                WITH this1
+                MATCH (this1)<-[:ACTED_IN]-(this1_Person:Person)
+                RETURN { __resolveType: \\"Person\\", id: this1_Person.id } AS subjects
+                UNION
+                WITH this1
+                MATCH (this1)<-[:ACTED_IN]-(this1_Place:Place)
+                RETURN { __resolveType: \\"Place\\", id: this1_Place.id } AS subjects
+            }
+            RETURN collect(subjects) AS subjects
+            }
+            WITH *
+            CALL {
+            WITH this1
+            CALL {
+                WITH this1
+                MATCH (this1)-[:ACTED_IN]->(this1_Person:Person)
+                RETURN { __resolveType: \\"Person\\", id: this1_Person.id } AS objects
+                UNION
+                WITH this1
+                MATCH (this1)-[:ACTED_IN]->(this1_Place:Place)
+                RETURN { __resolveType: \\"Place\\", id: this1_Place.id } AS objects
+            }
+            RETURN collect(objects) AS objects
+            }
+            RETURN [
+            this0 { .id, subjects: subjects, objects: objects },
+            this1 { .id, subjects: subjects, objects: objects }] AS data"
         `);
 
         expect(formatParams(result.params)).toMatchInlineSnapshot(`
