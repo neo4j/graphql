ef64faa3299146b1379f4ee8d18b7166c0707283
Darrell Warde <darrell.warde@neotechnology.com>
2022-10-06 15:01:01 +0100

    Fix repo configuration

--- packages/graphql-toolbox/jest.config.js
+++ packages/graphql-toolbox/jest.config.js
@@ -6,9 +6,12 @@ module.exports = {
     displayName: "@neo4j/graphql-toolbox",
     roots: ["<rootDir>/packages/graphql-toolbox/src/", "<rootDir>/packages/graphql-toolbox/tests/"],
     coverageDirectory: "<rootDir>/packages/graphql-toolbox/coverage/",
-    globals: {
-        "ts-jest": {
-            tsconfig: "<rootDir>/packages/graphql-toolbox/tests/tsconfig.json",
-        },
+    transform: {
+        "^.+\\.ts$": [
+            "ts-jest",
+            {
+                tsconfig: "<rootDir>/packages/graphql-toolbox/tests/tsconfig.json",
+            },
+        ],
     },
 };
