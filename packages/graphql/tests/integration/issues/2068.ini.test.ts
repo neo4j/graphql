836a4dfaaca1a3c002e37470dd2404bc635300bc
Liam Doodson <liam.doodson@neo4j.com>
2022-10-07 14:14:34 +0100

    Test unions in cypher directivve

--- packages/graphql/tests/integration/issues/2068.ini.test.ts
+++ packages/graphql/tests/integration/issues/2068.ini.test.ts
@@ -24,53 +24,9 @@ import { Neo4jGraphQL } from "../../../src/classes";
 import Neo4j from "../neo4j";
 import { createJwtRequest } from "../../utils/create-jwt-request";
 
-const testTypeDefs = gql`
-    interface Content {
-        id: ID
-        content: String
-        creator: User! @relationship(type: "HAS_CONTENT", direction: IN)
-    }
-
-    type User {
-        id: ID
-        name: String
-        content: [Content!]! @relationship(type: "HAS_CONTENT", direction: OUT)
-    }
-
-    type Comment implements Content {
-        id: ID
-        content: String
-        creator: User!
-    }
-
-    type Post implements Content
-        @auth(
-            rules: [{ operations: [READ, UPDATE, DELETE, CONNECT, DISCONNECT], where: { creator: { id: "$jwt.sub" } } }]
-        ) {
-        id: ID
-        content: String
-        creator: User!
-    }
-
-    extend type User
-        @auth(rules: [{ operations: [READ, UPDATE, DELETE, CONNECT, DISCONNECT], where: { id: "$jwt.sub" } }])
-
-    extend type User {
-        password: String! @auth(rules: [{ operations: [READ], where: { id: "$jwt.sub" } }])
-    }
-
-    extend type Post {
-        secretKey: String! @auth(rules: [{ operations: [READ], where: { creator: { id: "$jwt.sub" } } }])
-    }
-`;
-
 describe("https://github.com/neo4j/graphql/pull/2068", () => {
     let driver: Driver;
     let neo4j: Neo4j;
-    const secret = "secret";
-    const jwtPlugin = new Neo4jGraphQLAuthJWTPlugin({
-        secret: "secret",
-    });
 
     beforeAll(async () => {
         neo4j = new Neo4j();
@@ -80,11 +36,40 @@ describe("https://github.com/neo4j/graphql/pull/2068", () => {
     afterAll(async () => {
         await driver.close();
     });
-    describe("Missing with in update calls", () => {
+    describe("Updates within updates", () => {
+        const secret = "secret";
+        const jwtPlugin = new Neo4jGraphQLAuthJWTPlugin({
+            secret: "secret",
+        });
+
+        const typeDefs = gql`
+            interface Content {
+                id: ID
+                content: String
+                creator: User! @relationship(type: "HAS_CONTENT", direction: IN)
+            }
+
+            type User {
+                id: ID
+                name: String
+                content: [Content!]! @relationship(type: "HAS_CONTENT", direction: OUT)
+            }
+
+            type Comment implements Content {
+                id: ID
+                content: String
+                creator: User!
+            }
+
+            extend type User
+                @auth(rules: [{ operations: [READ, UPDATE, DELETE, CONNECT, DISCONNECT], where: { id: "$jwt.sub" } }])
+
+            extend type User {
+                password: String! @auth(rules: [{ operations: [READ], where: { id: "$jwt.sub" } }])
+            }
+        `;
         test("Connect node - update within an update", async () => {
             const session = await neo4j.getSession({ defaultAccessMode: "WRITE" });
-
-            const typeDefs = testTypeDefs;
 
             const userID = "someID";
             const contentID = "someContentID";
@@ -128,8 +113,6 @@ describe("https://github.com/neo4j/graphql/pull/2068", () => {
         });
         test("Connect node - user defined update where within an update", async () => {
             const session = await neo4j.getSession({ defaultAccessMode: "WRITE" });
-
-            const typeDefs = testTypeDefs;
 
             const userID1 = "someID";
             const userID2 = "differentID";
@@ -176,8 +159,6 @@ describe("https://github.com/neo4j/graphql/pull/2068", () => {
         test("Disconnect node - update within an update", async () => {
             const session = await neo4j.getSession({ defaultAccessMode: "WRITE" });
 
-            const typeDefs = testTypeDefs;
-
             const userId = "someID";
             const contentId = "someContentID";
 
@@ -220,8 +201,6 @@ describe("https://github.com/neo4j/graphql/pull/2068", () => {
         test("Disconnect node - user defined update where within an update", async () => {
             const session = await neo4j.getSession({ defaultAccessMode: "WRITE" });
 
-            const typeDefs = testTypeDefs;
-
             const userId = "someID";
             const contentId = "someContentID";
 
@@ -261,6 +240,154 @@ describe("https://github.com/neo4j/graphql/pull/2068", () => {
                 await session.close();
             }
         });
-        test("")
+    });
+    test("Unions in cypher directives", async () => {
+        const actorName = "someName"
+        const actorAge = 43
+        const numberOfSeasons = 2
+        const sharedTitle = "someTitle"
+
+        const typeDefs = `
+            type Actor {
+                name: String
+                age: Int
+                movies(title: String): [Movie]
+                    @cypher(
+                        statement: """
+                        MATCH (m:Movie {title: $title})
+                        RETURN m
+                        """
+                    )
+
+                tvShows(title: String): [Movie]
+                    @cypher(
+                        statement: """
+                        MATCH (t:TVShow {title: $title})
+                        RETURN t
+                        """
+                    )
+
+                movieOrTVShow(title: String): [MovieOrTVShow]
+                    @cypher(
+                        statement: """
+                        MATCH (n)
+                        WHERE (n:TVShow OR n:Movie) AND ($title IS NULL OR n.title = $title)
+                        RETURN n
+                        """
+                    )
+            }
+
+            union MovieOrTVShow = Movie | TVShow
+
+            type TVShow {
+                id: ID
+                title: String
+                numSeasons: Int
+                actors: [Actor]
+                    @cypher(
+                        statement: """
+                        MATCH (a:Actor)
+                        RETURN a
+                        """
+                    )
+                topActor: Actor
+                    @cypher(
+                        statement: """
+                        MATCH (a:Actor)
+                        RETURN a
+                        """
+                    )
+            }
+
+            type Movie {
+                id: ID
+                title: String
+                actors: [Actor]
+                    @cypher(
+                        statement: """
+                        MATCH (a:Actor)
+                        RETURN a
+                        """
+                    )
+                topActor: Actor
+                    @cypher(
+                        statement: """
+                        MATCH (a:Actor)
+                        RETURN a
+                        """
+                    )
+            }
+        `;
+
+        const query = `
+            {
+                actors {
+                    movieOrTVShow(title: "${sharedTitle}") {
+                        ... on Movie {
+                            title
+                            topActor {
+                                name
+                                age
+                            }
+                            actors {
+                                name
+                            }
+                        }
+                        ... on TVShow {
+                            title
+                            numSeasons
+                            topActor {
+                                name
+                            }
+                        }
+                    }
+                }
+            }
+        `;
+        const session = await neo4j.getSession({ defaultAccessMode: "WRITE" });
+        const neoSchema = new Neo4jGraphQL({ typeDefs });
+
+        try {
+            await session.run(`
+                CREATE (:Actor { name: "${actorName}", age: ${actorAge} })
+                CREATE (:TVShow { title: "${sharedTitle}", numSeasons: ${numberOfSeasons} })
+                CREATE (:Movie { title: "${sharedTitle}" })
+            `);
+
+            const gqlResult = await graphql({
+                schema: await neoSchema.getSchema(),
+                source: query,
+                contextValue: neo4j.getContextValuesWithBookmarks(session.lastBookmarks()),
+            });
+
+            expect(gqlResult.errors).toBeUndefined();
+            expect(gqlResult.data).toEqual({
+                actors: [{
+                    movieOrTVShow: [
+                        {
+                          title: "someTitle",
+                          numSeasons: 2,
+                          topActor: {
+                            name: "someName",
+                          },
+                        },
+                        {
+                          title: "someTitle",
+                          topActor: {
+                            name: "someName",
+                            age: 43,
+                          },
+                          actors: [
+                            {
+                              name: "someName",
+                            },
+                          ],
+                        },
+                    ]
+                }]
+            });
+        } finally {
+            await session.close();
+        }
     });
 });
