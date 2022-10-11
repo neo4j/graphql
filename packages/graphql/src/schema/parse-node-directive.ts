21de311cbe22189410230445d2e213e9bee963df
Liam Doodson <liam.doodson@neo4j.com>
2022-10-11 16:26:23 +0100

    Only show deprecation warning once

--- packages/graphql/src/schema/parse-node-directive.ts
+++ packages/graphql/src/schema/parse-node-directive.ts
@@ -21,14 +21,17 @@ import type { DirectiveNode} from "graphql";
 import { valueFromASTUntyped } from "graphql";
 import { NodeDirective } from "../classes/NodeDirective";
 
+let pluralDeprecationWarningShown = false;
+
 function parseNodeDirective(nodeDirective: DirectiveNode | undefined) {
     if (!nodeDirective || nodeDirective.name.value !== "node") {
         throw new Error("Undefined or incorrect directive passed into parseNodeDirective function");
     }
 
     const plural = getArgumentValue<string>(nodeDirective, "plural");
-    if (plural) {
+    if (plural && !pluralDeprecationWarningShown) {
         console.warn("The plural argument has been deprecated and will be removed in version 4.0. Please use the @plural directive instead.");
+        pluralDeprecationWarningShown = true;
     }
 
     return new NodeDirective({
