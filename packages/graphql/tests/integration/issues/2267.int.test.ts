31a3da20c0cbe9d5f90f254c22a55ff9c1b05789
angrykoala <angrykoala@outlook.es>
2022-10-21 16:54:23 +0100

    Add tests for #2267

---
+++ packages/graphql/tests/integration/issues/2267.int.test.ts
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { Driver, Session } from "neo4j-driver";
+import { graphql } from "graphql";
+import Neo4j from "../neo4j";
+import { Neo4jGraphQL } from "../../../src/classes";
+import { generateUniqueType, UniqueType } from "../../utils/graphql-types";
+
+describe("https://github.com/neo4j/graphql/issues/2267", () => {
+    let driver: Driver;
+    let neo4j: Neo4j;
+    let neoSchema: Neo4jGraphQL;
+    let session: Session;
+
+    let Place: UniqueType;
+    let Post: UniqueType;
+    let Story: UniqueType;
+
+    beforeAll(async () => {
+        neo4j = new Neo4j();
+        driver = await neo4j.getDriver();
+    });
+
+    beforeEach(async () => {
+        Place = generateUniqueType("Place");
+        Post = generateUniqueType("Post");
+        Story = generateUniqueType("Story");
+
+        session = await neo4j.getSession();
+
+        const typeDefs = `
+            type ${Place} {
+                displayName: String!
+                activity: [Publication!]! @relationship(type: "ACTIVITY", direction: IN)
+            }
+
+            interface Publication {
+                name: String
+                activity: [${Place}!]! @relationship(type: "ACTIVITY", direction: OUT)
+            }
+
+            type ${Post} implements Publication {
+                name: String
+                activity: [${Place}!]!
+            }
+
+            type ${Story} implements Publication {
+                name: String
+                activity: [${Place}!]!
+            }
+        `;
+
+        await session.run(`
+        CREATE(:${Place} {displayName: "786 aa"})
+        CREATE(:${Place} {displayName: "8 à Huita"})
+        CREATE(:${Place} {displayName: "9ème Sauvagea"})<-[:ACTIVITY]-(:${Story} {name: "A story"})
+        CREATE(:${Place} {displayName: "A One Shopa"})
+        CREATE(:${Place} {displayName: "zaza"})
+        `);
+
+        neoSchema = new Neo4jGraphQL({
+            typeDefs,
+            driver,
+        });
+    });
+
+    afterEach(async () => {
+        await session.close();
+    });
+
+    afterAll(async () => {
+        await driver.close();
+    });
+
+    test("should correctly order when requesting only top-level fields", async () => {
+        const query = `
+        query {
+            ${Place.plural}(options: {sort: {displayName: ASC}}) {
+              displayName
+            }
+          }
+        `;
+
+        const result = await graphql({
+            schema: await neoSchema.getSchema(),
+            source: query,
+            contextValue: neo4j.getContextValues(),
+        });
+
+        expect(result.errors).toBeFalsy();
+        expect(result.data).toEqual({
+            [Place.plural]: [
+                {
+                    displayName: "786 aa",
+                },
+                {
+                    displayName: "8 à Huita",
+                },
+                {
+                    displayName: "9ème Sauvagea",
+                },
+                {
+                    displayName: "A One Shopa",
+                },
+                {
+                    displayName: "zaza",
+                },
+            ],
+        });
+    });
+
+    test.only("should correctly order when requesting nested interface level fields", async () => {
+        const query = `
+        query {
+            ${Place.plural}(options: {sort: {displayName: ASC}}) {
+              displayName
+              activity{
+                name
+              }
+            }
+          }
+        `;
+
+        const result = await graphql({
+            schema: await neoSchema.getSchema(),
+            source: query,
+            contextValue: neo4j.getContextValues(),
+        });
+
+        expect(result.errors).toBeFalsy();
+        expect(result.data).toEqual({
+            [Place.plural]: [
+                {
+                    displayName: "786 aa",
+                    activity: [],
+                },
+                {
+                    displayName: "8 à Huita",
+                    activity: [],
+                },
+                {
+                    displayName: "9ème Sauvagea",
+                    activity: [
+                        {
+                            name: "A story",
+                        },
+                    ],
+                },
+                {
+                    displayName: "A One Shopa",
+                    activity: [],
+                },
+                {
+                    displayName: "zaza",
+                    activity: [],
+                },
+            ],
+        });
+    });
+});
