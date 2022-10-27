e89e8e3edb6d349c9a3c39714f386bb1c4650a07
Thomas Wiss <thomas.wiss@neotechnology.com>
2022-10-27 13:29:45 +0200

    chore: trigger new build

--- packages/graphql-toolbox/src/modules/Main/Main.tsx
+++ packages/graphql-toolbox/src/modules/Main/Main.tsx
@@ -39,7 +39,7 @@ export const Main = () => {
                 : process.env.SEGMENT_GRAPHQL_TOOLBOX_DEV_SOURCE;
         if (!segmentKey) return;
         invokeSegmentAnalytics(segmentKey);
-        console.log("Initialized app");
+        console.log("Initialized app.");
     }, []);
 
     if (!auth.driver) {
