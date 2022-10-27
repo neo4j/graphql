3cd83ed1cc8ea494ecc684111e7b8d80bbc0a52c
MacondoExpress <simone.gammicchia@gmail.com>
2022-10-27 17:49:40 +0100

    add type Procedure to CypherBuilder

--- packages/cypher-builder/src/types.ts
+++ packages/cypher-builder/src/types.ts
@@ -27,7 +27,7 @@ import type { BooleanOp } from "./expressions/operations/boolean";
 import type { ComparisonOp } from "./expressions/operations/comparison";
 import type { RawCypher } from "./clauses/RawCypher";
 import type { PredicateFunction } from "./expressions/functions/PredicateFunctions";
-import type { ApocExpr, ApocPredicate } from "./expressions/procedures/apoc/apoc";
+import type { ApocExpr, ApocPredicate, ValidatePredicate } from "./expressions/procedures/apoc/apoc";
 import type { Case } from "./expressions/Case";
 import type { MathOp } from "./expressions/operations/math";
 import type { ListComprehension } from "./expressions/list/ListComprehension";
