163af39dfbf2cf53ed52d5268fce39e0d6e521eb
MacondoExpress <simone.gammicchia@gmail.com>
2022-10-28 17:02:50 +0100

    remove unused code

--- packages/graphql/src/translate/batch-create/GraphQLInputAST/GraphQLInputAST.ts
+++ packages/graphql/src/translate/batch-create/GraphQLInputAST/GraphQLInputAST.ts
@@ -18,10 +18,8 @@
  */
 
 import { AST } from "./AST";
-import { ConnectAST } from "./ConnectAST";
-import { ConnectOrCreateAST } from "./ConnectOrCreateAST";
 import { CreateAST } from "./CreateAST";
 import { NestedCreateAST } from "./NestedCreateAST";
 
-export { AST, ConnectAST, ConnectOrCreateAST, CreateAST, NestedCreateAST };
+export { AST, CreateAST, NestedCreateAST };
 export type { IAST, IConnectAST, IConnectOrCreateAST, ICreateAST, INestedCreateAST, Visitor } from "./types";
