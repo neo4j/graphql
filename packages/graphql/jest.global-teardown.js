3df2ee1b2bdc412f17b7b6dceb1411feb46e92c9
Darrell Warde <darrell.warde@neotechnology.com>
2022-10-28 11:39:16 +0100

    Fix integration tests

--- packages/graphql/jest.global-teardown.js
+++ packages/graphql/jest.global-teardown.js
@@ -1,7 +1,7 @@
 const neo4j = require("neo4j-driver");
 
 module.exports = async function globalTeardown() {
-    const { NEO_USER = "admin", NEO_PASSWORD = "password", NEO_URL = "neo4j://localhost:7687/neo4j" } = process.env;
+    const { NEO_USER = "neo4j", NEO_PASSWORD = "password", NEO_URL = "neo4j://localhost:7687/neo4j" } = process.env;
     const auth = neo4j.auth.basic(NEO_USER, NEO_PASSWORD);
     const driver = neo4j.driver(NEO_URL, auth);
     const cypherDropDb = `DROP DATABASE  ${global.INT_TEST_DB_NAME} IF EXISTS`;
@@ -17,13 +17,19 @@ module.exports = async function globalTeardown() {
         await session.run(cypherDropDb);
     } catch (error) {
         if (
-            error.message.includes("This is an administration command and it should be executed against the system database") ||
+            error.message.includes(
+                "This is an administration command and it should be executed against the system database"
+            ) ||
             error.message.includes("Unsupported administration command") ||
             error.message.includes("Unable to route write operation to leader for database 'system'")
         ) {
-            console.log(`\nJest /packages/graphql teardown: Expected action - NO drop of database as not supported in the current environment.`);  
+            console.log(
+                `\nJest /packages/graphql teardown: Expected action - NO drop of database as not supported in the current environment.`
+            );
         } else {
-            console.log(`\nJest /packages/graphql teardown: Teardown failure on neo4j @ ${NEO_URL}, cypher: "${cypherDropDb}", Error: ${error.message}`);  
+            console.log(
+                `\nJest /packages/graphql teardown: Teardown failure on neo4j @ ${NEO_URL}, cypher: "${cypherDropDb}", Error: ${error.message}`
+            );
         }
     } finally {
         if (session) await session.close();
