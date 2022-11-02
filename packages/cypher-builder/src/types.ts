b52d1093b5f4005344113f054fd36a824875b386
angrykoala <angrykoala@outlook.es>
2022-11-02 09:29:21 +0000

    Fix procedures typings

--- packages/cypher-builder/src/types.ts
+++ packages/cypher-builder/src/types.ts
@@ -27,7 +27,6 @@ import type { BooleanOp } from "./expressions/operations/boolean";
 import type { ComparisonOp } from "./expressions/operations/comparison";
 import type { RawCypher } from "./clauses/RawCypher";
 import type { PredicateFunction } from "./expressions/functions/PredicateFunctions";
-import type { ApocExpr, ApocPredicate, ValidatePredicate } from "./expressions/procedures/apoc/apoc";
 import type { Case } from "./expressions/Case";
 import type { MathOp } from "./expressions/operations/math";
 import type { ListComprehension } from "./expressions/list/ListComprehension";
@@ -37,7 +36,8 @@ import type { MapProjection } from "./expressions/map/MapProjection";
 import type { HasLabel } from "./expressions/HasLabel";
 import type { Reference } from "./variables/Reference";
 import type { RunFirstColumn } from "./procedures/apoc/RunFirstColumn";
-import type { FullTextQueryNodes } from "./procedures/db";
+import type { ValidatePredicate } from "./procedures/apoc/ValidatePredicate";
+import type { ApocExpr, ApocPredicate } from "./procedures/apoc/apoc";
 
 export type Operation = BooleanOp | ComparisonOp | MathOp;
 
@@ -68,7 +68,7 @@ export type Predicate =
     | HasLabel;
 
 /** Represents a procedure invocable with the CALL statement */
-export type Procedure = ValidatePredicate | RunFirstColumn | FullTextQueryNodes;
+export type Procedure = ValidatePredicate | RunFirstColumn;
 
 export type CypherResult = {
     cypher: string;
