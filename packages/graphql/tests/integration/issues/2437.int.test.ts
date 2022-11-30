d952dd15186b4d1291852908932466e2a01b2f00
angrykoala <angrykoala@outlook.es>
2022-11-30 14:07:16 +0000

    Add unique types to integration test

--- packages/graphql/tests/integration/issues/2437.int.test.ts
+++ packages/graphql/tests/integration/issues/2437.int.test.ts
@@ -22,7 +22,7 @@ import { graphql } from "graphql";
 import Neo4j from "../neo4j";
 import { Neo4jGraphQL } from "../../../src/classes";
 import { cleanNodes } from "../../utils/clean-nodes";
-import type { UniqueType } from "../../utils/graphql-types";
+import { generateUniqueType, UniqueType } from "../../utils/graphql-types";
 
 describe("https://github.com/neo4j/graphql/issues/2437", () => {
     let driver: Driver;
@@ -30,9 +30,8 @@ describe("https://github.com/neo4j/graphql/issues/2437", () => {
     let neoSchema: Neo4jGraphQL;
     let session: Session;
 
-    let Place: UniqueType;
-    let Post: UniqueType;
-    let Story: UniqueType;
+    let Agent: UniqueType;
+    let Valuation: UniqueType;
 
     beforeAll(async () => {
         neo4j = new Neo4j();
@@ -41,39 +40,41 @@ describe("https://github.com/neo4j/graphql/issues/2437", () => {
 
     beforeEach(async () => {
         session = await neo4j.getSession();
+        Agent = generateUniqueType("Agent");
+        Valuation = generateUniqueType("Valuation");
 
         const typeDefs = `
-            type Agent @exclude(operations: [DELETE]) {
+            type ${Agent} @exclude(operations: [DELETE]) {
                 uuid: ID! @id
                 archivedAt: DateTime
 
-                valuations: [Valuation!]! @relationship(type: "IS_VALUATION_AGENT", direction: OUT)
+                valuations: [${Valuation}!]! @relationship(type: "IS_VALUATION_AGENT", direction: OUT)
             }
-            extend type Agent
+            extend type ${Agent}
                 @auth(rules: [{ operations: [CREATE], roles: ["Admin"] }, { where: { archivedAt: null } }])
 
-            type Valuation @exclude(operations: [DELETE]) {
+            type ${Valuation} @exclude(operations: [DELETE]) {
                 uuid: ID! @id
                 archivedAt: DateTime
 
-                agent: Agent! @relationship(type: "IS_VALUATION_AGENT", direction: IN)
+                agent: ${Agent}! @relationship(type: "IS_VALUATION_AGENT", direction: IN)
             }
-            extend type Valuation @auth(rules: [{ where: { archivedAt: null } }])
+            extend type ${Valuation} @auth(rules: [{ where: { archivedAt: null } }])
         `;
 
         await session.run(`
-        CREATE(a:Agent {uuid: "a1"})
-        CREATE(:Valuation {uuid: "v1"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v2"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v3"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v4"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v5"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v6"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v7"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v8"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v9"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v10"})<-[:IS_VALUATION_AGENT]-(a)
-        CREATE(:Valuation {uuid: "v11"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(a:${Agent} {uuid: "a1"})
+        CREATE(:${Valuation} {uuid: "v1"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v2"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v3"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v4"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v5"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v6"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v7"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v8"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v9"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v10"})<-[:IS_VALUATION_AGENT]-(a)
+        CREATE(:${Valuation} {uuid: "v11"})<-[:IS_VALUATION_AGENT]-(a)
         `);
 
         neoSchema = new Neo4jGraphQL({
@@ -83,7 +84,7 @@ describe("https://github.com/neo4j/graphql/issues/2437", () => {
     });
 
     afterEach(async () => {
-        await cleanNodes(session, [Place, Post, Story]);
+        await cleanNodes(session, [Agent, Valuation]);
         await session.close();
     });
 
@@ -93,8 +94,8 @@ describe("https://github.com/neo4j/graphql/issues/2437", () => {
 
     test("should return only the first elements", async () => {
         const query = `
-            query Agents {
-                agents(where: { uuid: "a1" }) {
+            query {
+                ${Agent.plural}(where: { uuid: "a1" }) {
                     uuid
                     valuationsConnection(first: 10) {
                         edges {
@@ -118,7 +119,7 @@ describe("https://github.com/neo4j/graphql/issues/2437", () => {
 
         expect(result.errors).toBeFalsy();
         expect(result.data).toEqual({
-            agents: [
+            [Agent.plural]: [
                 {
                     uuid: "a1",
                     valuationsConnection: {
