5d3cde8541decf0e8c39ca79ca075835e380fc73
Liam Doodson <liam.doodson@neo4j.com>
2023-01-04 13:44:07 +0000

    Typo

--- packages/graphql/tests/integration/issues/2662.int.test.ts
+++ packages/graphql/tests/integration/issues/2662.int.test.ts
@@ -24,7 +24,7 @@ import { Neo4jGraphQL } from "../../../src/classes";
 import { generateUniqueType, UniqueType } from "../../utils/graphql-types";
 import { cleanNodes } from "../../utils/clean-nodes";
 
-describe("https://github.com/neo4j/graphql/issues/2614", () => {
+describe("https://github.com/neo4j/graphql/issues/2662", () => {
     let driver: Driver;
     let neo4j: Neo4j;
     let neoSchema: Neo4jGraphQL;
