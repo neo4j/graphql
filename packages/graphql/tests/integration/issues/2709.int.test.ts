baa5d3430113992d4da48e8fa134c8682d526627
Thomas Wiss <thomas.wiss@neotechnology.com>
2023-01-16 13:35:17 +0100

    feat: add int test to cover edge case

--- packages/graphql/tests/integration/issues/2709.int.test.ts
+++ packages/graphql/tests/integration/issues/2709.int.test.ts
@@ -234,12 +234,126 @@ describe("https://github.com/neo4j/graphql/issues/2709", () => {
             ],
         });
     });
+});
 
-    test("should query the correct DistributionHouses when no _on present - Netflix type", async () => {
+describe("https://github.com/neo4j/graphql/issues/2709 - extended", () => {
+    let driver: Driver;
+    let neo4j: Neo4j;
+    let neoSchema: Neo4jGraphQL;
+    let session: Session;
+
+    let Movie: UniqueType;
+    let Dishney: UniqueType;
+    let Netflix: UniqueType;
+    let Publisher: UniqueType;
+
+    beforeAll(async () => {
+        neo4j = new Neo4j();
+        driver = await neo4j.getDriver();
+    });
+
+    beforeEach(async () => {
+        session = await neo4j.getSession();
+
+        Movie = generateUniqueType("Movie");
+        Dishney = generateUniqueType("Dishney");
+        Netflix = generateUniqueType("Netflix");
+        Publisher = generateUniqueType("Publisher");
+
+        const typeDefs = `
+            interface Production {
+                title: String!
+                actors: [Actor!]!
+                distribution: [DistributionHouse!]!
+            }
+
+            type ${Movie} implements Production @node(label: "Film") {
+                title: String!
+                actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN, properties: "ActedIn")
+                runtime: Int!
+                distribution: [DistributionHouse!]! @relationship(type: "DISTRIBUTED_BY", direction: IN)
+                publisher: ${Publisher}! @relationship(type: "DISTRIBUTED_BY", direction: IN)
+            }
+
+            type Series implements Production {
+                title: String!
+                actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN, properties: "ActedIn")
+                episodes: Int!
+                distribution: [DistributionHouse!]! @relationship(type: "DISTRIBUTED_BY", direction: IN)
+            }
+
+            interface ActedIn @relationshipProperties {
+                role: String!
+            }
+
+            interface Actor {
+                name: String!
+                actedIn: [Production!]! @relationship(type: "ACTED_IN", direction: OUT, properties: "ActedIn")
+            }
+
+            type MaleActor implements Actor {
+                name: String!
+                actedIn: [Production!]! @relationship(type: "ACTED_IN", direction: OUT, properties: "ActedIn")
+                rating: Int!
+            }
+            type FemaleActor implements Actor {
+                name: String!
+                actedIn: [Production!]! @relationship(type: "ACTED_IN", direction: OUT, properties: "ActedIn")
+                age: Int!
+            }
+
+            interface DistributionHouse {
+                name: String!
+            }
+
+            type ${Dishney} implements DistributionHouse {
+                name: String!
+                review: String!
+            }
+
+            type Prime implements DistributionHouse {
+                name: String!
+                review: String!
+            }
+
+            type ${Netflix} implements DistributionHouse {
+                name: String!
+                review: String!
+            }
+
+            ### Extension ###
+            type ${Publisher} {
+                name: String!
+            }
+            ################
+        `;
+
+        await session.run(`
+            CREATE (:Film { title: "A Netflix movie" })<-[:DISTRIBUTED_BY]-(:${Netflix} { name: "Netflix" })
+            CREATE (:Film { title: "A Dishney movie" })<-[:DISTRIBUTED_BY]-(:${Dishney} { name: "Dishney" })
+            CREATE (:Film { title: "A Publisher movie" })<-[:DISTRIBUTED_BY]-(:${Publisher} { name: "The Publisher" })
+        `);
+
+        neoSchema = new Neo4jGraphQL({
+            typeDefs,
+            driver,
+        });
+    });
+
+    afterEach(async () => {
+        await cleanNodes(session, [Movie, Netflix, Dishney, Publisher]);
+        await session.close();
+    });
+
+    afterAll(async () => {
+        await driver.close();
+    });
+
+    test("should query only DistributionHouse nodes and NOT nodes (Publisher) which are connected by the same rel-type (DISTRIBUTED_BY)", async () => {
         const query = `
             query {
                 ${Movie.plural}(
-                    where: { distributionConnection_SOME: { node: { name: "Netflix" } } }
+                    where: { distributionConnection_SOME: { node: { name_CONTAINS: "e" } } }
                 ) {
                     title
                 }
@@ -257,6 +371,23 @@ describe("https://github.com/neo4j/graphql/issues/2709", () => {
             [Movie.plural]: [
                 {
                     title: "A Netflix movie",
+                },
+                {
+                    title: "A Dishney movie",
+                },
+            ],
+        });
+        // Note: to not equal
+        expect(result.data as any).not.toEqual({
+            [Movie.plural]: [
+                {
+                    title: "A Netflix movie",
+                },
+                {
+                    title: "A Dishney movie",
+                },
+                {
+                    title: "A Publisher movie",
                 },
             ],
         });
