377e8ddfc5e365b97a757a77d5b8739e7fc695da
Darrell Warde <darrell.warde@neotechnology.com>
2023-01-27 10:32:22 +0100

    Remove redundant test

--- packages/graphql/src/schema/make-augmented-schema.test.ts
+++ packages/graphql/src/schema/make-augmented-schema.test.ts
@@ -215,24 +215,6 @@ describe("makeAugmentedSchema", () => {
         );
     });
 
-    test("should throw error if @cypher is used on relationship properties interface", () => {
-        const typeDefs = gql`
-            type Movie {
-                actors: Actor! @relationship(type: "ACTED_IN", direction: OUT, properties: "ActedIn")
-            }
-
-            type Actor {
-                name: String
-            }
-
-            interface ActedIn @cypher(statement: "RETURN rand()") {
-                screenTime: Int
-            }
-        `;
-
-        expect(() => makeAugmentedSchema(typeDefs)).toThrow('Directive "@cypher" may not be used on INTERFACE.');
-    });
-
     test("should throw error if @auth is used on relationship property", () => {
         const typeDefs = gql`
             type Movie {
