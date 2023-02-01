4deefbe6dd3d36f72f2466d93cf7c136c72693dd
MacondoExpress <simone.gammicchia@gmail.com>
2023-02-01 12:14:57 +0000

    use asArray in the logical operator implementation

--- packages/graphql/src/translate/create-aggregate-where-and-params.ts
+++ packages/graphql/src/translate/create-aggregate-where-and-params.ts
@@ -28,6 +28,7 @@ import {
 import { NODE_OR_EDGE_KEYS, AGGREGATION_AGGREGATE_COUNT_OPERATORS } from "../constants";
 import { getCypherLogicalOperator, isLogicalOperator, LogicalOperator } from "./utils/logical-operators";
 import mapToDbProperty from "../utils/map-to-db-property";
+import { asArray } from "../utils/utils";
 
 type WhereFilter = Record<string | LogicalOperator, any>;
 
@@ -139,7 +140,7 @@ function aggregateWhere(
         } else if (isLogicalOperator(key)) {
             const cypherBuilderFunction = getCypherLogicalOperator(key);
             const logicalPredicates: Cypher.Predicate[] = [];
-            (Array.isArray(value) ? value : [value]).forEach((whereInput) => {
+            asArray(value).forEach((whereInput) => {
                 const {
                     returnProjections: innerReturnProjections,
                     predicates: innerPredicates,
@@ -208,7 +209,7 @@ function aggregateEntityWhere(
         if (isLogicalOperator(key)) {
             const cypherBuilderFunction = getCypherLogicalOperator(key);
             const logicalPredicates: Cypher.Predicate[] = [];
-            (Array.isArray(value) ? value : [value]).forEach((whereInput) => {
+            asArray(value).forEach((whereInput) => {
                 const {
                     returnProjections: innerReturnProjections,
                     predicates: innerPredicates,
