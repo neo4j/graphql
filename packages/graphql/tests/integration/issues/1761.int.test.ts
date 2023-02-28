03d9389b4a5ea721e996fca5a8c5952ad660f9cf
Darrell Warde <darrell.warde@gmail.com>
2023-02-28 15:48:30 +0000

    Fix issue number

--- packages/graphql/tests/integration/issues/1761.int.test.ts
+++ packages/graphql/tests/integration/issues/1761.int.test.ts
@@ -24,7 +24,7 @@ import { Neo4jGraphQL } from "../../../src/classes";
 import { UniqueType } from "../../utils/graphql-types";
 import { cleanNodes } from "../../utils/clean-nodes";
 
-describe("https://github.com/neo4j/graphql/issues/1781", () => {
+describe("https://github.com/neo4j/graphql/issues/1761", () => {
     let driver: Driver;
     let neo4j: Neo4j;
     let neoSchema: Neo4jGraphQL;
