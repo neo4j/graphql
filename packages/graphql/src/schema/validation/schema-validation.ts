e605a6784980dd865163362fc2891f86281b7794
MacondoExpress <simone.gammicchia@gmail.com>
2023-03-09 15:10:34 +0000

    bench test file for custom rules

--- packages/graphql/src/schema/validation/schema-validation.ts
+++ packages/graphql/src/schema/validation/schema-validation.ts
@@ -22,11 +22,15 @@ import type { Maybe } from "@graphql-tools/utils";
 import {
     buildASTSchema,
     buildSchema,
+    ConstDirectiveNode,
     DefinitionNode,
     DirectiveLocation,
     DocumentNode,
+    ExecutableDefinitionsRule,
     execute,
     extendSchema,
+    FieldsOnCorrectTypeRule,
+    FragmentsOnCompositeTypesRule,
     GraphQLBoolean,
     GraphQLDirective,
     GraphQLEnumType,
@@ -39,120 +43,50 @@ import {
     GraphQLString,
     InputObjectTypeDefinitionNode,
     Kind,
+    KnownArgumentNamesRule,
+    KnownDirectivesRule,
+    KnownFragmentNamesRule,
+    KnownTypeNamesRule,
+    LoneAnonymousOperationRule,
+    NoFragmentCyclesRule,
+    NoUndefinedVariablesRule,
+    NoUnusedFragmentsRule,
+    NoUnusedVariablesRule,
     ObjectTypeDefinitionNode,
+    OverlappingFieldsCanBeMergedRule,
     parse,
+    PossibleFragmentSpreadsRule,
     printSchema,
+    ProvidedRequiredArgumentsRule,
+    ScalarLeafsRule,
+    SingleFieldSubscriptionsRule,
     typeFromAST,
+    TypeInfo,
+    UniqueArgumentNamesRule,
+    UniqueDirectivesPerLocationRule,
+    UniqueFragmentNamesRule,
+    UniqueInputFieldNamesRule,
+    UniqueOperationNamesRule,
+    UniqueVariableNamesRule,
     validate,
     validateSchema,
+    ValuesOfCorrectTypeRule,
+    VariablesAreInputTypesRule,
+    VariablesInAllowedPositionRule,
     visit,
     visitInParallel,
 } from "graphql";
 import { specifiedRules, specifiedSDLRules } from "graphql/validation/specifiedRules";
-import { SDLValidationContext, SDLValidationRule, ValidationContext, ValidationRule } from "graphql/validation/ValidationContext";
+import {
+    ASTValidationContext,
+    SDLValidationContext,
+    SDLValidationRule,
+    ValidationContext,
+    ValidationRule,
+} from "graphql/validation/ValidationContext";
 
 const directives: GraphQLDirective[] = [];
 const types: GraphQLNamedType[] = [];
-
-export function makeValidationSchema(userDocument: DocumentNode, augmentedDocument: DocumentNode) {
-    // const validateBeforeTC = composer.createEnumTC(
-    //     `enum AuthorizationValidateBefore { READ, CREATE, UPDATE, DELETE, CREATE_RELATIONSHIP, DELETE_RELATIONSHIP }`
-    // );
-    // const validateAfterTC = composer.createEnumTC(
-    //     `enum AuthorizationValidateAfter { CREATE, UPDATE, DELETE, CREATE_RELATIONSHIP, DELETE_RELATIONSHIP }`
-    // );
-    // const filterTC = composer.createEnumTC(
-    //     `enum AuthorizationFilterOperation { READ, UPDATE, DELETE, CREATE_RELATIONSHIP, DELETE_RELATIONSHIP }`
-    // );
-    const augmentendSchema = buildASTSchema(augmentedDocument, { assumeValid: true });
-    const userSchema = buildASTSchema(userDocument, { assumeValid: true });
-    const authedTypeNames: string[] = [];
-    const enhancedDefinitions = augmentedDocument.definitions.reduce((acc, definition) => {
-        switch (definition.kind) {
-            case Kind.OBJECT_TYPE_DEFINITION: {
-                const typeName = definition.name.value;
-                const authDirectivePredicate = (directive) => directive.name.value === "authorization";
-                const authDirective = userSchema.getType(typeName)?.astNode?.directives?.find(authDirectivePredicate);
-                if (!authDirective) {
-                    acc.push(definition);
-                    return acc;
-                }
-                //makeAuthorizationTypesForTypename(typeName, augmentendSchema);
-                authedTypeNames.push(typeName);
-                /*       acc.push({
-                    ...definition,
-                    directives: [
-                        // TODO: augmentedSchema does not remove the @authorization
-                        {
-                            ...authDirective,
-                            name: { value: `${typeName}Authorization`, kind: authDirective.name.kind },
-                        },
-                    ],
-                }); */
-                acc.push(definition);
-                return acc;
-            }
-            // TODO: implement these as well
-            // case Kind.FIELD_DEFINITION:
-            // case Kind.INTERFACE_TYPE_DEFINITION:
-            default:
-                acc.push(definition);
-                return acc;
-        }
-    }, [] as DefinitionNode[]);
-
-    authedTypeNames.forEach((typeName) => makeAuthorizationTypesForTypename(typeName, augmentendSchema));
-    const enhancedAugmentedDocument: DocumentNode = { ...augmentedDocument, definitions: enhancedDefinitions };
-    const enhancedAugmentedSchema = buildASTSchema(enhancedAugmentedDocument, { assumeValid: true });
-    const schemaToExtend = new GraphQLSchema({
-        directives,
-        types,
-    });
-    const validatationSchema = mergeSchemas({
-        schemas: [enhancedAugmentedSchema, schemaToExtend],
-        assumeValidSDL: true,
-    });
-    // console.log(">type?validation>", validatationSchema.getType("Post"));
-
-    // console.log("validate start", printSchema(validatationSchema));
-    // const errors = validate(validatationSchema, userDocument);
-    //const errors = validate(validatationSchema, parse(`{ posts { author { name } } }`));
-    const newDocument = parse(printSchema(validatationSchema));
-    const authedDefinitions = authedTypeNames.map((type) => {
-        const authType = newDocument.definitions.find(
-            (definition) => definition.kind === Kind.OBJECT_TYPE_DEFINITION && definition.name.value === type
-        ) as ObjectTypeDefinitionNode;
-        const authDirective = newDocument.definitions.find(
-            (definition) =>
-                definition.kind === Kind.DIRECTIVE_DEFINITION && definition.name.value === `${type}Authorization`
-        );
-        const authUsage = {
-            ...authDirective,
-            kind: Kind.DIRECTIVE,
-        }
-        const directives = authType.directives ? [...authType.directives, authUsage] : [authUsage];
-        return {
-            ...authType,
-            directives,
-        };
-    });
-    const definitionsWithoutAuthedNodes = newDocument.definitions.filter(
-        (definition) =>
-            definition.kind !== Kind.OBJECT_TYPE_DEFINITION ||
-            (definition.kind === Kind.OBJECT_TYPE_DEFINITION && !authedTypeNames.includes(definition.name.value))
-    );
-
-    const finalDocument = {
-        ...newDocument,
-        definitions: [...definitionsWithoutAuthedNodes, ...authedDefinitions],
-    } as DocumentNode;
-    
-    const errors2 = validateSDL(finalDocument);
-
-    const errors = validateSchema(validatationSchema);
-    //console.log("validate end", errors);
-    console.log(`New error: ${errors2}`);
-}
 
 function makeAuthorizationTypesForTypename(typename: string, augmentendSchema: GraphQLSchema) {
     const type = augmentendSchema.getType(`${typename}Where`);
@@ -233,26 +167,140 @@ function makeAuthorizationTypesForTypename(typename: string, augmentendSchema: G
     types.push(authorizationFilterRule);
     types.push(authorizationValidateRule);
     directives.push(authorizationDirective);
-    /*    const definitions = [
-        authorizationDirective,
-        authorizationWhere,
-        authorizationFilterRule,
-        authorizationValidateRule,
-    ];
-    return definitions.map((definition) => definition.astNode); */
+}
+/**
+ *  userDocument: DocumentNode
+ *  augmentedDocument: DocumentNode
+ *  augmentedSchema: GraphlQLSchema
+ *  schemaToExtend: GraphQLSchema
+ *  validationSchema: GraphQLSchema (merge betweem schemaToExtend and augmentedSchema)
+ *  validationDocument: DocumentNode
+ *  finalDocument: DocumentNode
+ *  */
+
+function getUserAuthDefinition(userDocument, augmentedDefinition) {
+    const authDirectivePredicate = (directive) => directive.name.value === "authorization";
+    return userDocument.definitions.find(
+        (userDefinition) =>
+            userDefinition.kind === Kind.OBJECT_TYPE_DEFINITION &&
+            augmentedDefinition.name.value === userDefinition.name.value &&
+            userDefinition.directives?.find(authDirectivePredicate)
+    ) as ObjectTypeDefinitionNode;
+}
+export function makeValidationSchema(userDocument: DocumentNode, augmentedDocument: DocumentNode) {
+    const augmentendSchema = buildASTSchema(augmentedDocument, { assumeValid: true });
+
+    augmentedDocument.definitions.forEach((definition) => {
+        switch (definition.kind) {
+            case Kind.OBJECT_TYPE_DEFINITION: {
+                const typeName = definition.name.value;
+                const authDirective = getUserAuthDefinition(userDocument, definition);
+                if (authDirective) {
+                    makeAuthorizationTypesForTypename(typeName, augmentendSchema);
+                }
+            }
+            // TODO: implement these as well
+            // case Kind.FIELD_DEFINITION:
+            // case Kind.INTERFACE_TYPE_DEFINITION:
+        }
+    });
+
+    const schemaToExtend = new GraphQLSchema({
+        directives,
+        types,
+    });
+    const validationSchema = mergeSchemas({
+        schemas: [augmentendSchema, schemaToExtend],
+        assumeValidSDL: true,
+    });
+    const validationDocument = parse(printSchema(validationSchema)); // find a better way to obtain a DocumentNode from a GraphQLSchema
+    const finalDocument: DocumentNode = {
+        ...validationDocument,
+        definitions: validationDocument.definitions.reduce((acc, definition: DefinitionNode) => {
+            switch (definition.kind) {
+                case Kind.OBJECT_TYPE_DEFINITION: {
+                    const userDocumentObjectType = getUserAuthDefinition(userDocument, definition);
+                    if (userDocumentObjectType) {
+                        const userAuthUsage = userDocumentObjectType.directives?.find(
+                            (directive) => directive.name.value === "authorization"
+                        ) as ConstDirectiveNode;
+                        const newAuthUsage: ConstDirectiveNode = {
+                            ...userAuthUsage,
+                            name: {
+                                kind: Kind.NAME,
+                                value: `${userDocumentObjectType.name.value}Authorization`,
+                            },
+                        };
+                        acc.push({
+                            ...definition,
+                            directives: [newAuthUsage],
+                        });
+                        return acc;
+                    }
+                    acc.push(definition);
+                    return acc;
+                }
+                // TODO: implement these as well
+                // case Kind.FIELD_DEFINITION:
+                // case Kind.INTERFACE_TYPE_DEFINITION:
+                default:
+                    acc.push(definition);
+                    return acc;
+            }
+        }, [] as DefinitionNode[]),
+    };
+
+    const errors = validateSDL(finalDocument, validationSchema);
+    console.log(`Errors: ${errors}`);
 }
 
 function validateSDL(
     documentAST: DocumentNode,
-    schemaToExtend?: Maybe<GraphQLSchema>,
+    schema: GraphQLSchema,
     rules: ReadonlyArray<SDLValidationRule> = specifiedSDLRules
 ): ReadonlyArray<GraphQLError> {
     const errors: Array<GraphQLError> = [];
-    const context = new SDLValidationContext(documentAST, schemaToExtend, (error) => {
+    const context = new SDLValidationContext(documentAST, null, (error) => {
         errors.push(error);
     });
-
-    const visitors = rules.map((rule) => rule(context));
+    const validationContext = new ValidationContext(schema, documentAST, new TypeInfo(schema), (error) => {
+        errors.push(error);
+    });
+    const sdlVisitors = rules.map((rule) => rule(context));
+    /* const astVisitors = specifiedRules.map(rule => rule(validationContext)); Non SDL Rules..*/
+    /* const myListOfRules = 
+    [
+        ExecutableDefinitionsRule,
+        UniqueOperationNamesRule,
+        LoneAnonymousOperationRule,
+        SingleFieldSubscriptionsRule,
+        KnownTypeNamesRule,
+        FragmentsOnCompositeTypesRule,
+        VariablesAreInputTypesRule,
+        ScalarLeafsRule,
+        FieldsOnCorrectTypeRule,
+        UniqueFragmentNamesRule,
+        KnownFragmentNamesRule,
+        NoUnusedFragmentsRule,
+        PossibleFragmentSpreadsRule,
+        NoFragmentCyclesRule,
+        UniqueVariableNamesRule,
+        NoUndefinedVariablesRule,
+        NoUnusedVariablesRule,
+        KnownDirectivesRule,
+        UniqueDirectivesPerLocationRule,
+        KnownArgumentNamesRule,
+        UniqueArgumentNamesRule,
+        ValuesOfCorrectTypeRule,
+        ProvidedRequiredArgumentsRule,
+        VariablesInAllowedPositionRule,
+        OverlappingFieldsCanBeMergedRule,
+        UniqueInputFieldNamesRule,
+      ];
+ */
+    const myListOfRules = [KnownDirectivesRule];
+    const myCustomRulesVisitors = myListOfRules.map((rule) => rule(validationContext));
+    const visitors = [...sdlVisitors, ...myCustomRulesVisitors];
     visit(documentAST, visitInParallel(visitors));
     return errors;
 }
