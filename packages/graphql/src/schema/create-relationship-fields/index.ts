c82baaa49dc8a01dd82541fbbd18c4a2896d1b08
MacondoExpress <simone.gammicchia@gmail.com>
2023-03-10 11:17:00 +0000

    fix 2981

--- packages/graphql/src/schema/create-relationship-fields/index.ts
+++ packages/graphql/src/schema/create-relationship-fields/index.ts
@@ -347,7 +347,7 @@ function createRelationshipFields({
                     });
 
                     unionCreateFieldInput.addFields({
-                        [n.name]: `[${createName}!]`,
+                        [n.name]:  rel.typeMeta.array ? `[${createName}!]` : createName,
                     });
                 }
 
