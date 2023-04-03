0d6d178923ac9c0a1c81520a95b325c65054e8b2
Michael Webb <michael.webb@neo4j.com>
2023-04-03 14:47:11 +0200

    refactor: remove type casting

--- packages/graphql/src/classes/utils/asserts-indexes-and-constraints.ts
+++ packages/graphql/src/classes/utils/asserts-indexes-and-constraints.ts
@@ -226,18 +226,19 @@ async function getMissingConstraints({
 
     const constraintsCypher = "SHOW UNIQUE CONSTRAINTS";
     debug(`About to execute Cypher: ${constraintsCypher}`);
-    const constraintsResult = await session.run(constraintsCypher);
+    const constraintsResult = await session.run<{ labelsOrTypes: string; properties: string }>(constraintsCypher);
 
     constraintsResult.records
         .map((record) => {
             return record.toObject();
         })
         .forEach((constraint) => {
-            const label = constraint.labelsOrTypes[0] as string;
-            const property = constraint.properties[0];
+            const label = constraint.labelsOrTypes;
+            const property = constraint.properties;
+            const existingConstraint = existingConstraints[label];
 
-            if (existingConstraints[label]) {
-                (existingConstraints[label] as any).push(property as string);
+            if (existingConstraint) {
+                existingConstraint.push(property);
             } else {
                 existingConstraints[label] = [property];
             }
