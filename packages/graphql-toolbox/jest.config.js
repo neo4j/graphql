51cc9e9bcbfef4024d0efa7de3f7d16111af13ad
Thomas Wiss <thomas.wiss@neotechnology.com>
2023-04-03 16:08:48 +0200

    feat: toolbox jest config for jose

--- packages/graphql-toolbox/jest.config.js
+++ packages/graphql-toolbox/jest.config.js
@@ -2,10 +2,17 @@ const globalConf = require("../../jest.config.base");
 
 module.exports = {
     ...globalConf,
+    globals: {
+        TextEncoder: require("util").TextEncoder,
+        TextDecoder: require("util").TextDecoder,
+    },
     testEnvironment: "jsdom",
     displayName: "@neo4j/graphql-toolbox",
     roots: ["<rootDir>/packages/graphql-toolbox/src/", "<rootDir>/packages/graphql-toolbox/tests/"],
     coverageDirectory: "<rootDir>/packages/graphql-toolbox/coverage/",
+    moduleNameMapper: {
+        "jose(.*)$": "<rootDir>/node_modules/jose/dist/node/cjs/$1",
+    },
     transform: {
         "^.+\\.ts$": [
             "ts-jest",
