77392772760618e7a1b8c6482e69a09968d3755c
Michael Webb <michael.webb@neo4j.com>
2023-04-26 10:40:37 +0200

    refactor: remove neo-push

--- examples/neo-push/client/src/config.ts
+++
@@ -1,4 +0,0 @@
-// https://github.com/mrsteele/dotenv-webpack/issues/70
-export const { API_URL } = process.env;
-export const { JWT_KEY } = process.env;
-export const DEV_SERVER_PORT = Number(process.env.DEV_SERVER_PORT);
--- examples/neo-push/server/src/debugger.ts
+++
@@ -1,7 +0,0 @@
-import debug from "debug";
-
-const main = debug("Server");
-
-export default function createDebugger(input: string): (...any) => void {
-    return main.extend(input);
-}
