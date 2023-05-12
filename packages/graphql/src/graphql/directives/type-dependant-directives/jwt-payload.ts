e4c54e71ace0369edbb91505b42f52ec6ddb9469
Darrell Warde <darrell.warde@gmail.com>
2023-05-12 16:52:59 +0200

    Fix ESLint issues

--- packages/graphql/src/graphql/directives/type-dependant-directives/jwt-payload.ts
+++ packages/graphql/src/graphql/directives/type-dependant-directives/jwt-payload.ts
@@ -18,8 +18,8 @@
  */
 
 import { astFromObjectType } from "@graphql-tools/utils";
-import { GraphQLInt, GraphQLSchema, ObjectTypeDefinitionNode } from "graphql";
-import { GraphQLList, GraphQLString, GraphQLObjectType } from "graphql";
+import type { GraphQLSchema, ObjectTypeDefinitionNode } from "graphql";
+import { GraphQLInt , GraphQLList, GraphQLString, GraphQLObjectType } from "graphql";
 import getObjFieldMeta from "../../../schema/get-obj-field-meta";
 import type { ObjectFields } from "../../../schema/get-obj-field-meta";
 
