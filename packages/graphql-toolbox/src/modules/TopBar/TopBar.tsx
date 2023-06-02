eda77a841984eb1aa2c93dc96a0942e8dd340bf8
Thomas Wiss <thomas.wiss@neotechnology.com>
2023-06-02 10:56:31 +0200

    feat: base setup connection menu

--- packages/graphql-toolbox/src/modules/TopBar/TopBar.tsx
+++ packages/graphql-toolbox/src/modules/TopBar/TopBar.tsx
@@ -17,29 +17,26 @@
  * limitations under the License.
  */
 
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
-import { Button, IconButton } from "@neo4j-ndl/react";
-import {
-    ArrowRightOnRectangleIconOutline,
-    Cog8ToothIconOutline,
-    QuestionMarkCircleIconOutline,
-} from "@neo4j-ndl/react/icons";
+import { Button, IconButton, Menu, MenuItem, MenuItems } from "@neo4j-ndl/react";
+import { Cog8ToothIconOutline, QuestionMarkCircleIconOutline } from "@neo4j-ndl/react/icons";
 
 import { tracking } from "../../analytics/tracking";
 // @ts-ignore - SVG Import
 import Neo4jLogoIcon from "../../assets/neo4j-logo-white.svg";
 import { cannySettings } from "../../common/canny";
-import { CustomSelect } from "../../components/CustomSelect";
 import { DEFAULT_BOLT_URL } from "../../constants";
 import { AuthContext } from "../../contexts/auth";
-import { Screen, ScreenContext } from "../../contexts/screen";
+import { ScreenContext } from "../../contexts/screen";
 import { SettingsContext } from "../../contexts/settings";
 
 export const TopBar = () => {
     const auth = useContext(AuthContext);
     const settings = useContext(SettingsContext);
     const screen = useContext(ScreenContext);
+    const menuButtonRef = useRef<HTMLDivElement>(null);
+    const [openConnectionMenu, setOpenConnectionMenu] = useState<boolean>(false);
     const greenDot = <span className="ml-1 mr-1 h-2 w-2 bg-green-400 rounded-full inline-block" />;
     const redDot = <span className="ml-1 mr-1 h-2 w-2 bg-red-400 rounded-full inline-block" />;
 
@@ -83,6 +80,60 @@ export const TopBar = () => {
         return `${protocol}://${modifiedUsername}@${host}`;
     };
 
+    const ConnectionMenu = () => {
+        return (
+            <Menu
+                id="connection-menu"
+                rev={undefined}
+                open={openConnectionMenu}
+                anchorEl={menuButtonRef.current}
+                className="mt-2"
+                onClick={() => setOpenConnectionMenu(!openConnectionMenu)}
+            >
+                <MenuItems rev={undefined}>
+                    {auth.databases?.length ? (
+                        // TODO: disabled={screen.view !== Screen.TYPEDEFS}
+                        // TODO: db name text ellipse
+                        <>
+                            <Menu.Subheader title="Databases" data-test-topbar-database-selection />
+                            {auth.databases.map((db) => {
+                                return (
+                                    <MenuItem
+                                        rev={undefined}
+                                        onClick={() => handleSetSelectedDatabaseName(db.name)}
+                                        key={db.name}
+                                        title={db.name}
+                                    />
+                                );
+                            })}
+
+                            {/* // <span className="mx-2">/</span>
+                            // <CustomSelect
+                            //     value={auth.selectedDatabaseName}
+                            //     disabled={screen.view !== Screen.TYPEDEFS}
+                            //     onChange={(event) => handleSetSelectedDatabaseName(event.target.value)}
+                            //     testTag="data-test-topbar-database-selection"
+                            // >
+                               
+                            // </CustomSelect> */}
+                        </>
+                    ) : null}
+
+                    <Menu.Divider />
+                    {!auth.isNeo4jDesktop ? (
+                        <MenuItem
+                            rev={undefined}
+                            description={constructDbmsUrlWithUsername()}
+                            onClick={() => () => auth?.logout()}
+                            className="text-color-danger-50"
+                            title="Disconnect"
+                        />
+                    ) : null}
+                </MenuItems>
+            </Menu>
+        );
+    };
+
     return (
         <div className="flex w-full h-16 n-bg-neutral-90 border-b border-gray-100">
             <div className="flex-1 flex justify-start">
@@ -92,55 +143,32 @@ export const TopBar = () => {
                 </div>
             </div>
             <div className="flex-1 flex justify-center">
-                <div className="flex items-center n-text-dark-neutral-text-weaker">
+                <div
+                    onClick={() => setOpenConnectionMenu(!openConnectionMenu)}
+                    onKeyDown={() => setOpenConnectionMenu(!openConnectionMenu)}
+                    ref={menuButtonRef}
+                    className="flex items-center n-text-dark-neutral-text-weaker cursor-pointer"
+                    role="button"
+                    tabIndex={0}
+                >
                     <p className="mr-2">{auth?.isConnected ? greenDot : redDot} </p>
                     <div className="flex items-center">{constructDbmsUrlWithUsername()}</div>
-                    {auth.databases?.length ? (
-                        <>
-                            <span className="mx-2">/</span>
-                            <CustomSelect
-                                value={auth.selectedDatabaseName}
-                                disabled={screen.view !== Screen.TYPEDEFS}
-                                onChange={(event) => handleSetSelectedDatabaseName(event.target.value)}
-                                testTag="data-test-topbar-database-selection"
-                            >
-                                {auth.databases.map((db) => {
-                                    return (
-                                        <option key={db.name} value={db.name}>
-                                            {db.name}
-                                        </option>
-                                    );
-                                })}
-                            </CustomSelect>
-                        </>
-                    ) : null}
+                    <span className="mx-2">/</span>
+                    <span>{auth.selectedDatabaseName}</span>
                 </div>
+                <ConnectionMenu />
             </div>
             <div className="flex-1 flex justify-end">
                 <div className="flex items-center text-sm">
                     <Button
                         data-test-send-feedback-topbar
-                        className="ndl-theme-dark"
+                        className="ndl-theme-dark mr-2"
                         color="primary"
                         fill="outlined"
                         onClick={handleSendFeedbackClick}
                     >
                         Send feedback
                     </Button>
-                    {!auth.isNeo4jDesktop ? (
-                        <div className="mr-4 ml-2">
-                            <Button
-                                data-test-topbar-disconnect-button
-                                className="w-36 ndl-theme-dark"
-                                color="primary"
-                                fill="text"
-                                onClick={() => auth?.logout()}
-                            >
-                                <ArrowRightOnRectangleIconOutline className="w-full h-full" />
-                                <span>Disconnect</span>
-                            </Button>
-                        </div>
-                    ) : null}
                     <div className="flex items-center mr-6">
                         <div className="canny-indication-wrapper pb-8 pl-10 pointer-events-none absolute">
                             {/* This element is not clickable as we do not want to show the changelog here */}
