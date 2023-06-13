8e3fae882fb4896ca176a91b2520a84db5b7ef4a
Michael Webb <michael.webb@neo4j.com>
2023-06-13 10:14:08 +0200

    refactor: move editorView state and buttons into QueryEditor

--- packages/graphql-toolbox/src/modules/EditorView/QueryEditor.tsx
+++ packages/graphql-toolbox/src/modules/EditorView/QueryEditor.tsx
@@ -34,10 +34,14 @@ import {
     keymap,
     lineNumbers,
 } from "@codemirror/view";
+import { tokens } from "@neo4j-ndl/base";
+import { Button, IconButton } from "@neo4j-ndl/react";
+import { PlayIconOutline } from "@neo4j-ndl/react/icons";
+import classNames from "classnames";
 import { graphql as graphqlExtension } from "cm6-graphql";
+import type { EditorView as CodeMirrorEditorView } from "codemirror";
 import { EditorView } from "codemirror";
 import type { GraphQLSchema } from "graphql";
-import type { ReactElement } from "react";
 import { dracula, tomorrow } from "thememirror";
 
 import { Extension, FileName } from "../../components/Filename";
@@ -48,20 +52,23 @@ import { formatCode, handleEditorDisableState, ParserOptions } from "./utils";
 
 export interface Props {
     loading: boolean;
-    buttons: ReactElement;
-    editorView: EditorView | null;
-    setEditorView: React.Dispatch<React.SetStateAction<EditorView | null>>;
     onSubmit: (override?: string) => Promise<void>;
     schema: GraphQLSchema;
 }
 
 const External = Annotation.define<boolean>();
 
-export const QueryEditor = ({ loading, buttons, editorView, setEditorView, onSubmit, schema }: Props) => {
+export const QueryEditor = ({ loading, onSubmit, schema }: Props) => {
     const theme = useContext(ThemeContext);
     const store = useStore();
     const elementRef = useRef<HTMLDivElement | null>(null);
     const [value, setValue] = useState<string>();
+    const [editorView, setEditorView] = useState<CodeMirrorEditorView | null>(null);
+
+    const formatTheCode = (): void => {
+        if (!editorView) return;
+        formatCode(editorView, ParserOptions.GRAPH_QL);
+    };
 
     // Taken from https://github.com/uiwjs/react-codemirror/blob/master/core/src/useCodeMirror.ts
     const updateListener = EditorView.updateListener.of((vu: ViewUpdate) => {
@@ -183,9 +190,43 @@ export const QueryEditor = ({ loading, buttons, editorView, setEditorView, onSub
             <FileName
                 name={"query"}
                 extension={Extension.GRAPHQL}
-                rightButtons={buttons}
+                rightButtons={
+                    <>
+                        <Button
+                            aria-label="Prettify code"
+                            className={classNames(
+                                "mr-2",
+                                theme.theme === Theme.LIGHT ? "ndl-theme-light" : "ndl-theme-dark"
+                            )}
+                            color="neutral"
+                            fill="outlined"
+                            size="small"
+                            onClick={formatTheCode}
+                            disabled={loading}
+                        >
+                            Prettify
+                        </Button>
+                        <IconButton
+                            data-test-editor-query-button
+                            aria-label="Execute query"
+                            style={{ height: "1.7rem" }}
+                            className={classNames(theme.theme === Theme.LIGHT ? "ndl-theme-light" : "ndl-theme-dark")}
+                            color="primary"
+                            clean
+                            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                            onClick={() => onSubmit()}
+                            disabled={!schema || loading}
+                        >
+                            <PlayIconOutline
+                                style={{
+                                    color: tokens.colors.primary[50],
+                                }}
+                            />
+                        </IconButton>
+                    </>
+                }
                 borderRadiusTop={false}
-            ></FileName>
+            />
             <div
                 id={EDITOR_QUERY_INPUT}
                 className={theme.theme === Theme.LIGHT ? "cm-light" : "cm-dark"}
