de93007532d8ff9a6917fa718536d3af79684793
Darrell Warde <darrell.warde@gmail.com>
2023-06-16 10:51:45 +0100

    Standalone createBearerToken util

--- packages/graphql/tests/utils/create-bearer-token.ts
+++ packages/graphql/tests/utils/create-bearer-token.ts
@@ -17,22 +17,9 @@
  * limitations under the License.
  */
 
-import { Socket } from "net";
-import { IncomingMessage } from "http";
 import jsonwebtoken from "jsonwebtoken";
-import { createBearerToken } from "./create-bearer-token";
 
-/** Creates a JWT valid request with the given secret and the extraData in the JWT token */
-
-export function createJwtRequest(secret: string, extraData: Record<string, any> = {}): IncomingMessage {
-    const requestHeader = createBearerToken(secret, extraData);
-    const socket = new Socket({ readable: true });
-    const req = new IncomingMessage(socket);
-    req.headers.authorization = requestHeader;
-    return req;
-}
-
-export function createJwtHeader(secret: string, extraData: Record<string, any> = {}): string {
+export function createBearerToken(secret: string, extraData: Record<string, any> = {}): string {
     const token = jsonwebtoken.sign(
         {
             roles: [],
