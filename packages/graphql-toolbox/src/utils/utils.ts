528a20c366495bde5746c2846f0ca35b536d6520
Thomas Wiss <thomas.wiss@neotechnology.com>
2023-06-19 09:01:35 +0200

    chore: clean-up

--- packages/graphql-toolbox/src/utils/utils.ts
+++ packages/graphql-toolbox/src/utils/utils.ts
@@ -21,14 +21,10 @@ import { useEffect, useRef } from "react";
 
 export const getAuraDBIdFromText = (text: string | null | undefined): string | null => {
     if (!text) return null;
-    try {
-        const myRegexp = new RegExp("://(.*?|$).databases.neo4j.io", "g");
-        const matches = myRegexp.exec(text);
-        if (!matches || matches?.length < 2) return null;
-        return matches[1];
-    } catch (_) {
-        return null;
-    }
+    const myRegexp = new RegExp("://(.*?|$).databases.neo4j.io", "g");
+    const matches = myRegexp.exec(text);
+    if (!matches || matches?.length < 2) return null;
+    return matches[1];
 };
 
 export const getURLProtocolFromText = (text: string | null | undefined): string => {
