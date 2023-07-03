b87b0dffdd7572cf79195cc28d3ab82ebecda2b5
Darrell Warde <darrell.warde@gmail.com>
2023-07-03 13:37:48 +0100

    Remove unused code

--- packages/graphql/src/graphql/directives/type-dependant-directives/subscriptions-authorization.ts
+++ packages/graphql/src/graphql/directives/type-dependant-directives/subscriptions-authorization.ts
@@ -17,16 +17,9 @@
  * limitations under the License.
  */
 
-import { astFromDirective, astFromEnumType, astFromInputObjectType } from "@graphql-tools/utils";
-import type {
-    TypeDefinitionNode,
-    DirectiveDefinitionNode,
-    ObjectTypeDefinitionNode,
-    EnumTypeDefinitionNode,
-    InputObjectTypeDefinitionNode,
-} from "graphql";
+import { astFromDirective, astFromInputObjectType } from "@graphql-tools/utils";
+import type { TypeDefinitionNode, DirectiveDefinitionNode } from "graphql";
 import {
-    GraphQLEnumType,
     GraphQLSchema,
     GraphQLDirective,
     GraphQLInputObjectType,
@@ -34,20 +27,6 @@ import {
     GraphQLBoolean,
     DirectiveLocation,
 } from "graphql";
-import { SchemaComposer } from "graphql-compose";
-import getWhereFields from "../../../schema/get-where-fields";
-import { getJwtFields } from "./jwt-payload";
-
-const SUBSCRIPTIONS_AUTHORIZATION_FILTER_EVENT = new GraphQLEnumType({
-    name: "SubscriptionsAuthorizationFilterEvent",
-    values: {
-        CREATE: { value: "CREATE" },
-        UPDATE: { value: "UPDATE" },
-        DELETE: { value: "DELETE" },
-        CREATE_RELATIONSHIP: { value: "CREATE_RELATIONSHIP" },
-        DELETE_RELATIONSHIP: { value: "DELETE_RELATIONSHIP" },
-    },
-});
 
 function createSubscriptionsAuthorizationWhere(
     typeDefinitionName: string,
@@ -138,22 +117,6 @@ function createSubscriptionsAuthorization({
     });
 }
 
-function createJWTPayloadWhere(
-    schema: GraphQLSchema,
-    JWTPayloadDefinition?: ObjectTypeDefinitionNode
-): GraphQLInputObjectType {
-    const inputFieldsType = getWhereFields({
-        typeName: "JWTPayload",
-        fields: getJwtFields(schema, JWTPayloadDefinition),
-    });
-    const composer = new SchemaComposer();
-    const inputTC = composer.createInputTC({
-        name: "JWTPayloadWhere",
-        fields: inputFieldsType,
-    });
-    return inputTC.getType();
-}
-
 export function createSubscriptionsAuthorizationDefinitions(
     typeDefinitionName: string,
     schema: GraphQLSchema
@@ -191,16 +154,4 @@ export function createSubscriptionsAuthorizationDefinitions(
 
     const subscriptionsAuthorizationAST = astFromDirective(subscriptionsAuthorization);
     return [subscriptionsAuthorizationWhereAST, subscriptionsAuthorizationFilterRuleAST, subscriptionsAuthorizationAST];
-}
-
-export function getStaticAuthorizationDefinitions(
-    JWTPayloadDefinition?: ObjectTypeDefinitionNode
-): Array<InputObjectTypeDefinitionNode | EnumTypeDefinitionNode> {
-    const schema = new GraphQLSchema({});
-    const subscriptionsAuthorizationFilterEvent = astFromEnumType(SUBSCRIPTIONS_AUTHORIZATION_FILTER_EVENT, schema);
-
-    const JWTPayloadWhere = createJWTPayloadWhere(schema, JWTPayloadDefinition);
-    const JWTPayloadWhereAST = astFromInputObjectType(JWTPayloadWhere, schema);
-
-    return [subscriptionsAuthorizationFilterEvent, JWTPayloadWhereAST];
 }
