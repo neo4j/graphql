e61c0f366f4fbe930ab1b512b94bd98fec2abaa7
Michael Webb <michael.webb@neo4j.com>
2023-07-07 15:49:24 +0200

    feat: add settable directive annotation

---
+++ packages/graphql/src/schema-model/parser/settable-annotation.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import type { DirectiveNode } from "graphql";
+import { SettableAnnotation } from "../annotation/SettableAnnotation";
+import { parseArguments } from "./utils";
+
+export function parseSettableAnnotation(directive: DirectiveNode): SettableAnnotation {
+    const { onCreate, onUpdate } = parseArguments(directive) as { onCreate: boolean; onUpdate: boolean };
+
+    return new SettableAnnotation({
+        onCreate,
+        onUpdate,
+    });
+}
