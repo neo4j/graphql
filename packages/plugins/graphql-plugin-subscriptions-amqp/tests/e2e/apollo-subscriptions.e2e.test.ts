5c3ce3ae8b81102dc844153568bf0b8195e13cc4
Darrell Warde <darrell.warde@gmail.com>
2023-07-14 11:24:55 +0100

    Simplify test which will be removed on 4.0.0

--- packages/plugins/graphql-plugin-subscriptions-amqp/tests/e2e/apollo-subscriptions.e2e.test.ts
+++ packages/plugins/graphql-plugin-subscriptions-amqp/tests/e2e/apollo-subscriptions.e2e.test.ts
@@ -62,13 +62,7 @@ describe("Apollo and RabbitMQ Subscription", () => {
             },
         });
 
-        // eslint-disable-next-line @typescript-eslint/require-await
-        server = new ApolloTestServer(neoSchema, async ({ req }) => ({
-            sessionConfig: {
-                database: neo4j.getIntegrationDatabaseName(),
-            },
-            token: req.headers.authorization,
-        }));
+        server = new ApolloTestServer(neoSchema);
         await server.start();
         wsClient = new WebSocketTestClient(server.wsPath);
     });
