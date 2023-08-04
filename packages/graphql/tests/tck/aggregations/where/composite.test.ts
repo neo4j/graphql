70545487a1c82f14c0489311885ad168f80fa350
angrykoala <angrykoala@outlook.es>
2023-08-04 13:06:47 +0100

    Add aggregation filters tests for logical filters

--- packages/graphql/tests/tck/aggregations/where/composite.test.ts
+++ packages/graphql/tests/tck/aggregations/where/composite.test.ts
@@ -34,7 +34,11 @@ describe("Cypher Aggregations where with count and node", () => {
 
             type Post {
                 content: String!
-                likes: [User!]! @relationship(type: "LIKES", direction: IN)
+                likes: [User!]! @relationship(type: "LIKES", direction: IN, properties: "Likes")
+            }
+
+            interface Likes @relationshipProperties {
+                someString: String
             }
         `;
 
@@ -73,6 +77,97 @@ describe("Cypher Aggregations where with count and node", () => {
                     \\"high\\": 0
                 },
                 \\"param1\\": \\"potato\\"
+            }"
+        `);
+    });
+
+    test("Equality Count, node and edge", async () => {
+        const query = gql`
+            {
+                posts(
+                    where: {
+                        likesAggregate: { count: 10, node: { name_EQUAL: "potato" }, edge: { someString_EQUAL: "10" } }
+                    }
+                ) {
+                    content
+                }
+            }
+        `;
+
+        const result = await translateQuery(neoSchema, query);
+
+        expect(formatCypher(result.cypher)).toMatchInlineSnapshot(`
+            "MATCH (this:Post)
+            CALL {
+                WITH this
+                MATCH (this)<-[this0:LIKES]-(this1:User)
+                RETURN count(this1) = $param0 AS var2, any(var3 IN collect(this1.name) WHERE var3 = $param1) AS var4, any(var5 IN collect(this0.someString) WHERE var5 = $param2) AS var6
+            }
+            WITH *
+            WHERE (var2 = true AND var4 = true AND var6 = true)
+            RETURN this { .content } AS this"
+        `);
+
+        expect(formatParams(result.params)).toMatchInlineSnapshot(`
+            "{
+                \\"param0\\": {
+                    \\"low\\": 10,
+                    \\"high\\": 0
+                },
+                \\"param1\\": \\"potato\\",
+                \\"param2\\": \\"10\\"
+            }"
+        `);
+    });
+
+    test("Equality Count, node, edge and logical", async () => {
+        const query = gql`
+            {
+                posts(
+                    where: {
+                        likesAggregate: {
+                            count: 10
+                            node: { name_EQUAL: "potato" }
+                            edge: { someString_EQUAL: "10" }
+                            AND: [{ count_GT: 10 }, { count_LT: 20 }]
+                        }
+                    }
+                ) {
+                    content
+                }
+            }
+        `;
+
+        const result = await translateQuery(neoSchema, query);
+
+        expect(formatCypher(result.cypher)).toMatchInlineSnapshot(`
+            "MATCH (this:Post)
+            CALL {
+                WITH this
+                MATCH (this)<-[this0:LIKES]-(this1:User)
+                RETURN count(this1) = $param0 AS var2, count(this1) > $param1 AS var3, count(this1) < $param2 AS var4, any(var5 IN collect(this1.name) WHERE var5 = $param3) AS var6, any(var7 IN collect(this0.someString) WHERE var7 = $param4) AS var8
+            }
+            WITH *
+            WHERE (var2 = true AND (var3 = true AND var4 = true) AND var6 = true AND var8 = true)
+            RETURN this { .content } AS this"
+        `);
+
+        expect(formatParams(result.params)).toMatchInlineSnapshot(`
+            "{
+                \\"param0\\": {
+                    \\"low\\": 10,
+                    \\"high\\": 0
+                },
+                \\"param1\\": {
+                    \\"low\\": 10,
+                    \\"high\\": 0
+                },
+                \\"param2\\": {
+                    \\"low\\": 20,
+                    \\"high\\": 0
+                },
+                \\"param3\\": \\"potato\\",
+                \\"param4\\": \\"10\\"
             }"
         `);
     });
