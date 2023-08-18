977d70bfdd99bf85112fef686bc22562c6936775
Darrell Warde <darrell.warde@gmail.com>
2023-08-18 15:31:24 +0200

    Single type for common context fields

--- packages/graphql/src/types/neo4j-graphql-subscriptions-context.ts
+++ packages/graphql/src/types/neo4j-graphql-subscriptions-context.ts
@@ -17,42 +17,9 @@
  * limitations under the License.
  */
 
-import type { JWTPayload } from "jose";
+import type { Neo4jGraphQLCommonContext } from "./neo4j-graphql-common-context";
 
-export interface Neo4jGraphQLSubscriptionsConnectionParams {
-    /**
-     * A decoded JWT payload which can be provided for use in authentication and authorization.
-     * Takes precedence over {@link token} if both are present in the context.
-     *
-     * Will be populated with the decoded {@link token} if authorization has been enabled in the library.
-     *
-     * @example
-     * ```
-     * {
-     *   sub: "1234567890",
-     *   name: "John Doe",
-     *   iat: 1516239022,
-     * }
-     * ```
-     */
-    jwt?: JWTPayload;
-    /**
-     * The bearer token to be decoded/verified for use in authentication and authorization.
-     * Normally found in the Authorization HTTP header. Can be provided with or without authentication scheme.
-     *
-     * @example
-     * With authentication scheme (standard when used in Authorization header):
-     * ```
-     * Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
-     * ```
-     * @example
-     * Without authentication scheme:
-     * ```
-     * eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
-     * ```
-     */
-    token?: string;
-}
+export interface Neo4jGraphQLSubscriptionsConnectionParams extends Neo4jGraphQLCommonContext {}
 
 export interface Neo4jGraphQLSubscriptionsContext {
     connectionParams: Neo4jGraphQLSubscriptionsConnectionParams;
