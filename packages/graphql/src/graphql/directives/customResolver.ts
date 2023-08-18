563c8ad5e27c89c6f138c0b693e308fb7d8f4c95
Liam Doodson <liam.doodson@neo4j.com>
2023-08-18 14:41:43 +0100

    Fix directive type

--- packages/graphql/src/graphql/directives/customResolver.ts
+++ packages/graphql/src/graphql/directives/customResolver.ts
@@ -17,8 +17,7 @@
  * limitations under the License.
  */
 
-import { DirectiveLocation, GraphQLDirective } from "graphql";
-import { GraphQLSelectionSet } from "../scalars/SelectionSet";
+import { DirectiveLocation, GraphQLDirective, GraphQLString } from "graphql";
 
 export const customResolverDirective = new GraphQLDirective({
     name: "customResolver",
@@ -28,8 +27,8 @@ export const customResolverDirective = new GraphQLDirective({
     args: {
         requires: {
             description:
-                "Selection set of the fields that the custom resolver will depend on. These fields are passed as an object to the first argument of the custom resolver.",
-            type: GraphQLSelectionSet,
+                "Fields that the custom resolver will depend on. These are passed as an object to the first argument of the custom resolver.",
+            type: GraphQLString,
         },
     },
 });
