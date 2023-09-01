fdfe27e32c288f927f090aafbfeac663cbc703e5
Darrell Warde <darrell.warde@gmail.com>
2023-09-01 14:19:17 +0100

    Remove incorrect deprecation

--- packages/graphql/src/schema/parse/parse-fulltext-directive.ts
+++ packages/graphql/src/schema/parse/parse-fulltext-directive.ts
@@ -22,12 +22,6 @@ import type { FullText, FulltextContext } from "../../types";
 import type { ObjectFields } from "../get-obj-field-meta";
 import { parseValueNode } from "../../schema-model/parser/parse-value-node";
 
-const deprecationWarning =
-    "The @fulltext name argument has been deprecated and will be removed in 4.0.0. " +
-    "Please use indexName instead. More information about the changes to @fulltext can be found here: " +
-    "https://neo4j.com/docs/graphql-manual/current/guides/v4-migration/#_fulltext_changes.";
-let deprecationWarningShown = false;
-
 function parseFulltextDirective({
     directive,
     nodeFields,
@@ -48,10 +42,6 @@ function parseFulltextDirective({
         const indexName = index.indexName || index.name;
         if (indexName === undefined) {
             throw new Error("The name of the fulltext index should be defined using the indexName argument.");
-        }
-        if (index.name && !deprecationWarningShown) {
-            console.warn(deprecationWarning);
-            deprecationWarningShown = true;
         }
         const names = value.filter((i) => indexName === i.indexName || indexName === i.name);
         if (names.length > 1) {
