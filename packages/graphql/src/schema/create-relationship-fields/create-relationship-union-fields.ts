8bf1020b5dbb32c6da47c7d4aac36c5c5b20f272
Michael Webb <michael.webb@neo4j.com>
2023-09-08 12:27:49 +0200

    wip: add createRelationshipUnionFields2

--- packages/graphql/src/schema/create-relationship-fields/create-relationship-union-fields.ts
+++ packages/graphql/src/schema/create-relationship-fields/create-relationship-union-fields.ts
@@ -17,15 +17,20 @@
  * limitations under the License.
  */
 
+import type { DirectiveNode } from "graphql";
 import type { InputTypeComposer, ObjectTypeComposer, SchemaComposer } from "graphql-compose";
 import { InterfaceTypeComposer, upperFirst } from "graphql-compose";
 import type { Node } from "../../classes";
 import { RelationshipNestedOperationsOption } from "../../constants";
+import type { ConcreteEntityAdapter } from "../../schema-model/entity/model-adapters/ConcreteEntityAdapter";
+import type { InterfaceEntityAdapter } from "../../schema-model/entity/model-adapters/InterfaceEntityAdapter";
+import type { UnionEntityAdapter } from "../../schema-model/entity/model-adapters/UnionEntityAdapter";
+import type { RelationshipAdapter } from "../../schema-model/relationship/model-adapters/RelationshipAdapter";
 import type { RelationField } from "../../types";
 import { DEPRECATE_NOT } from "../constants";
-import { addDirectedArgument } from "../directed-argument";
+import { addDirectedArgument, addDirectedArgument2 } from "../directed-argument";
 import { graphqlDirectivesToCompose } from "../to-compose";
-import { createConnectOrCreateField } from "./create-connect-or-create-field";
+import { createConnectOrCreateField, createConnectOrCreateField2 } from "./create-connect-or-create-field";
 
 export function createRelationshipUnionFields({
     nodes,
@@ -421,6 +426,412 @@ export function createRelationshipUnionFields({
         const nodeDisconnectInput = schemaComposer.getOrCreateITC(`${sourceName}DisconnectInput`);
         nodeDisconnectInput.addFields({
             [rel.fieldName]: unionDisconnectInput,
+        });
+    }
+}
+
+export function createRelationshipUnionFields2({
+    relationship,
+    composeNode,
+    schemaComposer,
+    hasNonGeneratedProperties,
+    hasNonNullNonGeneratedProperties,
+    userDefinedFieldDirectives,
+}: {
+    relationship: RelationshipAdapter;
+    composeNode: ObjectTypeComposer | InterfaceTypeComposer;
+    schemaComposer: SchemaComposer;
+    hasNonGeneratedProperties: boolean;
+    hasNonNullNonGeneratedProperties: boolean;
+    userDefinedFieldDirectives: Map<string, DirectiveNode[]>;
+}) {
+    const refNodes = (relationship.target as UnionEntityAdapter).concreteEntities;
+
+    const sourceAdapter = relationship.source as ConcreteEntityAdapter | InterfaceEntityAdapter;
+    const nestedOperations = new Set(relationship.nestedOperations);
+    const nodeCreateInput = schemaComposer.getITC(`${sourceAdapter.name}CreateInput`);
+    // const refNodes = nodes.filter((x) => rel.union?.nodes?.includes(x.name));fNodes.find((n) => n.uniqueFields.length);
+
+    const onlyConnectOrCreateAndNoUniqueFieldsInAllRefTypes =
+        nestedOperations.size === 1 &&
+        nestedOperations.has(RelationshipNestedOperationsOption.CONNECT_OR_CREATE) &&
+        !refNodes.find((n) => n.uniqueFields.length);
+
+    if (relationship.isReadable()) {
+        const baseNodeFieldArgs = {
+            options: "QueryOptions",
+            where: `${relationship.target.name}Where`,
+        };
+        const nodeFieldArgs = addDirectedArgument2(baseNodeFieldArgs, relationship);
+
+        composeNode.addFields({
+            [relationship.name]: {
+                type: relationship.getTargetTypePrettyName(),
+                args: nodeFieldArgs,
+                description: relationship.description,
+                directives: graphqlDirectivesToCompose(userDefinedFieldDirectives.get(relationship.name) ?? []),
+            },
+        });
+    }
+
+    const upperFieldName = upperFirst(relationship.name);
+    const upperNodeName = upperFirst(sourceAdapter.name);
+    const typePrefix = `${upperNodeName}${upperFieldName}`;
+
+    const unionConnectInput = nestedOperations.has(RelationshipNestedOperationsOption.CONNECT)
+        ? schemaComposer.createInputTC({
+              name: `${typePrefix}ConnectInput`,
+              fields: {},
+          })
+        : undefined;
+    const unionDeleteInput = nestedOperations.has(RelationshipNestedOperationsOption.DELETE)
+        ? schemaComposer.createInputTC({
+              name: `${typePrefix}DeleteInput`,
+              fields: {},
+          })
+        : undefined;
+    const unionDisconnectInput = nestedOperations.has(RelationshipNestedOperationsOption.DISCONNECT)
+        ? schemaComposer.createInputTC({
+              name: `${typePrefix}DisconnectInput`,
+              fields: {},
+          })
+        : undefined;
+    let unionCreateInput: InputTypeComposer<any> | undefined;
+
+    const connectOrCreateAndUniqueFieldsInRefTypes =
+        nestedOperations.has(RelationshipNestedOperationsOption.CONNECT_OR_CREATE) &&
+        refNodes.find((n) => n.uniqueFields.length);
+
+    if (
+        nestedOperations.has(RelationshipNestedOperationsOption.CREATE) ||
+        nestedOperations.has(RelationshipNestedOperationsOption.CONNECT) ||
+        connectOrCreateAndUniqueFieldsInRefTypes
+    ) {
+        unionCreateInput = schemaComposer.createInputTC({
+            name: `${typePrefix}CreateInput`,
+            fields: {},
+        });
+    }
+
+    const unionUpdateInput = schemaComposer.createInputTC({
+        name: `${typePrefix}UpdateInput`,
+        fields: {},
+    });
+
+    const unionCreateFieldInput = schemaComposer.createInputTC({
+        name: `${typePrefix}CreateFieldInput`,
+        fields: {},
+    });
+
+    refNodes.forEach((unionMemberEntity) => {
+        const unionPrefix = `${sourceAdapter.name}${upperFieldName}${unionMemberEntity.name}`;
+        const updateField = `${unionMemberEntity.name}UpdateInput`;
+        const nodeFieldInputName = `${unionPrefix}FieldInput`;
+        const whereName = `${unionPrefix}ConnectionWhere`;
+
+        const deleteName = `${unionPrefix}DeleteFieldInput`;
+        const deleteField = relationship.isList ? `[${deleteName}!]` : `${deleteName}`;
+
+        const disconnectName = `${unionPrefix}DisconnectFieldInput`;
+        const disconnect = relationship.isList ? `[${disconnectName}!]` : `${disconnectName}`;
+
+        const connectionUpdateInputName = `${unionPrefix}UpdateConnectionInput`;
+
+        const connectAndCreateAndUniqueFields =
+            nestedOperations.has(RelationshipNestedOperationsOption.CONNECT_OR_CREATE) &&
+            unionMemberEntity.uniqueFields.length;
+        const onlyConnectOrCreateAndNoUniqueFields =
+            nestedOperations.size === 1 &&
+            nestedOperations.has(RelationshipNestedOperationsOption.CONNECT_OR_CREATE) &&
+            !unionMemberEntity.uniqueFields.length;
+
+        let updateFields: Record<string, string> | undefined;
+        if (nestedOperations.size !== 0 && !onlyConnectOrCreateAndNoUniqueFields) {
+            updateFields = {
+                where: whereName,
+            };
+        }
+
+        let fieldInputFields: Record<string, string> | undefined;
+        if (
+            nestedOperations.has(RelationshipNestedOperationsOption.CONNECT) ||
+            nestedOperations.has(RelationshipNestedOperationsOption.CREATE) ||
+            connectAndCreateAndUniqueFields
+        ) {
+            // Created as {} because the connect/create fields are added later
+            fieldInputFields = {};
+        }
+
+        const createName = `${sourceAdapter.name}${upperFirst(relationship.name)}${
+            unionMemberEntity.name
+        }CreateFieldInput`;
+        if (!schemaComposer.has(createName)) {
+            schemaComposer.getOrCreateITC(createName, (tc) => {
+                tc.addFields({ node: `${unionMemberEntity.name}CreateInput!` });
+
+                if (hasNonGeneratedProperties) {
+                    tc.addFields({
+                        edge: `${relationship.propertiesTypeName}CreateInput${
+                            hasNonNullNonGeneratedProperties ? `!` : ""
+                        }`,
+                    });
+                }
+            });
+
+            if (
+                unionCreateInput &&
+                (nestedOperations.has(RelationshipNestedOperationsOption.CREATE) ||
+                    nestedOperations.has(RelationshipNestedOperationsOption.CONNECT) ||
+                    connectAndCreateAndUniqueFields)
+            ) {
+                unionCreateInput.addFields({
+                    [unionMemberEntity.name]: nodeFieldInputName,
+                });
+            }
+
+            unionCreateFieldInput.addFields({
+                [unionMemberEntity.name]: relationship.isList ? `[${createName}!]` : createName,
+            });
+        }
+        if (nestedOperations.has(RelationshipNestedOperationsOption.CREATE) && (updateFields || fieldInputFields)) {
+            const create = relationship.isList ? `[${createName}!]` : createName;
+            if (updateFields) {
+                updateFields.create = create;
+            }
+            if (fieldInputFields) {
+                fieldInputFields.create = create;
+            }
+        }
+
+        if (unionConnectInput && (updateFields || fieldInputFields)) {
+            const connectWhereName = `${unionMemberEntity.name}ConnectWhere`;
+            if (!schemaComposer.has(connectWhereName)) {
+                schemaComposer.createInputTC({
+                    name: connectWhereName,
+                    fields: {
+                        node: `${unionMemberEntity.name}Where!`,
+                    },
+                });
+            }
+
+            const connectName = `${unionPrefix}ConnectFieldInput`;
+            const connect = relationship.isList ? `[${connectName}!]` : `${connectName}`;
+            if (!schemaComposer.has(connectName)) {
+                schemaComposer.getOrCreateITC(connectName, (tc) => {
+                    tc.addFields({ where: connectWhereName });
+
+                    if (unionMemberEntity.relationships.size) {
+                        tc.addFields({
+                            connect: relationship.isList
+                                ? `[${unionMemberEntity.name}ConnectInput!]`
+                                : `${unionMemberEntity.name}ConnectInput`,
+                        });
+                    }
+
+                    if (hasNonGeneratedProperties) {
+                        tc.addFields({
+                            edge: `${relationship.propertiesTypeName}CreateInput${
+                                hasNonNullNonGeneratedProperties ? `!` : ""
+                            }`,
+                        });
+                    }
+                });
+
+                unionConnectInput.addFields({
+                    [unionMemberEntity.name]: connect,
+                });
+
+                if (updateFields) {
+                    updateFields.connect = connect;
+                }
+                if (fieldInputFields) {
+                    fieldInputFields.connect = connect;
+                }
+            }
+        }
+
+        if (
+            nestedOperations.has(RelationshipNestedOperationsOption.CONNECT_OR_CREATE) &&
+            (updateFields || fieldInputFields)
+        ) {
+            const connectOrCreate = createConnectOrCreateField2({
+                relationshipAdapter: relationship,
+                targetEntityAdapter: unionMemberEntity,
+                schemaComposer,
+                userDefinedFieldDirectives,
+            });
+
+            if (connectOrCreate) {
+                if (updateFields) {
+                    updateFields.connectOrCreate = connectOrCreate;
+                }
+                if (fieldInputFields) {
+                    fieldInputFields.connectOrCreate = connectOrCreate;
+                }
+            }
+        }
+
+        if (unionDeleteInput && updateFields) {
+            if (!schemaComposer.has(deleteName)) {
+                schemaComposer.getOrCreateITC(deleteName, (tc) => {
+                    tc.addFields({ where: whereName });
+
+                    if (unionMemberEntity.relationships.size) {
+                        tc.addFields({
+                            delete: `${unionMemberEntity.name}DeleteInput`,
+                        });
+                    }
+                });
+
+                unionDeleteInput.addFields({
+                    [unionMemberEntity.name]: deleteField,
+                });
+            }
+
+            updateFields.delete = relationship.isList ? `[${deleteName}!]` : deleteName;
+        }
+
+        if (unionDisconnectInput && updateFields) {
+            if (!schemaComposer.has(disconnectName)) {
+                schemaComposer.getOrCreateITC(disconnectName, (tc) => {
+                    tc.addFields({ where: whereName });
+
+                    if (unionMemberEntity.relationships.size) {
+                        tc.addFields({
+                            disconnect: `${unionMemberEntity.name}DisconnectInput`,
+                        });
+                    }
+                });
+
+                unionDisconnectInput.addFields({
+                    [unionMemberEntity.name]: disconnect,
+                });
+
+                updateFields.disconnect = relationship.isList ? `[${disconnectName}!]` : disconnectName;
+            }
+        }
+
+        if (updateFields) {
+            const updateName = `${unionPrefix}UpdateFieldInput`;
+            const update = relationship.isList ? `[${updateName}!]` : updateName;
+            if (nestedOperations.has(RelationshipNestedOperationsOption.UPDATE)) {
+                updateFields.update = connectionUpdateInputName;
+            }
+            if (!schemaComposer.has(updateName)) {
+                schemaComposer.createInputTC({
+                    name: updateName,
+                    fields: updateFields,
+                });
+
+                unionUpdateInput.addFields({
+                    [unionMemberEntity.name]: update,
+                });
+            }
+
+            schemaComposer.getOrCreateITC(connectionUpdateInputName, (tc) => {
+                tc.addFields({ node: updateField });
+
+                if (hasNonGeneratedProperties) {
+                    tc.addFields({
+                        edge: `${relationship.propertiesTypeName}UpdateInput`,
+                    });
+                }
+            });
+        }
+
+        if (fieldInputFields) {
+            schemaComposer.createInputTC({
+                name: nodeFieldInputName,
+                fields: fieldInputFields,
+            });
+        }
+
+        schemaComposer.getOrCreateITC(whereName, (tc) => {
+            tc.addFields({
+                node: `${unionMemberEntity.name}Where`,
+                node_NOT: {
+                    type: `${unionMemberEntity.name}Where`,
+                    directives: [DEPRECATE_NOT],
+                },
+                AND: `[${whereName}!]`,
+                OR: `[${whereName}!]`,
+                NOT: whereName,
+            });
+
+            if (relationship.propertiesTypeName) {
+                tc.addFields({
+                    edge: `${relationship.propertiesTypeName}Where`,
+                    edge_NOT: {
+                        type: `${relationship.propertiesTypeName}Where`,
+                        directives: [DEPRECATE_NOT],
+                    },
+                });
+            }
+        });
+
+        if (connectAndCreateAndUniqueFields) {
+            // TODO: merge with createTopLevelConnectOrCreateInput
+            const nodeConnectOrCreateInput: InputTypeComposer<any> = schemaComposer.getOrCreateITC(
+                `${sourceAdapter.name}ConnectOrCreateInput`
+            );
+
+            const nodeRelationConnectOrCreateInput: InputTypeComposer<any> = schemaComposer.getOrCreateITC(
+                `${sourceAdapter.name}${upperFirst(relationship.name)}ConnectOrCreateInput`
+            );
+
+            nodeConnectOrCreateInput.addFields({
+                [relationship.name]: nodeRelationConnectOrCreateInput,
+            });
+
+            const nodeFieldConnectOrCreateInputName = `${sourceAdapter.name}${upperFirst(relationship.name)}${
+                unionMemberEntity.name
+            }ConnectOrCreateFieldInput`;
+
+            nodeRelationConnectOrCreateInput.addFields({
+                [unionMemberEntity.name]: relationship.isList
+                    ? `[${nodeFieldConnectOrCreateInputName}!]`
+                    : nodeFieldConnectOrCreateInputName,
+            });
+        }
+    });
+
+    if (nestedOperations.has(RelationshipNestedOperationsOption.CREATE)) {
+        const nodeRelationInput = schemaComposer.getOrCreateITC(`${sourceAdapter.name}RelationInput`);
+        nodeRelationInput.addFields({
+            [relationship.name]: unionCreateFieldInput,
+        });
+    }
+    if (relationship.isCreatable() && !(composeNode instanceof InterfaceTypeComposer) && unionCreateInput) {
+        nodeCreateInput.addFields({
+            [relationship.name]: unionCreateInput,
+        });
+    }
+    if (
+        relationship.isUpdatable() &&
+        nestedOperations.size !== 0 &&
+        !onlyConnectOrCreateAndNoUniqueFieldsInAllRefTypes
+    ) {
+        const nodeUpdateInput = schemaComposer.getITC(`${sourceAdapter.name}UpdateInput`);
+        nodeUpdateInput.addFields({
+            [relationship.name]: unionUpdateInput,
+        });
+    }
+    if (unionConnectInput) {
+        const nodeConnectInput = schemaComposer.getOrCreateITC(`${sourceAdapter.name}ConnectInput`);
+        nodeConnectInput.addFields({
+            [relationship.name]: unionConnectInput,
+        });
+    }
+    if (unionDeleteInput) {
+        const nodeDeleteInput = schemaComposer.getOrCreateITC(`${sourceAdapter.name}DeleteInput`);
+        nodeDeleteInput.addFields({
+            [relationship.name]: unionDeleteInput,
+        });
+    }
+    if (unionDisconnectInput) {
+        const nodeDisconnectInput = schemaComposer.getOrCreateITC(`${sourceAdapter.name}DisconnectInput`);
+        nodeDisconnectInput.addFields({
+            [relationship.name]: unionDisconnectInput,
         });
     }
 }
