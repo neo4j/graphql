3c45856960c888d1571500d73974f5ee26128e90
angrykoala <angrykoala@outlook.es>
2023-09-27 15:01:53 +0200

    Add license headers to queryAST

--- packages/graphql/src/schema-model/parser/annotations-parser/private-annotation.ts
+++ packages/graphql/src/schema-model/parser/annotations-parser/private-annotation.ts
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import type { DirectiveNode } from "graphql";
 import { PrivateAnnotation } from "../../annotation/PrivateAnnotation";
 
