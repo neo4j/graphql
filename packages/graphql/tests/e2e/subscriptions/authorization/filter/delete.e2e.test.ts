eb0422410f54319c078337c4eb62cc95b2ac8826
angrykoala <angrykoala@outlook.es>
2023-10-05 11:36:01 +0100

    Use default subscription engine in e2e tests

--- packages/graphql/tests/e2e/subscriptions/authorization/filter/delete.e2e.test.ts
+++ packages/graphql/tests/e2e/subscriptions/authorization/filter/delete.e2e.test.ts
@@ -24,10 +24,10 @@ import { Neo4jGraphQL } from "../../../../../src/classes";
 import { UniqueType } from "../../../../utils/graphql-types";
 import type { TestGraphQLServer } from "../../../setup/apollo-server";
 import { ApolloTestServer } from "../../../setup/apollo-server";
-import { TestSubscriptionsEngine } from "../../../../utils/TestSubscriptionsEngine";
 import { WebSocketTestClient } from "../../../setup/ws-client";
 import Neo4j from "../../../setup/neo4j";
 import { createJwtHeader } from "../../../../utils/create-jwt-request";
+import { Neo4jGraphQLSubscriptionsDefaultEngine } from "../../../../../src/classes/subscription/Neo4jGraphQLSubscriptionsDefaultEngine";
 
 describe("Subscriptions authorization with delete events", () => {
     let neo4j: Neo4j;
@@ -66,7 +66,7 @@ describe("Subscriptions authorization with delete events", () => {
             driver,
             features: {
                 authorization: { key },
-                subscriptions: new TestSubscriptionsEngine(),
+                subscriptions: new Neo4jGraphQLSubscriptionsDefaultEngine(),
             },
         });
 
