9be11fa71a4789c82168b9bb7a58bd4382676297
Andreas Berger <andreas@berger-ecommerce.com>
2024-01-09 16:49:20 +0100

    chore: clean up leftovers of @exclude-directive

--- packages/graphql/src/classes/index.ts
+++ packages/graphql/src/classes/index.ts
@@ -18,7 +18,6 @@
  */
 
 export * from "./Error";
-export { default as Exclude, ExcludeConstructor } from "./Exclude";
 export { GraphElement } from "./GraphElement";
 export { Neo4jDatabaseInfo } from "./Neo4jDatabaseInfo";
 export { default as Neo4jGraphQL, Neo4jGraphQLConstructor } from "./Neo4jGraphQL";
