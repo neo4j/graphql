916c37eac96fda54e7d0c33384c0039fc1f989cd
Andreas Berger <andreas@berger-ecommerce.com>
2024-01-16 15:43:58 +0100

    fix: #4514 augment interfaces the same way no matter if they are used as the target of a relationship or not

--- packages/graphql/src/schema/generation/interface-type.ts
+++ packages/graphql/src/schema/generation/interface-type.ts
@@ -20,28 +20,18 @@ import type { DirectiveNode } from "graphql";
 import type { InterfaceTypeComposer, SchemaComposer } from "graphql-compose";
 import { InterfaceEntityAdapter } from "../../schema-model/entity/model-adapters/InterfaceEntityAdapter";
 import type { RelationshipAdapter } from "../../schema-model/relationship/model-adapters/RelationshipAdapter";
-import {
-    attributeAdapterToComposeFields,
-    graphqlDirectivesToCompose,
-    relationshipAdapterToComposeFields,
-} from "../to-compose";
+import { attributeAdapterToComposeFields, graphqlDirectivesToCompose } from "../to-compose";
 
 export function withInterfaceType({
     entityAdapter,
     userDefinedFieldDirectives,
     userDefinedInterfaceDirectives,
     composer,
-    config = {
-        includeRelationships: false,
-    },
 }: {
     entityAdapter: InterfaceEntityAdapter | RelationshipAdapter;
     userDefinedFieldDirectives: Map<string, DirectiveNode[]>;
     userDefinedInterfaceDirectives: DirectiveNode[];
     composer: SchemaComposer;
-    config?: {
-        includeRelationships: boolean;
-    };
 }): InterfaceTypeComposer {
     // TODO: maybe create interfaceEntity.interfaceFields() method abstraction even if it retrieves all attributes?
     // can also take includeRelationships as argument
@@ -49,24 +39,13 @@ export function withInterfaceType({
         Array.from(entityAdapter.attributes.values()),
         userDefinedFieldDirectives
     );
-    let fields = objectComposeFields;
-    if (config.includeRelationships && entityAdapter instanceof InterfaceEntityAdapter) {
-        fields = {
-            ...fields,
-            ...relationshipAdapterToComposeFields(
-                Array.from(entityAdapter.relationships.values()),
-                userDefinedFieldDirectives
-            ),
-        };
-    }
     const interfaceTypeName =
         entityAdapter instanceof InterfaceEntityAdapter
             ? entityAdapter.name
             : (entityAdapter.propertiesTypeName as string); // this is checked one layer above in execution
-    const composeInterface = composer.createInterfaceTC({
+    return composer.createInterfaceTC({
         name: interfaceTypeName,
-        fields: fields,
+        fields: objectComposeFields,
         directives: graphqlDirectivesToCompose(userDefinedInterfaceDirectives),
     });
-    return composeInterface;
 }
