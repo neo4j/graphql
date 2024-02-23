47b064ad9c98b4ef94e532dccebb835d906fb215
MacondoExpress <simone.gammicchia@gmail.com>
2024-02-23 16:01:33 +0000

    reintroduce legacy support for null on relationship filter 1..1

--- packages/graphql/tests/tck/aggregations/where/node/logical.test.ts
+++ packages/graphql/tests/tck/aggregations/where/node/logical.test.ts
@@ -17,10 +17,10 @@
  * limitations under the License.
  */
 
-import { gql } from "graphql-tag";
 import type { DocumentNode } from "graphql";
+import { gql } from "graphql-tag";
 import { Neo4jGraphQL } from "../../../../../src";
-import { formatCypher, translateQuery, formatParams } from "../../../utils/tck-test-utils";
+import { formatCypher, formatParams, translateQuery } from "../../../utils/tck-test-utils";
 
 describe("Cypher Aggregations where node with Logical AND + OR", () => {
     let typeDefs: DocumentNode;
