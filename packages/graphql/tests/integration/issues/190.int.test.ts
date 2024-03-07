c2dc41e99fa6dbe3736e0f901307ac37bc00bee5
angrykoala <angrykoala@outlook.es>
2024-03-07 12:07:02 +0000

    Add test helpers

--- packages/graphql/tests/integration/issues/190.int.test.ts
+++ packages/graphql/tests/integration/issues/190.int.test.ts
@@ -18,26 +18,21 @@
  */
 
 import type { DocumentNode } from "graphql";
-import { graphql } from "graphql";
 import { gql } from "graphql-tag";
-import type { Driver } from "neo4j-driver";
-import { Neo4jGraphQL } from "../../../src/classes";
-import { UniqueType } from "../../utils/graphql-types";
-import Neo4jHelper from "../neo4j";
+import type { UniqueType } from "../../utils/graphql-types";
+import { TestHelper } from "../utils/tests-helper";
 
 describe("https://github.com/neo4j/graphql/issues/190", () => {
-    let driver: Driver;
-    let neo4j: Neo4jHelper;
     let User: UniqueType;
     let UserDemographics: UniqueType;
     let typeDefs: DocumentNode;
 
+    let testHelper: TestHelper;
+
     beforeAll(async () => {
-        neo4j = new Neo4jHelper();
-        driver = await neo4j.getDriver();
-        const session = await neo4j.getSession();
-        User = new UniqueType("User");
-        UserDemographics = new UniqueType("UserDemographics");
+        testHelper = new TestHelper();
+        User = testHelper.getUniqueType("User");
+        UserDemographics = testHelper.getUniqueType("UserDemographics");
 
         typeDefs = gql`
         type ${User} {
@@ -54,9 +49,7 @@ describe("https://github.com/neo4j/graphql/issues/190", () => {
         }
     `;
 
-        try {
-            await session.run(
-                `
+        await testHelper.runCypher(`
                     CREATE (user1:${User} {uid: 'user1'}),(user2:${User} {uid: 'user2'}),(female:${UserDemographics}{type:'Gender',value:'Female'}),(male:${UserDemographics}{type:'Gender',value:'Male'}),(age:${UserDemographics}{type:'Age',value:'50+'}),(state:${UserDemographics}{type:'State',value:'VIC'})
                     CREATE (user1)-[:HAS_DEMOGRAPHIC]->(female)
                     CREATE (user2)-[:HAS_DEMOGRAPHIC]->(male)
@@ -64,34 +57,16 @@ describe("https://github.com/neo4j/graphql/issues/190", () => {
                     CREATE (user2)-[:HAS_DEMOGRAPHIC]->(age)
                     CREATE (user1)-[:HAS_DEMOGRAPHIC]->(state)
                     CREATE (user2)-[:HAS_DEMOGRAPHIC]->(state)
-                `
-            );
-        } finally {
-            await session.close();
-        }
+                `);
+        await testHelper.initNeo4jGraphQL({ typeDefs });
     });
 
     afterAll(async () => {
-        const session = await neo4j.getSession();
-
-        try {
-            await session.run(`MATCH (u:${User}) WHERE (u.uid = 'user1' OR u.uid = 'user2') DETACH DELETE u`);
-            await session.run(
-                `MATCH (ud:${UserDemographics}) WHERE ((ud.type = 'Gender' AND ud.value = 'Female') OR (ud.type = 'Gender' AND ud.value = 'Male') OR (ud.type = 'Age' AND ud.value = '50+') OR (ud.type = 'State' AND ud.value = 'VIC')) DELETE ud`
-            );
-        } finally {
-            await session.close();
-        }
-
-        await driver.close();
+        await testHelper.close();
     });
 
     test("Example 1", async () => {
-        const session = await neo4j.getSession();
-
-        const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
-
-        const query = `
+        const query = /* GraphQL */ `
             query {
                 ${User.plural}(where: { demographics: { type: "Gender", value: "Female" } }) {
                     uid
@@ -103,41 +78,27 @@ describe("https://github.com/neo4j/graphql/issues/190", () => {
             }
         `;
 
-        try {
-            await neoSchema.checkNeo4jCompat();
+        const result = await testHelper.runGraphQL(query);
 
-            const result = await graphql({
-                schema: await neoSchema.getSchema(),
-                source: query,
-                contextValue: neo4j.getContextValues(),
-            });
-
-            expect(result.errors).toBeFalsy();
-            expect((result?.data as any)?.[User.plural][0].uid).toBe("user1");
-            expect((result?.data as any)?.[User.plural][0].demographics).toHaveLength(3);
-            expect((result?.data as any)?.[User.plural][0].demographics).toContainEqual({
-                type: "Age",
-                value: "50+",
-            });
-            expect((result?.data as any)?.[User.plural][0].demographics).toContainEqual({
-                type: "Gender",
-                value: "Female",
-            });
-            expect((result?.data as any)?.[User.plural][0].demographics).toContainEqual({
-                type: "State",
-                value: "VIC",
-            });
-        } finally {
-            await session.close();
-        }
+        expect(result.errors).toBeFalsy();
+        expect((result?.data as any)?.[User.plural][0].uid).toBe("user1");
+        expect((result?.data as any)?.[User.plural][0].demographics).toHaveLength(3);
+        expect((result?.data as any)?.[User.plural][0].demographics).toContainEqual({
+            type: "Age",
+            value: "50+",
+        });
+        expect((result?.data as any)?.[User.plural][0].demographics).toContainEqual({
+            type: "Gender",
+            value: "Female",
+        });
+        expect((result?.data as any)?.[User.plural][0].demographics).toContainEqual({
+            type: "State",
+            value: "VIC",
+        });
     });
 
     test("Example 2", async () => {
-        const session = await neo4j.getSession();
-
-        const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
-
-        const query = `
+        const query = /* GraphQL */ `
             query {
                 ${User.plural}(
                     where: {
@@ -153,64 +114,38 @@ describe("https://github.com/neo4j/graphql/issues/190", () => {
             }
         `;
 
-        try {
-            await neoSchema.checkNeo4jCompat();
+        const result = await testHelper.runGraphQL(query);
 
-            const result = await graphql({
-                schema: await neoSchema.getSchema(),
-                source: query,
-                contextValue: neo4j.getContextValues(),
-            });
+        expect(result.errors).toBeFalsy();
 
-            expect(result.errors).toBeFalsy();
+        expect(result?.data?.[User.plural]).toHaveLength(2);
 
-            expect(result?.data?.[User.plural]).toHaveLength(2);
+        expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user1")[0].demographics).toHaveLength(3);
+        expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user1")[0].demographics).toContainEqual({
+            type: "Gender",
+            value: "Female",
+        });
+        expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user1")[0].demographics).toContainEqual({
+            type: "State",
+            value: "VIC",
+        });
+        expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user1")[0].demographics).toContainEqual({
+            type: "Age",
+            value: "50+",
+        });
 
-            expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user1")[0].demographics).toHaveLength(
-                3
-            );
-            expect(
-                (result?.data as any)?.[User.plural].filter((u) => u.uid === "user1")[0].demographics
-            ).toContainEqual({
-                type: "Gender",
-                value: "Female",
-            });
-            expect(
-                (result?.data as any)?.[User.plural].filter((u) => u.uid === "user1")[0].demographics
-            ).toContainEqual({
-                type: "State",
-                value: "VIC",
-            });
-            expect(
-                (result?.data as any)?.[User.plural].filter((u) => u.uid === "user1")[0].demographics
-            ).toContainEqual({
-                type: "Age",
-                value: "50+",
-            });
-
-            expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user2")[0].demographics).toHaveLength(
-                3
-            );
-            expect(
-                (result?.data as any)?.[User.plural].filter((u) => u.uid === "user2")[0].demographics
-            ).toContainEqual({
-                type: "Gender",
-                value: "Male",
-            });
-            expect(
-                (result?.data as any)?.[User.plural].filter((u) => u.uid === "user2")[0].demographics
-            ).toContainEqual({
-                type: "State",
-                value: "VIC",
-            });
-            expect(
-                (result?.data as any)?.[User.plural].filter((u) => u.uid === "user2")[0].demographics
-            ).toContainEqual({
-                type: "Age",
-                value: "50+",
-            });
-        } finally {
-            await session.close();
-        }
+        expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user2")[0].demographics).toHaveLength(3);
+        expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user2")[0].demographics).toContainEqual({
+            type: "Gender",
+            value: "Male",
+        });
+        expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user2")[0].demographics).toContainEqual({
+            type: "State",
+            value: "VIC",
+        });
+        expect((result?.data as any)?.[User.plural].filter((u) => u.uid === "user2")[0].demographics).toContainEqual({
+            type: "Age",
+            value: "50+",
+        });
     });
 });
---
+++ packages/graphql/tests/integration/utils/tests-helper.ts
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Cypher from "@neo4j/cypher-builder";
+import type { ExecutionResult } from "graphql";
+import { graphql as graphqlRuntime } from "graphql";
+import * as neo4j from "neo4j-driver";
+import { Memoize } from "typescript-memoize";
+import type { Neo4jGraphQLConstructor, Neo4jGraphQLContext } from "../../../src";
+import { Neo4jGraphQL } from "../../../src";
+import { UniqueType } from "../../utils/graphql-types";
+
+const INT_TEST_DB_NAME = "neo4jgraphqlinttestdatabase";
+const DEFAULT_DB = "neo4j";
+
+export class TestHelper {
+    // private driver: neo4j.Driver | undefined;
+    // private hasIntegrationTestDb: boolean;
+
+    private database: string = DEFAULT_DB;
+
+    private neo4jGraphQL: Neo4jGraphQL | undefined;
+
+    private uniqueTypes: UniqueType[] = [];
+
+    constructor() {
+        // this.hasIntegrationTestDb = false;
+    }
+
+    public async runCypher(query: string, params: Record<string, unknown> = {}): Promise<neo4j.QueryResult> {
+        const driver = await this.getDriver();
+        return driver.executeQuery(query, params, { database: this.database });
+    }
+
+    public async initNeo4jGraphQL(options: Omit<Neo4jGraphQLConstructor, "driver">): Promise<Neo4jGraphQL> {
+        if (this.neo4jGraphQL) {
+            throw new Error("Neo4jGraphQL already initialized");
+        }
+        const driver = await this.getDriver();
+        this.neo4jGraphQL = new Neo4jGraphQL({
+            ...options,
+            driver,
+        });
+        return this.neo4jGraphQL;
+    }
+
+    public getUniqueType(type: string): UniqueType {
+        const uniqueType = new UniqueType(type);
+        this.uniqueTypes.push(uniqueType);
+        return uniqueType;
+    }
+
+    public async runGraphQL(query: string): Promise<ExecutionResult> {
+        if (!this.neo4jGraphQL) {
+            throw new Error("Neo4j GraphQL not ready. Did you forget calling 'initNeo4jGraphQL'?");
+        }
+        const schema = await this.neo4jGraphQL.getSchema();
+        return graphqlRuntime({
+            schema,
+            source: query,
+            contextValue: await this.getContextValues(),
+        });
+    }
+
+    public async close(preClose?: () => Promise<void>): Promise<void> {
+        const driver = await this.getDriver();
+        if (preClose) {
+            try {
+                await preClose();
+            } catch (err) {
+                // Ignore error
+            }
+        }
+        await this.cleanNodes(driver, this.uniqueTypes); // TODO: take the database into account
+        await driver.close();
+    }
+
+    private async getContextValues(options?: Record<string, unknown>): Promise<Neo4jGraphQLContext> {
+        const driver = await this.getDriver();
+        return {
+            executionContext: driver,
+            sessionConfig: { database: this.database },
+            ...(options || {}),
+        };
+    }
+
+    @Memoize()
+    public async getDriver(): Promise<neo4j.Driver> {
+        const { NEO_USER = "neo4j", NEO_PASSWORD = "password", NEO_URL = "neo4j://localhost:7687/neo4j" } = process.env;
+
+        // if (process.env.NEO_WAIT) {
+        //     await util.promisify(setTimeout)(Number(process.env.NEO_WAIT));
+        // }
+
+        const auth = neo4j.auth.basic(NEO_USER, NEO_PASSWORD);
+        const driver = neo4j.driver(NEO_URL, auth);
+        try {
+            this.database = await this.checkConnectivity(driver);
+        } catch (error: any) {
+            await driver.close();
+            throw new Error(
+                `Could not connect to neo4j @ ${NEO_URL}, database ${INT_TEST_DB_NAME}, Error: ${error.message}`
+            );
+        }
+
+        return driver;
+    }
+
+    private async checkConnectivity(driver: neo4j.Driver): Promise<string> {
+        if (process.env.USE_DEFAULT_DB) {
+            return this.checkConnectivityToDefaultDatabase(driver);
+        } else {
+            return this.verifyConnectivityToTestDatabase(driver);
+        }
+    }
+
+    private async checkConnectivityToDefaultDatabase(driver: neo4j.Driver): Promise<string> {
+        await driver.verifyConnectivity();
+        return DEFAULT_DB;
+    }
+
+    private async verifyConnectivityToTestDatabase(driver: neo4j.Driver): Promise<string> {
+        try {
+            await driver.verifyConnectivity({ database: INT_TEST_DB_NAME });
+            return INT_TEST_DB_NAME;
+        } catch (error: any) {
+            if (
+                error.message.includes("Could not perform discovery. No routing servers available.") ||
+                error.message.includes(
+                    `Unable to get a routing table for database '${INT_TEST_DB_NAME}' because this database does not exist`
+                ) ||
+                error.message.includes(`Database does not exist. Database name: '${INT_TEST_DB_NAME}'`)
+            ) {
+                await this.checkConnectivityToDefaultDatabase(driver);
+                return DEFAULT_DB;
+            } else {
+                throw error;
+            }
+        }
+    }
+
+    /** Removes all nodes with the given labels from the database */
+    private async cleanNodes(driver: neo4j.Driver, labels: Array<string | UniqueType>): Promise<neo4j.Result> {
+        const nodeRef = new Cypher.Node({});
+
+        const nodeHasLabelPredicates = labels.map((l) => {
+            return nodeRef.hasLabel(`${l}`);
+        });
+
+        const nodeHasAnyLabelPredicate = Cypher.or(...nodeHasLabelPredicates);
+
+        const query = new Cypher.Match(nodeRef).where(nodeHasAnyLabelPredicate).detachDelete(nodeRef);
+        const { cypher } = query.build();
+        return driver.executeQuery(cypher, {}, { database: this.database });
+    }
+}
