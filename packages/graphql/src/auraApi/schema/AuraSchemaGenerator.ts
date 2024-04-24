7cd5f92739133b3fa200022ba00a5cc2fed357ab
MacondoExpress <simone.gammicchia@gmail.com>
2024-04-24 12:33:16 +0100

    add QueryType and use AuraEntityOperations

--- packages/graphql/src/auraApi/schema/AuraSchemaGenerator.ts
+++ packages/graphql/src/auraApi/schema/AuraSchemaGenerator.ts
@@ -1,6 +1,8 @@
+import { printSchemaWithDirectives } from "@graphql-tools/utils";
 import { type GraphQLSchema } from "graphql";
 import type { Neo4jGraphQLSchemaModel } from "../../schema-model/Neo4jGraphQLSchemaModel";
 import type { ConcreteEntity } from "../../schema-model/entity/ConcreteEntity";
+import { ConcreteEntityOperations } from "../AuraEntityOperations";
 import { SchemaBuilder } from "./SchemaBuilder";
 
 export class AuraSchemaGenerator {
@@ -11,21 +13,49 @@ export class AuraSchemaGenerator {
     }
 
     public generate({ schemaModel }: { schemaModel: Neo4jGraphQLSchemaModel }): GraphQLSchema {
-        const schemaBuilder = new SchemaBuilder();
-
         // 1. Use the schemaModel
         for (const entity of schemaModel.entities.values()) {
             if (entity.isConcreteEntity()) {
-                this.createObjectType(entity);
+                this.concreteEntitySteps(entity);
             }
         }
-
-        return schemaBuilder.build();
+        const schema = this.schemaBuilder.build();
+        console.log(printSchemaWithDirectives(schema));
+        return schema;
     }
 
-    private createObjectType(concreteEntity: ConcreteEntity): void {
-        console.log(concreteEntity);
+    private concreteEntitySteps(concreteEntity: ConcreteEntity) {
+        // steps are:
+        // 1. Movie
+        // 2. MovieEdge
+        // 3. MovieConnection
+        // 4. MovieOperation
+        // 5. Query with movies: MovieOperation
 
-        this.schemaBuilder.createObjectType(concreteEntity.name);
+        this.createEntityType(concreteEntity);
+        this.createEntityEdgeType(concreteEntity);
+        this.addQueryField(concreteEntity);
+    }
+
+    private addQueryField(concreteEntity: ConcreteEntity) {
+        const objectType = this.schemaBuilder.getObjectType(concreteEntity.name);
+        const entityOperations = new ConcreteEntityOperations(concreteEntity);
+        this.schemaBuilder.addQueryField(concreteEntity.name, entityOperations.edgeType);
+    }
+
+    private createEntityEdgeType(concreteEntity: ConcreteEntity): void {
+        const entityOperations = new ConcreteEntityOperations(concreteEntity);
+        this.schemaBuilder.createObjectType(entityOperations.edgeType, entityOperations.edgeFields);
+    }
+
+    private createEntityType(concreteEntity: ConcreteEntity): void {
+        const fields = this.getObjectFields(concreteEntity);
+        this.schemaBuilder.createObjectType(concreteEntity.name, fields);
+    }
+
+    private getObjectFields(concreteEntity: ConcreteEntity): Record<string, string> {
+        return Object.fromEntries(
+            [...concreteEntity.attributes.values()].map((attribute) => [attribute.name, attribute.type.name])
+        );
     }
 }
--- packages/graphql/src/auraApi/schema/SchemaBuilder.ts
+++ packages/graphql/src/auraApi/schema/SchemaBuilder.ts
@@ -1,4 +1,5 @@
 import type { GraphQLSchema } from "graphql";
+import type { ObjectTypeComposer } from "graphql-compose";
 import { SchemaComposer } from "graphql-compose";
 
 export class SchemaBuilder {
@@ -8,11 +9,25 @@ export class SchemaBuilder {
         this.composer = new SchemaComposer();
     }
 
-    public createObjectType(name: string, description?: string) {
+    public createObjectType(name: string, fields: Record<string, any>, description?: string) {
         this.composer.createObjectTC({
             name,
             description,
+            fields,
         });
+    }
+
+    public addQueryField(name: string, type: ObjectTypeComposer | string) {
+        this.composer.Query.addFields({
+            [name]: {
+                type: type,
+                resolve: () => null,
+            },
+        });
+    }
+
+    public getObjectType(typeName: string) {
+        return this.composer.getOTC(typeName);
     }
 
     public addFieldToObjectType(typeName: string) {
