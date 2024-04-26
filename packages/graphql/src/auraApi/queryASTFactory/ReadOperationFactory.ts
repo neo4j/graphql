baf31202cbec73c5b5e1174e671eb79fc3c4de3f
angrykoala <angrykoala@outlook.es>
2024-04-26 14:58:37 +0100

    ResolveTree parser

--- packages/graphql/src/auraApi/queryASTFactory/ReadOperationFactory.ts
+++ packages/graphql/src/auraApi/queryASTFactory/ReadOperationFactory.ts
@@ -4,16 +4,114 @@ import { AttributeAdapter } from "../../schema-model/attribute/model-adapters/At
 import type { ConcreteEntity } from "../../schema-model/entity/ConcreteEntity";
 import { ConcreteEntityAdapter } from "../../schema-model/entity/model-adapters/ConcreteEntityAdapter";
 import { QueryAST } from "../../translate/queryAST/ast/QueryAST";
-import type { Field } from "../../translate/queryAST/ast/fields/Field";
 import { AttributeField } from "../../translate/queryAST/ast/fields/attribute-fields/AttributeField";
 import { ConnectionReadOperation } from "../../translate/queryAST/ast/operations/ConnectionReadOperation";
 import type { Operation } from "../../translate/queryAST/ast/operations/operations";
-import type { EntitySelection } from "../../translate/queryAST/ast/selection/EntitySelection";
 import { NodeSelection } from "../../translate/queryAST/ast/selection/NodeSelection";
 import { AuraEntityOperations } from "../AuraEntityOperations";
 
+/** Nested Records helper type, supports any level of recursion. Ending in properties of type T */
+// export interface NestedRecord<T> extends Record<string | symbol | number, T | NestedRecord<T>> {} // Using interface to allow recursive types
+
+type ResolveTreeElement<T extends Record<string, ResolveTreeEntityField | ResolveTreeElement<any>>> = {
+    alias: string;
+    args: Record<string, any>;
+    fields: T;
+};
+
+type ResolveTreeEntityField = {
+    alias: string;
+    args: Record<string, any>;
+};
+type ResolveTreeNode = ResolveTreeElement<Record<string, ResolveTreeEntityField>>;
+type ResolveTreeEdge = ResolveTreeElement<{ node?: ResolveTreeNode }>;
+type ResolveTreeConnection = ResolveTreeElement<{ edges?: ResolveTreeEdge }>;
+
+type AuraAPIResolveTree = ResolveTreeElement<{ connection?: ResolveTreeConnection }>;
+
+class ResolveTreeParser {
+    public parse(concreteEntity: ConcreteEntity, resolveTree: ResolveTree): AuraAPIResolveTree {
+        const auraOps = new AuraEntityOperations(concreteEntity);
+        const fieldsByTypeName = resolveTree.fieldsByTypeName[auraOps.connectionOperation] ?? {};
+
+        const connectionResolveTree = fieldsByTypeName["connection"];
+        const connection = connectionResolveTree
+            ? this.parseConnection(concreteEntity, auraOps, connectionResolveTree)
+            : undefined;
+
+        return {
+            alias: resolveTree.alias,
+            args: resolveTree.args,
+            fields: {
+                connection,
+            },
+        };
+    }
+
+    private parseConnection(
+        concreteEntity: ConcreteEntity,
+        auraOps: AuraEntityOperations,
+        connectionResolveTree: ResolveTree
+    ): ResolveTreeConnection {
+        const edgesResolveTree = connectionResolveTree.fieldsByTypeName[auraOps.connectionType]?.["edges"];
+        const edgeResolveTree = edgesResolveTree
+            ? this.parseEdges(concreteEntity, auraOps, edgesResolveTree)
+            : undefined;
+        return {
+            alias: connectionResolveTree.alias,
+            args: connectionResolveTree.args,
+            fields: {
+                edges: edgeResolveTree,
+            },
+        };
+    }
+
+    private parseEdges(
+        concreteEntity: ConcreteEntity,
+        auraOps: AuraEntityOperations,
+        connectionResolveTree: ResolveTree
+    ): ResolveTreeEdge {
+        const nodeResolveTree = connectionResolveTree.fieldsByTypeName[auraOps.edgeType]?.["node"];
+
+        const node = nodeResolveTree ? this.parseEntity(concreteEntity, auraOps, nodeResolveTree) : undefined;
+
+        return {
+            alias: connectionResolveTree.alias,
+            args: connectionResolveTree.args,
+            fields: {
+                node: node,
+            },
+        };
+    }
+
+    private parseEntity(
+        concreteEntity: ConcreteEntity,
+        auraOps: AuraEntityOperations,
+        nodeResolveTree: ResolveTree
+    ): ResolveTreeEdge {
+        const fieldsResolveTree = nodeResolveTree.fieldsByTypeName[auraOps.nodeType] ?? {};
+        const fields = Object.entries(fieldsResolveTree).reduce(
+            (acc, [key, f]): Record<string, ResolveTreeEntityField> => {
+                acc[key] = {
+                    alias: f.alias,
+                    args: f.args,
+                };
+                return acc;
+            },
+            {}
+        );
+
+        return {
+            alias: nodeResolveTree.alias,
+            args: nodeResolveTree.args,
+            fields: fields,
+        };
+    }
+}
+
 export class ReadOperationFactory {
     public schemaModel: Neo4jGraphQLSchemaModel;
+
     constructor(schemaModel: Neo4jGraphQLSchemaModel) {
         this.schemaModel = schemaModel;
     }
@@ -27,129 +125,56 @@ export class ReadOperationFactory {
         entity: ConcreteEntity;
         context;
     }): QueryAST {
-        // console.log(JSON.stringify(resolveTree, null, 2));
-        const operation = this.parseResolveTree({ resolveTree, entity, context });
+        const resolveTreeParser = new ResolveTreeParser();
+
+        const parsedTree = resolveTreeParser.parse(entity, resolveTree);
+        const operation = this.generateOperation({
+            parsedTree: parsedTree,
+            entity,
+            context,
+        });
         return new QueryAST(operation);
     }
 
-    private parseResolveTree({
-        resolveTree,
+    private generateOperation({
+        parsedTree,
         entity,
         context,
     }: {
-        resolveTree: ResolveTree;
+        parsedTree: AuraAPIResolveTree;
         entity: ConcreteEntity;
         context;
     }): Operation {
-        const entityOperations = new AuraEntityOperations(entity);
-        const operationEntry = resolveTree.fieldsByTypeName[entityOperations.connectionOperation];
-        if (!operationEntry) {
-            throw new Error(`Transpile Error: OperationType ${entityOperations.connectionOperation} not found`);
+        const connectionTree = parsedTree.fields.connection;
+        if (!connectionTree) {
+            throw new Error("No Connection");
         }
+        const target = new ConcreteEntityAdapter(entity);
+        const selection = new NodeSelection({
+            target,
+        });
 
-        const connectionField = operationEntry["connection"];
-        if (connectionField) {
-            const resolveConnectionField = connectionField.fieldsByTypeName[entityOperations.connectionType];
-            if (resolveConnectionField) {
-                return this.parseConnectionOperation({
-                    connectionEntry: resolveConnectionField,
-                    entity,
-                    entityOperations,
-                    context,
-                });
+        const nodeFieldsTree = connectionTree.fields.edges?.fields.node?.fields ?? {};
+
+        const nodeFields = Object.entries(nodeFieldsTree).map(([name, rawField]) => {
+            const attribute = entity.attributes.get(name);
+            if (!attribute) {
+                throw new Error(`Attribute ${name} not found in ${entity.name}`);
             }
-        }
 
-        throw new Error("missing Operation");
-    }
-
-    private parseConnectionOperation({
-        connectionEntry,
-        entity,
-        entityOperations,
-        context,
-    }: {
-        connectionEntry: {
-            [str: string]: ResolveTree;
-        };
-        entity: ConcreteEntity;
-        entityOperations: AuraEntityOperations;
-        context;
-    }): Operation {
-        const selection = this.parseConnectionSelection({ entity });
-
-        const fields = this.parseConnectionFields({
-            fields: connectionEntry.edges?.fieldsByTypeName[entityOperations.edgeType] ?? {},
-            entityOperations,
-            concreteEntity: entity,
-        });
-
-        const connectionOperation = new ConnectionReadOperation({
-            target: new ConcreteEntityAdapter(entity),
-            selection,
-            fields,
-        });
-        return connectionOperation;
-    }
-
-    private parseConnectionSelection({ entity }: { entity: ConcreteEntity }): EntitySelection {
-        return new NodeSelection({
-            target: new ConcreteEntityAdapter(entity),
-        });
-    }
-
-    private parseConnectionFields({
-        fields,
-        entityOperations,
-        concreteEntity,
-    }: {
-        fields: Record<string, ResolveTree>;
-        entityOperations: AuraEntityOperations;
-        concreteEntity: ConcreteEntity;
-    }): {
-        node: Field[];
-        edge: Field[];
-    } {
-        const res = this.splitConnectionFields(fields);
-
-        const nodeFields = res.node?.fieldsByTypeName[entityOperations.nodeType] ?? {};
-        // const nodeFields;
-        const nodeFields2 = Object.values(nodeFields).map((rawField) => {
             return new AttributeField({
                 alias: rawField.alias,
-                attribute: new AttributeAdapter(concreteEntity.attributes.get(rawField.name)!),
+                attribute: new AttributeAdapter(attribute),
             });
         });
-        return {
-            node: nodeFields2,
-            edge: [],
-        };
-    }
 
-    private splitConnectionFields(rawFields: Record<string, ResolveTree>): {
-        node: ResolveTree | undefined;
-        edge: ResolveTree | undefined;
-        fields: Record<string, ResolveTree>;
-    } {
-        let nodeField: ResolveTree | undefined;
-        let edgeField: ResolveTree | undefined;
-
-        const fields: Record<string, ResolveTree> = {};
-
-        Object.entries(rawFields).forEach(([key, field]) => {
-            if (field.name === "node") {
-                nodeField = field;
-            } else if (field.name === "edge") {
-                edgeField = field;
-            } else {
-                fields[key] = field;
-            }
+        return new ConnectionReadOperation({
+            target,
+            selection,
+            fields: {
+                edge: [],
+                node: nodeFields,
+            },
         });
-
-        return {
-            node: nodeField,
-            edge: edgeField,
-            fields,
-        };
     }
 }
