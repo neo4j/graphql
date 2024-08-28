783e99cfa4a59ebe40d95fd2c3b8f31b5672f115
Michael Webb <michael.webb@neo4j.com>
2024-08-28 16:36:25 +0200

    feat: enable schema where filters on cypher scalar fields

--- packages/graphql/tests/schema/directives/cypher.test.ts
+++ packages/graphql/tests/schema/directives/cypher.test.ts
@@ -357,6 +357,14 @@ describe("Cypher", () => {
               name_NOT_IN: [String] @deprecated(reason: \\"Negation filters will be deprecated, use the NOT operator to achieve the same behavior\\")
               name_NOT_STARTS_WITH: String @deprecated(reason: \\"Negation filters will be deprecated, use the NOT operator to achieve the same behavior\\")
               name_STARTS_WITH: String
+              totalScreenTime: Int
+              totalScreenTime_GT: Int
+              totalScreenTime_GTE: Int
+              totalScreenTime_IN: [Int!]
+              totalScreenTime_LT: Int
+              totalScreenTime_LTE: Int
+              totalScreenTime_NOT: Int @deprecated(reason: \\"Negation filters will be deprecated, use the NOT operator to achieve the same behavior\\")
+              totalScreenTime_NOT_IN: [Int!] @deprecated(reason: \\"Negation filters will be deprecated, use the NOT operator to achieve the same behavior\\")
             }
 
             type ActorsConnection {
