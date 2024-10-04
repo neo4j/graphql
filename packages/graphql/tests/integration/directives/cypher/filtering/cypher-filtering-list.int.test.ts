11e0ff80f2958aebfa82f7beb37ffdeeb16430c3
Michael Webb <michael.webb@neo4j.com>
2024-10-04 15:40:35 +0200

    test: make individual tests showing filtered results

--- packages/graphql/tests/integration/directives/cypher/filtering/cypher-filtering-list.int.test.ts
+++ packages/graphql/tests/integration/directives/cypher/filtering/cypher-filtering-list.int.test.ts
@@ -21,101 +21,366 @@ import type { UniqueType } from "../../../../utils/graphql-types";
 import { TestHelper } from "../../../../utils/tests-helper";
 
 describe("cypher directive filtering - List", () => {
-    let CustomType: UniqueType;
-
     const testHelper = new TestHelper();
-
-    afterEach(async () => {
-        await testHelper.close();
-    });
+    let CustomType: UniqueType;
 
     beforeEach(() => {
         CustomType = testHelper.createUniqueType("CustomType");
     });
 
-    test.each([
-        {
-            title: "String List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: "a"`,
-            type: "String",
-            cypherStatement: "RETURN ['a', 'b', 'c'] as list",
-        },
-        {
-            title: "Int List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: 2`,
-            type: "Int",
-            cypherStatement: "RETURN [1,2,3,4,5] as list",
-        },
-        {
-            title: "Float List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: 2.0`,
-            type: "Float",
-            cypherStatement: "RETURN [1.0,2.0,3.0,4.0,5.0] as list",
-        },
-        {
-            title: "Point List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: { latitude: 1, longitude: 2 }`,
-            type: "Point",
-            cypherStatement:
-                "RETURN [point({ latitude: 1, longitude: 2 }), point({ latitude: 3, longitude: 4 })] as list",
-        },
-        {
-            title: "CartesianPoint List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: { x: 1, y: 2, z: 3 }`,
-            type: "CartesianPoint",
-            cypherStatement: "RETURN [point({ x: 1, y: 2, z: 3 }), point({ x: 3, y: 4, z: 5 })] as list",
-        },
-        {
-            title: "DateTime List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: "2021-01-01T00:00:00Z"`,
-            type: "DateTime",
-            cypherStatement: "RETURN [datetime('2021-01-01T00:00:00Z'), datetime('2021-01-02T00:00:00Z')] as list",
-        },
-        {
-            title: "LocalDateTime List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: "2021-01-01T00:00:00"`,
-            type: "LocalDateTime",
-            cypherStatement:
-                "RETURN [localdatetime('2021-01-01T00:00:00'), localdatetime('2021-01-02T00:00:00')] as list",
-        },
-        {
-            title: "Date List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: "2021-01-01"`,
-            type: "Date",
-            cypherStatement: "RETURN [date('2021-01-01'), date('2021-01-02')] as list",
-        },
-        {
-            title: "Time List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: "12:00:00"`,
-            type: "Time",
-            cypherStatement: "RETURN [time('12:00:00'), time('13:00:00')] as list",
-        },
-        {
-            title: "LocalTime List cypher field: INCLUDES",
-            filter: `custom_cypher_list_INCLUDES: "12:00:00"`,
-            type: "LocalTime",
-            cypherStatement: "RETURN [localtime('12:00:00'), localtime('13:00:00')] as list",
-        },
-    ] as const)("$title", async ({ filter, type, cypherStatement }) => {
+    afterEach(async () => {
+        await testHelper.close();
+    });
+
+    test("String List cypher field: INCLUDES", async () => {
         const typeDefs = /* GraphQL */ `
             type ${CustomType} @node {
                 title: String
-                custom_cypher_list: [${type}] @cypher(statement: "${cypherStatement}", columnName: "list")
+                custom_cypher_list: [String] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
             }
         `;
 
         await testHelper.initNeo4jGraphQL({ typeDefs });
-        await testHelper.executeCypher(`CREATE (m:${CustomType} { title: "test" })`, {});
-
-        const query = /* GraphQL */ `
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: ['a', 'b', 'c'] })
+            CREATE (:${CustomType} { title: "test2", custom_data: ['d', 'e', 'f'] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
             query {
-                ${CustomType.plural}(where: { ${filter} }) {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: "a" }) {
                     title
                 }
             }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("Int List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [Int] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
         `;
 
-        const gqlResult = await testHelper.executeGraphQL(query);
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [1, 2, 3] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [4, 5, 6] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: 2 }) {
+                    title
+                }
+            }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("Float List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [Float] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
+        `;
+
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [10.0, 20.0, 30.0] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [40.0, 50.0, 60.0] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: 20.0 }) {
+                    title
+                }
+            }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("Point List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [Point] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
+        `;
+
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [point({ latitude: 1, longitude: 2 }), point({ latitude: 3, longitude: 4 })] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [point({ latitude: 5, longitude: 6 }), point({ latitude: 7, longitude: 8 })] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: { latitude: 1, longitude: 2 } }) {
+                    title
+                }
+            }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("CartesianPoint List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [CartesianPoint] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
+        `;
+
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [point({ x: 1, y: 2, z: 3 }), point({ x: 3, y: 4, z: 5 })] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [point({ x: 5, y: 6, z: 7 }), point({ x: 7, y: 8, z: 9 })] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: { x: 1, y: 2, z: 3 } }) {
+                    title
+                }
+            }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("DateTime List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [DateTime] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
+        `;
+
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [datetime('2021-01-01T00:00:00Z'), datetime('2021-02-01T00:00:00Z')] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [datetime('2021-03-01T00:00:00Z'), datetime('2021-04-01T00:00:00Z')] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: "2021-01-01T00:00:00Z" }) {
+                    title
+                }
+            }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("LocalDateTime List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [LocalDateTime] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
+        `;
+
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [localdatetime('2021-01-01T00:00:00'), localdatetime('2021-02-01T00:00:00')] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [localdatetime('2021-03-01T00:00:00'), localdatetime('2021-04-01T00:00:00')] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: "2021-01-01T00:00:00" }) {
+                    title
+                }
+            }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("Date List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [Date] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
+        `;
+
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [date('2021-01-01'), date('2021-02-01')] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [date('2021-03-01'), date('2021-04-01')] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: "2021-01-01" }) {
+                    title
+                }
+            }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("Time List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [Time] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
+        `;
+
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [time('12:00:00'), time('13:00:00')] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [time('14:00:00'), time('15:00:00')] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: "12:00:00" }) {
+                    title
+                }
+            }
+        `);
+
+        expect(gqlResult.errors).toBeFalsy();
+        expect(gqlResult?.data).toEqual({
+            [CustomType.plural]: [
+                {
+                    title: "test",
+                },
+            ],
+        });
+    });
+
+    test("LocalTime List cypher field: INCLUDES", async () => {
+        const typeDefs = /* GraphQL */ `
+            type ${CustomType} @node {
+                title: String
+                custom_cypher_list: [LocalTime] @cypher(statement: 
+                    """
+                    MATCH (this)
+                    RETURN this.custom_data as list
+                    """
+                    , columnName: "list")
+            }
+        `;
+
+        await testHelper.initNeo4jGraphQL({ typeDefs });
+        await testHelper.executeCypher(`
+            CREATE (:${CustomType} { title: "test", custom_data: [localtime('12:00:00'), localtime('13:00:00')] })
+            CREATE (:${CustomType} { title: "test2", custom_data: [localtime('14:00:00'), localtime('15:00:00')] })
+        `);
+        const gqlResult = await testHelper.executeGraphQL(/* GraphQL */ `
+            query {
+                ${CustomType.plural}(where: { custom_cypher_list_INCLUDES: "12:00:00" }) {
+                    title
+                }
+            }
+        `);
 
         expect(gqlResult.errors).toBeFalsy();
         expect(gqlResult?.data).toEqual({
