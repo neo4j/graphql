bf7147622ffd7587ffffc89ba3e5f9f90a33ec9f
Darrell Warde <darrell.warde@gmail.com>
2024-10-09 13:22:27 +0100

    Update schema test

--- packages/graphql/tests/schema/remove-deprecated/implicit-equality.test.ts
+++ packages/graphql/tests/schema/remove-deprecated/implicit-equality.test.ts
@@ -104,6 +104,7 @@ describe("Implicit Equality filters", () => {
               AND: [ActedInWhere!]
               NOT: ActedInWhere
               OR: [ActedInWhere!]
+              role: String @deprecated(reason: \\"Please use the explicit _EQ version\\")
               role_CONTAINS: String
               role_ENDS_WITH: String
               role_EQ: String
@@ -167,6 +168,7 @@ describe("Implicit Equality filters", () => {
               AND: [ActorMoviesAggregateInput!]
               NOT: ActorMoviesAggregateInput
               OR: [ActorMoviesAggregateInput!]
+              count: Int @deprecated(reason: \\"Please use the explicit _EQ version\\")
               count_EQ: Int
               count_GT: Int
               count_GTE: Int
@@ -292,6 +294,7 @@ describe("Implicit Equality filters", () => {
               movies_SINGLE: MovieWhere
               \\"\\"\\"Return Actors where some of the related Movies match this filter\\"\\"\\"
               movies_SOME: MovieWhere
+              name: String @deprecated(reason: \\"Please use the explicit _EQ version\\")
               name_CONTAINS: String
               name_ENDS_WITH: String
               name_EQ: String
@@ -361,6 +364,7 @@ describe("Implicit Equality filters", () => {
               AND: [MovieActorsAggregateInput!]
               NOT: MovieActorsAggregateInput
               OR: [MovieActorsAggregateInput!]
+              count: Int @deprecated(reason: \\"Please use the explicit _EQ version\\")
               count_EQ: Int
               count_GT: Int
               count_GTE: Int
@@ -532,6 +536,7 @@ describe("Implicit Equality filters", () => {
               actors_SINGLE: ActorWhere
               \\"\\"\\"Return Movies where some of the related Actors match this filter\\"\\"\\"
               actors_SOME: ActorWhere
+              id: ID @deprecated(reason: \\"Please use the explicit _EQ version\\")
               id_CONTAINS: ID
               id_ENDS_WITH: ID
               id_EQ: ID
