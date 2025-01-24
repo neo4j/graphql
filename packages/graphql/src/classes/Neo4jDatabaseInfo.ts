dbd5e407b4a8def0daf228c1ed9a33545665cab5
angrykoala <angrykoala@outlook.es>
2025-01-24 12:16:07 +0000

    Fix for CalVer
    (cherry picked from commit ebbb92136ef834a8d1b923ed4a4e8c0a11505924)

--- packages/graphql/src/classes/Neo4jDatabaseInfo.ts
+++ packages/graphql/src/classes/Neo4jDatabaseInfo.ts
@@ -29,6 +29,8 @@ export class Neo4jDatabaseInfo {
     public edition: Neo4jEdition | undefined;
 
     constructor(version: string, edition?: Neo4jEdition) {
+        // Quick hack to support CalVar
+        version = version.replace(/\.0([0-9]+)/, ".$1");
         this.version = this.toSemVer(version);
         this.rawVersion = version;
         this.edition = edition;
