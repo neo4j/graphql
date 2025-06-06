1e77428b034b383fefeebb0459b8327f58563e0b
MacondoExpress <simone.gammicchia@gmail.com>
2025-06-06 15:28:22 +0100

    remove unnecessary comments from ReadOperation

--- packages/graphql/src/translate/queryAST/ast/operations/ReadOperation.ts
+++ packages/graphql/src/translate/queryAST/ast/operations/ReadOperation.ts
@@ -214,11 +214,6 @@ export class ReadOperation extends Operation {
             }
             matchBlock.push(...extraMatches, extraMatchesWith);
 
-            // if (this.relationship) {
-            //     const distinctTargetWith = new Cypher.With(nestedContext.target).distinct();
-            //     matchBlock.push(distinctTargetWith);
-            // }
-
             clause = Cypher.utils.concat(
                 ...matchBlock,
                 this.relationship ? new Cypher.With(nestedContext.target).distinct() : undefined,
