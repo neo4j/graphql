49c2ed5590cf83e542e4b629beb4c6d8631d1215
angrykoala <angrykoala@outlook.es>
2025-08-04 11:03:53 +0100

    Add test cases for create with deeply nested connect with authorization

--- packages/graphql/tests/integration/mutations/create/authorization/filter/create-authorization-filter.int.test.ts
+++ packages/graphql/tests/integration/mutations/create/authorization/filter/create-authorization-filter.int.test.ts
@@ -20,15 +20,17 @@
 import type { UniqueType } from "../../../../../utils/graphql-types";
 import { TestHelper } from "../../../../../utils/tests-helper";
 
-describe("create with authorization validate", () => {
+describe("create with authorization filter", () => {
     const testHelper = new TestHelper();
     let User: UniqueType;
     let Post: UniqueType;
+    let Link: UniqueType;
     const secret = "secret";
 
     beforeEach(async () => {
         User = testHelper.createUniqueType("User");
         Post = testHelper.createUniqueType("Post");
+        Link = testHelper.createUniqueType("Link");
 
         const typeDefs = /* GraphQL */ `
             type ${User} @node {
@@ -45,6 +47,14 @@ describe("create with authorization validate", () => {
                 creatorId: ID
                 content: String
                 creator: [${User}!]! @relationship(type: "HAS_CONTENT", direction: IN)
+                links: [${Link}!]! @relationship(type: "HAS_LINK", direction: OUT)
+            }
+
+            type ${Link} 
+                @node 
+                @authorization( filter: [{ operations: [CREATE_RELATIONSHIP], where: { node: { public: { eq: true } } } }]) {
+                url: String
+                public: Boolean
             }
 
             extend type ${User}
@@ -122,6 +132,91 @@ describe("create with authorization validate", () => {
                     name: "Bob",
                 },
                 to: {
+                    creatorId: id,
+                    content: "dont panic",
+                },
+                relationship: {},
+            },
+        ]);
+    });
+
+    test("create -> connect -> connect with authorization filter", async () => {
+        const id = "123";
+        const query = /* GraphQL */ `
+            mutation ($id: ID!) {
+                ${User.operations.create}(input: [{ id: $id, name: "Bob", content: {connect: {
+                    where: {
+                        node: {
+                            content: {eq: "dont panic"}
+                        }
+                    },
+                    connect: {
+                        links: {}
+                    }
+                }} }]) {
+                    ${User.plural} {
+                        id
+                        content {
+                            creatorId,
+                            content
+                        }
+                    }
+                }
+            }
+        `;
+
+        const token = testHelper.createBearerToken(secret, { sub: id });
+
+        await testHelper.executeCypher(`
+            CREATE(:${Post} {creatorId: "${id}", content: "dont panic"})
+            CREATE(:${Post} {creatorId: "another-id", content: "dont panic"})
+            
+            CREATE(:${Link} {url: "url-1", public: true})
+            CREATE(:${Link} {url: "url-2", public: false})
+            `);
+        const gqlResult = await testHelper.executeGraphQLWithToken(query, token, {
+            variableValues: { id },
+        });
+
+        expect(gqlResult.errors).toBeFalsy();
+
+        expect(gqlResult.data).toEqual({
+            [User.operations.create]: {
+                [User.plural]: [
+                    {
+                        id,
+                        content: [
+                            {
+                                creatorId: id,
+                                content: "dont panic",
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
+
+        await testHelper.expectRelationship(User, Post, "HAS_CONTENT").toEqual([
+            {
+                from: {
+                    id,
+                    name: "Bob",
+                },
+                to: {
+                    creatorId: id,
+                    content: "dont panic",
+                },
+                relationship: {},
+            },
+        ]);
+
+        await testHelper.expectRelationship(Post, Link, "HAS_LINK").toEqual([
+            {
+                to: {
+                    url: "url-1",
+                    public: true,
+                },
+                from: {
                     creatorId: id,
                     content: "dont panic",
                 },
--- packages/graphql/tests/integration/mutations/create/authorization/validate/create-authorization-validate.int.test.ts
+++ packages/graphql/tests/integration/mutations/create/authorization/validate/create-authorization-validate.int.test.ts
@@ -25,11 +25,13 @@ describe("create with authorization validate", () => {
     const testHelper = new TestHelper();
     let User: UniqueType;
     let Post: UniqueType;
+    let Link: UniqueType;
     const secret = "secret";
 
     beforeEach(async () => {
         User = testHelper.createUniqueType("User");
         Post = testHelper.createUniqueType("Post");
+        Link = testHelper.createUniqueType("Link");
 
         const typeDefs = /* GraphQL */ `
             type ${User} @node {
@@ -46,6 +48,14 @@ describe("create with authorization validate", () => {
                 creatorId: ID
                 content: String
                 creator: [${User}!]! @relationship(type: "HAS_CONTENT", direction: IN)
+                links: [${Link}!]! @relationship(type: "HAS_LINK", direction: OUT)
+            }
+
+            type ${Link} 
+                @node 
+                @authorization( validate: [{ operations: [CREATE_RELATIONSHIP], where: { node: { public: { eq: true } } } }]) {
+                url: String
+                public: Boolean
             }
 
             extend type ${User}
@@ -284,5 +294,121 @@ describe("create with authorization validate", () => {
         expect(gqlResult.errors?.[0]).toBeInstanceOf(GraphQLError);
 
         await testHelper.expectNode(User).toNotExist();
+    });
+
+    test("create -> connect -> connect with authorization validation pass", async () => {
+        const id = "123";
+        const query = /* GraphQL */ `
+            mutation ($id: ID!) {
+                ${User.operations.create}(input: [{ id: $id, name: "Bob", content: {connect: {
+                    where: {
+                        node: {
+                            creatorId: {eq: "123"},
+                        }
+                    },
+                    connect: {
+                        links: {
+                            where: {
+                                node: {
+                                    url: {eq: "url-1"}
+                                }
+                            }
+                        }
+                    }
+                }} }]) {
+                    ${User.plural} {
+                        id
+                        content {
+                            creatorId,
+                            content
+                        }
+                    }
+                }
+          }
+        `;
+
+        const token = testHelper.createBearerToken(secret, { sub: id });
+
+        await testHelper.executeCypher(`
+            CREATE(:${Post} {creatorId: "123", content: "Existing content"})
+            CREATE(:${Link} {url: "url-1", public: true})
+            CREATE(:${Link} {url: "url-2", public: false})
+            
+            `);
+        const gqlResult = await testHelper.executeGraphQLWithToken(query, token, {
+            variableValues: { id },
+        });
+
+        expect(gqlResult.errors).toBeFalsy();
+
+        expect(gqlResult.data).toEqual({
+            [User.operations.create]: {
+                [User.plural]: [
+                    {
+                        id,
+                        content: [
+                            {
+                                creatorId: "123",
+                                content: "Existing content",
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
+
+        await testHelper.expectRelationship(User, Post, "HAS_CONTENT").count(1);
+        await testHelper.expectRelationship(Post, Link, "HAS_LINK").count(1);
+    });
+
+    test("create -> connect -> connect with authorization validation fails", async () => {
+        const id = "123";
+        const query = /* GraphQL */ `
+            mutation ($id: ID!) {
+                ${User.operations.create}(input: [{ id: $id, name: "Bob", content: {connect: {
+                    where: {
+                        node: {
+                            creatorId: {eq: "123"},
+                        }
+                    },
+                    connect: {
+                        links: {
+                            where: {
+                                node: {
+                                    url: {eq: "url-2"}
+                                }
+                            }
+                        }
+                    }
+                }} }]) {
+                    ${User.plural} {
+                        id
+                        content {
+                            creatorId,
+                            content
+                        }
+                    }
+                }
+          }
+        `;
+
+        const token = testHelper.createBearerToken(secret, { sub: id });
+
+        await testHelper.executeCypher(`
+            CREATE(:${Post} {creatorId: "123", content: "Existing content"})
+            CREATE(:${Link} {url: "url-1", public: true})
+            CREATE(:${Link} {url: "url-2", public: false})
+            
+            `);
+        const gqlResult = await testHelper.executeGraphQLWithToken(query, token, {
+            variableValues: { id },
+        });
+
+        expect(gqlResult.errors?.[0]?.message).toBe("Forbidden");
+        expect(gqlResult.errors?.[0]).toBeInstanceOf(GraphQLError);
+
+        await testHelper.expectNode(User).toNotExist();
+        await testHelper.expectRelationship(User, Post, "HAS_CONTENT").toNotExist();
+        await testHelper.expectRelationship(Post, Link, "HAS_LINK").toNotExist();
     });
 });
