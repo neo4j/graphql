a7aa30f561184ab00fec4a36989366a576e8ca2b
angrykoala <angrykoala@outlook.es>
2025-11-25 14:19:41 +0000

    Remove console log in tests

--- packages/graphql/tests/integration/connections/interfaces-top-level.int.test.ts
+++ packages/graphql/tests/integration/connections/interfaces-top-level.int.test.ts
@@ -117,7 +117,6 @@ describe("Interfaces top level connections", () => {
             }
         `;
         const queryResults = await testHelper.executeGraphQL(query);
-        console.log(queryResults.errors);
         expect(queryResults.errors).toBeUndefined();
         expect(queryResults.data).toEqual({
             showsConnection: {
