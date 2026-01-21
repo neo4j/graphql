a3db9085c34d27e14c9895ee8742c3307cf22a19
a-alle <alexandra.albu@neo4j.com>
2026-01-21 11:07:21 +0000

    fix interface schema config logic

--- packages/graphql/src/schema-model/entity/model-adapters/InterfaceEntityAdapter.ts
+++ packages/graphql/src/schema-model/entity/model-adapters/InterfaceEntityAdapter.ts
@@ -106,9 +106,11 @@ export class InterfaceEntityAdapter {
     }
     public isReadableFromConnectionRootQuery(schemaModel: Neo4jGraphQLSchemaModel): boolean {
         if (this.annotations.query) {
-            return this.annotations.query.connection;
+            if (this.annotations.query.connection !== undefined) {
+                return this.annotations.query.connection;
+            }
+            return this.annotations.query.read;
         }
-
         return schemaModel.annotations.query === undefined || schemaModel.annotations.query.connection === true;
     }
 
