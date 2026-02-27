66ca82d9feafb8da0874e9d7be34e32ad730385d
a-alle <alexandra.albu@neo4j.com>
2026-02-27 17:13:01 +0000

    change computation formula

--- packages/graphql/src/classes/ComplexityEstimatorHelper.ts
+++ packages/graphql/src/classes/ComplexityEstimatorHelper.ts
@@ -16,65 +16,76 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { type DefinitionNode,type GraphQLFieldExtensions, type GraphQLSchema, GraphQLInterfaceType, GraphQLObjectType, Kind } from "graphql";
-import type { ComplexityEstimator} from "graphql-query-complexity";
-import { fieldExtensionsEstimator, simpleEstimator } from "graphql-query-complexity";
+import {
+    type DefinitionNode,
+    type GraphQLFieldExtensions,
+    type GraphQLSchema,
+    GraphQLInterfaceType,
+    GraphQLObjectType,
+    Kind,
+} from "graphql";
 
 export class ComplexityEstimatorHelper {
     private objectTypeNameToFieldNamesMapForComplexityExtensions: Map<string, string[]>;
     private useComplexityEstimators: boolean;
 
     constructor(useComplexityEstimators: boolean) {
-        this.useComplexityEstimators = useComplexityEstimators
+        this.useComplexityEstimators = useComplexityEstimators;
         this.objectTypeNameToFieldNamesMapForComplexityExtensions = new Map<string, string[]>();
     }
 
     public registerField(parentObjectTypeNameOrInterfaceTypeName: string, fieldName: string): void {
-        if(this.useComplexityEstimators) {
-            const existingFieldsForTypeName = this.objectTypeNameToFieldNamesMapForComplexityExtensions.get(parentObjectTypeNameOrInterfaceTypeName) ?? []
-            this.objectTypeNameToFieldNamesMapForComplexityExtensions.set(parentObjectTypeNameOrInterfaceTypeName, existingFieldsForTypeName.concat(fieldName))
+        if (this.useComplexityEstimators) {
+            const existingFieldsForTypeName =
+                this.objectTypeNameToFieldNamesMapForComplexityExtensions.get(
+                    parentObjectTypeNameOrInterfaceTypeName
+                ) ?? [];
+            this.objectTypeNameToFieldNamesMapForComplexityExtensions.set(
+                parentObjectTypeNameOrInterfaceTypeName,
+                existingFieldsForTypeName.concat(fieldName)
+            );
         }
     }
 
     public hydrateDefinitionNodeWithComplexityExtensions(definition: DefinitionNode): DefinitionNode {
-        if(definition.kind !== Kind.OBJECT_TYPE_DEFINITION && definition.kind !== Kind.INTERFACE_TYPE_DEFINITION) {
+        if (definition.kind !== Kind.OBJECT_TYPE_DEFINITION && definition.kind !== Kind.INTERFACE_TYPE_DEFINITION) {
             return definition;
         }
-        if(!this.objectTypeNameToFieldNamesMapForComplexityExtensions.has(definition.name.value)) {
-            return definition
+        if (!this.objectTypeNameToFieldNamesMapForComplexityExtensions.has(definition.name.value)) {
+            return definition;
         }
-        
-        const fieldsWithComplexity = definition.fields?.map(f => {
-            const hasFieldComplexityEstimator = this.getFieldsForParentTypeName(definition.name.value).find(fieldName => fieldName === f.name.value)
+
+        const fieldsWithComplexity = definition.fields?.map((f) => {
+            const hasFieldComplexityEstimator = this.getFieldsForParentTypeName(definition.name.value).find(
+                (fieldName) => fieldName === f.name.value
+            );
             if (!hasFieldComplexityEstimator) {
-                return f
+                return f;
             }
             return {
                 ...f,
                 extensions: {
                     // COMPLEXITY FORMULA
-                    // c = c_child + lvl_limit * c_field, where
+                    // c = c_child * lvl_limit + c_field, where
                     // c_field = 1
                     // lvl_limit defaults to 1
-                    // c_child comes from simpleEstimator 
-                    complexity: ({childComplexity, args}) => {
-                        const fieldDefaultComplexity = 1
-                        const defaultLimitIfNotProvided = 1
-                        if(args.limit ?? args.first) {
-                            return childComplexity + (args.limit ?? args.first) * fieldDefaultComplexity
+                    // c_child comes from simpleEstimator
+                    complexity: ({ childComplexity, args }) => {
+                        const fieldDefaultComplexity = 1;
+                        const defaultLimitIfNotProvided = 1;
+                        if (args.limit ?? args.first) {
+                            return childComplexity * (args.limit ?? args.first) + fieldDefaultComplexity;
                         }
-                        return childComplexity + defaultLimitIfNotProvided
-                        
+                        return childComplexity * defaultLimitIfNotProvided + fieldDefaultComplexity;
                     },
                 },
-            }
-        })
+            };
+        });
         return {
             ...definition,
             fields: fieldsWithComplexity,
-        }
+        };
     }
-
 
     public hydrateSchemaFromSDLWithASTNodeExtensions(schema: GraphQLSchema): void {
         const types = schema.getTypeMap();
@@ -82,7 +93,7 @@ export class ComplexityEstimatorHelper {
             if (type instanceof GraphQLObjectType || type instanceof GraphQLInterfaceType) {
                 const fields = type.getFields();
                 Object.values(fields).forEach((field) => {
-                    if (field.astNode && 'extensions' in field.astNode) {
+                    if (field.astNode && "extensions" in field.astNode) {
                         field.extensions = field.astNode.extensions as GraphQLFieldExtensions<any, any, any>;
                     }
                 });
@@ -90,17 +101,9 @@ export class ComplexityEstimatorHelper {
         });
     }
 
-    public getComplexityEstimators(): ComplexityEstimator[] {
-        if (!this.useComplexityEstimators) {
-            return []
-        }
-        return [
-            fieldExtensionsEstimator(),
-            simpleEstimator({ defaultComplexity: 1 }),
-        ];
-    }
-
     private getFieldsForParentTypeName(parentObjectTypeNameOrInterfaceTypeName: string): string[] {
-        return this.objectTypeNameToFieldNamesMapForComplexityExtensions.get(parentObjectTypeNameOrInterfaceTypeName) || []
+        return (
+            this.objectTypeNameToFieldNamesMapForComplexityExtensions.get(parentObjectTypeNameOrInterfaceTypeName) || []
+        );
     }
 }
